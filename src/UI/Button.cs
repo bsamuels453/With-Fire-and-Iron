490d9b9eff3e48832bdf7de703432effa2877476
Ben Samuels <bsamuels453@gmail.com>
2012-12-22 19:06:59 -0800

    changing position storage from floats to ints

--- src/UI/Button.cs
+++ src/UI/Button.cs
@@ -10,148 +10,12 @@ using Gondola.Logic;
 using Gondola.UI.Components;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 #endregion
 
 namespace Gondola.UI{
     internal class Button{
-        #region Nested type: OnBasicMouseEvent
-
-        internal delegate void OnBasicMouseEvent(int identifier);
-
-        #endregion
-
-        #region properties and fields
-
-        public const int DefaultTexRepeat = 1;
-        public const int DefaultIdentifier = 1;
-
-        readonly FloatingRectangle _boundingBox; //bounding box that represents the bounds of the button
-        readonly int _identifier; //non-function based identifier that can be used to differentiate buttons
-        readonly Sprite2D _sprite; //the button's sprite
-        Vector2 _centPosition; //represents the approximate center of the button
-        bool _isEnabled;
-        string _texture;
-
-        public RenderTarget RenderTarget { get; private set; }
-
-        public Vector2 CentPosition{
-            get { return _centPosition; }
-            set{
-                _centPosition = value;
-                _boundingBox.X = _centPosition.X - _boundingBox.Width/2;
-                _boundingBox.Y = _centPosition.Y - _boundingBox.Height/2;
-            }
-        }
-
-        public IUIComponent[] Components { get; set; }
-
-        public String Texture{
-            get { return _texture; }
-            set{
-                _texture = value;
-                _sprite.SetTextureFromString(value);
-            }
-        }
-
-        public int Identifier{
-            get { return _identifier; }
-        }
-
-        //xxxx setting these position variables after construction wont update highlight masks
-        public int X{
-            get { return (int)_boundingBox.X; }
-            set{
-                _boundingBox.X = value;
-                _centPosition.X = _boundingBox.X + _boundingBox.Width/2;
-                _sprite.X =  value;
-            }
-        }
-
-        public int Y {
-            get { return (int)_boundingBox.Y; }
-            set{
-                _boundingBox.Y = value;
-                _centPosition.Y = _boundingBox.Y + _boundingBox.Height/2;
-                _sprite.Y = (int) value;
-            }
-        }
-
-        public float Width{
-            get { return _boundingBox.Width; }
-            set{
-                _boundingBox.Width = value;
-                _sprite.Width = (int) value;
-            }
-        }
-
-        public float Height{
-            get { return _boundingBox.Height; }
-            set{
-                _boundingBox.Height = value;
-                _sprite.Height = (int) value;
-            }
-        }
-
-        public FloatingRectangle BoundingBox{
-            get { return _boundingBox; }
-        }
-
-        public bool IsEnabled{
-            get { return _isEnabled; }
-            set{
-                _isEnabled = value;
-                _sprite.IsEnabled = value;
-                foreach (var component in Components){
-                    component.IsEnabled = value;
-                }
-            }
-        }
-
-        public bool ContainsMouse { get; set; }
-
-        public bool ContainedMousePrevisly { get; private set; }
-
-        public float Opacity{
-            get { return _sprite.Opacity; }
-            set { _sprite.Opacity = value; }
-        }
-
-        public event OnBasicMouseEvent OnLeftClickDispatcher;
-        public event OnBasicMouseEvent OnLeftPressDispatcher;
-        public event OnBasicMouseEvent OnLeftReleaseDispatcher;
-
-        #endregion
-
-        #region ctor
-
-        //xxx why are these position coordinates all floats?
-        public Button(RenderTarget target, int x, int y, int width, int height, string textureName, float spriteTexRepeatX = DefaultTexRepeat, float spriteTexRepeatY = DefaultTexRepeat, int identifier = DefaultIdentifier, IUIComponent[] components = null){
-            RenderTarget = target;
-            _identifier = identifier;
-            _isEnabled = true;
-
-            _centPosition = new Vector2();
-            _boundingBox = new FloatingRectangle(x, y, width, height);
-            _sprite = new Sprite2D(target, textureName,  x,  y,  width,  height, 1, spriteTexRepeatX, spriteTexRepeatY);
-
-            _centPosition.X = _boundingBox.X + _boundingBox.Width/2;
-            _centPosition.Y = _boundingBox.Y + _boundingBox.Height/2;
-
-            Components = components;
-            Opacity = 1;
-            if (Components != null){
-                foreach (IUIComponent component in Components){
-                    component.ComponentCtor(this);
-                }
-            }
-            ////UIElementCollection.AddElement(this);
-        }
-
-        #endregion
-
         public TComponent GetComponent<TComponent>(string identifier = null){
             if (Components != null){
                 foreach (IUIComponent component in Components){
@@ -196,7 +60,7 @@ namespace Gondola.UI{
         }
 
         //xxx fix loops
-        public void Update(ref InputState state, double timeDelta) {
+        public void Update(ref InputState state, double timeDelta){
             if (IsEnabled){
                 if (ContainsMouse && !ContainedMousePrevisly)
                     ContainedMousePrevisly = true;
@@ -215,8 +79,8 @@ namespace Gondola.UI{
                 }
 
 
-                if (Components != null) {
-                    foreach (IUIComponent component in Components) {
+                if (Components != null){
+                    foreach (IUIComponent component in Components){
                         component.Update(state, timeDelta);
                     }
                 }
@@ -314,11 +178,145 @@ namespace Gondola.UI{
                 point.X < BoundingBox.X + BoundingBox.Width &&
                 point.Y > BoundingBox.Y &&
                 point.Y < BoundingBox.Y + BoundingBox.Height){
-
-                    return true;
+                return true;
             }
             return false;
         }
+
+        #region Nested type: OnBasicMouseEvent
+
+        internal delegate void OnBasicMouseEvent(int identifier);
+
+        #endregion
+
+        #region properties and fields
+
+        public const int DefaultTexRepeat = 1;
+        public const int DefaultIdentifier = 1;
+
+        readonly FloatingRectangle _boundingBox; //bounding box that represents the bounds of the button
+        readonly int _identifier; //non-function based identifier that can be used to differentiate buttons
+        readonly Sprite2D _sprite; //the button's sprite
+        Vector2 _centPosition; //represents the approximate center of the button
+        bool _isEnabled;
+        string _texture;
+
+        public RenderTarget RenderTarget { get; private set; }
+
+        public Vector2 CentPosition{
+            get { return _centPosition; }
+            set{
+                _centPosition = value;
+                _boundingBox.X = _centPosition.X - _boundingBox.Width/2;
+                _boundingBox.Y = _centPosition.Y - _boundingBox.Height/2;
+            }
+        }
+
+        public IUIComponent[] Components { get; set; }
+
+        public String Texture{
+            get { return _texture; }
+            set{
+                _texture = value;
+                _sprite.SetTextureFromString(value);
+            }
+        }
+
+        public int Identifier{
+            get { return _identifier; }
+        }
+
+        //xxxx setting these position variables after construction wont update highlight masks
+        public int X{
+            get { return (int) _boundingBox.X; }
+            set{
+                _boundingBox.X = value;
+                _centPosition.X = _boundingBox.X + _boundingBox.Width/2;
+                _sprite.X = value;
+            }
+        }
+
+        public int Y{
+            get { return (int) _boundingBox.Y; }
+            set{
+                _boundingBox.Y = value;
+                _centPosition.Y = _boundingBox.Y + _boundingBox.Height/2;
+                _sprite.Y = value;
+            }
+        }
+
+        public int Width{
+            get { return (int) _boundingBox.Width; }
+            set{
+                _boundingBox.Width = value;
+                _sprite.Width = value;
+            }
+        }
+
+        public int Height{
+            get { return (int) _boundingBox.Height; }
+            set{
+                _boundingBox.Height = value;
+                _sprite.Height = value;
+            }
+        }
+
+        public FloatingRectangle BoundingBox{
+            get { return _boundingBox; }
+        }
+
+        public bool IsEnabled{
+            get { return _isEnabled; }
+            set{
+                _isEnabled = value;
+                _sprite.IsEnabled = value;
+                foreach (var component in Components){
+                    component.IsEnabled = value;
+                }
+            }
+        }
+
+        public bool ContainsMouse { get; set; }
+
+        public bool ContainedMousePrevisly { get; private set; }
+
+        public float Opacity{
+            get { return _sprite.Opacity; }
+            set { _sprite.Opacity = value; }
+        }
+
+        public event OnBasicMouseEvent OnLeftClickDispatcher;
+        public event OnBasicMouseEvent OnLeftPressDispatcher;
+        public event OnBasicMouseEvent OnLeftReleaseDispatcher;
+
+        #endregion
+
+        #region ctor
+
+        //xxx why are these position coordinates all floats?
+        public Button(RenderTarget target, int x, int y, int width, int height, string textureName, float spriteTexRepeatX = DefaultTexRepeat, float spriteTexRepeatY = DefaultTexRepeat, int identifier = DefaultIdentifier, IUIComponent[] components = null){
+            RenderTarget = target;
+            _identifier = identifier;
+            _isEnabled = true;
+
+            _centPosition = new Vector2();
+            _boundingBox = new FloatingRectangle(x, y, width, height);
+            _sprite = new Sprite2D(target, textureName, x, y, width, height, 1, spriteTexRepeatX, spriteTexRepeatY);
+
+            _centPosition.X = _boundingBox.X + _boundingBox.Width/2;
+            _centPosition.Y = _boundingBox.Y + _boundingBox.Height/2;
+
+            Components = components;
+            Opacity = 1;
+            if (Components != null){
+                foreach (IUIComponent component in Components){
+                    component.ComponentCtor(this);
+                }
+            }
+            ////UIElementCollection.AddElement(this);
+        }
+
+        #endregion
     }
 
     internal class ButtonGenerator{
@@ -410,10 +408,10 @@ namespace Gondola.UI{
 
             return new Button(
                 RenderTarget,
-                (int)X,
-                (int)Y,
-                (int)Width,
-                (int)Height,
+                (int) X,
+                (int) Y,
+                (int) Width,
+                (int) Height,
                 TextureName,
                 spriteTexRepeatX,
                 spriteTexRepeatY,
