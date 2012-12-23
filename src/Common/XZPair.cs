2ba3a70ae9e12dde5cac98b6e7955ffa36cdf323
Ben Samuels <bsamuels453@gmail.com>
2012-12-23 15:55:33 -0800

    porting over some more classes from forge

---
+++ src/Common/XZPair.cs
@@ -0,0 +1,92 @@
+﻿#region
+
+using System;
+using System.Runtime.Serialization;
+
+#endregion
+
+namespace Gondola.Common{
+    /// <summary>
+    ///   each XZpair cooresponds to an x and z value. SERIALIZABLE CLONEABLE
+    /// </summary>
+    struct XZPair : ICloneable{
+        public int X;
+        public int Z;
+
+        public XZPair(int x, int z){
+            X = x;
+            Z = z;
+        }
+
+        public XZPair(XZPair xzpair){
+            X = xzpair.X;
+            Z = xzpair.Z;
+        }
+
+        public XZPair(SerializationInfo info, StreamingContext ctxt){
+            X = (int) info.GetValue("X", typeof (int));
+            Z = (int) info.GetValue("Z", typeof (int));
+        }
+
+        #region ICloneable Members
+
+        public object Clone(){
+            return new XZPair(X, Z);
+        }
+
+        #endregion
+
+        public static XZPair Zero(){
+            return new XZPair(0, 0);
+        }
+
+        /*
+        public static bool operator ==(Pair var1, XZpair var2) {
+            if ((var1.X == var2.X) && (var1.Y == var2.Z)) {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator !=(Pair var1, XZpair var2) {
+            if ((var1.X == var2.X) && (var1.Y == var2.Z)) {
+                return false;
+            }
+            return true;
+        }
+         * */
+
+        public static bool operator ==(XZPair var1, XZPair var2){
+            if ((var1.X == var2.X) && (var1.Z == var2.Z)){
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator !=(XZPair var1, XZPair var2){
+            if ((var1.X == var2.X) && (var1.Z == var2.Z)){
+                return false;
+            }
+            return true;
+        }
+
+        public bool Equals(XZPair other){
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return other.X == X && other.Z == Z;
+        }
+
+        public override bool Equals(object obj){
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != typeof (XZPair)) return false;
+            return Equals((XZPair) obj);
+        }
+
+        public override int GetHashCode(){
+            unchecked{
+                return (X*397) ^ Z;
+            }
+        }
+    }
+}
---
+++ src/GameState/Terrain/ChunkData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gondola.Common;
+using Gondola.Draw;
+
+namespace Gondola.GameState.Terrain {
+    struct ChunkData {
+        public XZPair Identifier;
+        public TerrainBuffer Buffer;
+    }
+}
