4b234879ecf7c0835fd8e170e0cea12970fa9ed3
Ben Samuels <bsamuels453@gmail.com>
2012-12-23 15:55:45 -0800

    adding terrainmanager game state unimplemented

---
+++ src/GameState/Terrain/TerrainManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Gondola.Common;
+using Gondola.Logic;
+
+namespace Gondola.GameState.Terrain {
+    class TerrainManager : IGameState {
+        List<ChunkData> _loadedChunks;
+        Vec3Ref _viewportPos;
+
+        public TerrainManager(){
+
+        }
+
+        public void Dispose(){
+
+        }
+
+        public void Update(InputState state, double timeDelta){
+
+        }
+
+        public void Draw(){
+            foreach (var chunk in _loadedChunks){
+                
+            }
+        }
+    }
+}
