d0298096462eaf529cbfd3889e00a4480e70465b
Ben Samuels <bsamuels453@gmail.com>
2012-12-26 21:35:17 -0800

    updating terraingen to accommodate new terraingen script variables

--- src/GameState/Terrain/TerrainGen.cs
+++ src/GameState/Terrain/TerrainGen.cs
@@ -1,27 +1,33 @@
 ﻿#region
 
-using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using Cloo;
 using Gondola.Logic;
-using OpenCLTemplate;
 
 #endregion
 
 namespace Gondola.GameState.Terrain{
     internal class TerrainGen{
+
         readonly int _blockWidth;
-        readonly int _chunkVertWidth;
-        readonly CLCalc.Program.Variable _terrGeometry;
-        readonly CLCalc.Program.Image2D _terrNormals;
-        readonly CLCalc.Program.Image2D _terrBiNormals;
-        readonly CLCalc.Program.Image2D _terrTangents;
-        readonly CLCalc.Program.MemoryObject[] _terrainGenArgs;
-        readonly CLCalc.Program.Kernel _terrainGenKernel;
-        readonly int[] _terrainGenWorkers;
+        readonly int _chunkWidthInVerts;
+        readonly ComputeCommandQueue _cmdQueue;
+
+        readonly ComputeBuffer<float> _constants;
+        readonly ComputeContext _context;
+        readonly List<ComputeDevice> _devices;
+        readonly ComputeBuffer<float> _geometry;
+        readonly ComputeKernel _kernel;
+        readonly ComputeProgram _program;
+        readonly ComputeContextPropertyList _properties;
+
+        readonly ComputeBuffer<byte> _normals;
+        readonly ComputeBuffer<byte> _binormals;
+        readonly ComputeBuffer<byte> _tangents;
 
         public TerrainGen(){
-            CLCalc.InitCL();
-
-            _chunkVertWidth = Gbl.LoadContent<int>("TGen_ChunkWidthInBlocks") + 1;
+            _chunkWidthInVerts = Gbl.LoadContent<int>("TGen_ChunkWidthInBlocks") + 1;
             _blockWidth = Gbl.LoadContent<int>("TGen_BlockWidthInMeters");
             float lacunarity = Gbl.LoadContent<float>("TGen_Lacunarity");
             float gain = Gbl.LoadContent<float>("TGen_Gain");
@@ -30,58 +36,64 @@ namespace Gondola.GameState.Terrain{
             float hScale = Gbl.LoadContent<float>("TGen_HScale");
             float vScale = Gbl.LoadContent<float>("TGen_VScale");
 
-            CLCalc.Program.Variable constants = new CLCalc.Program.Variable(
-                new[]{
-                    lacunarity,
-                    gain,
-                    offset,
-                    octaves,
-                    hScale,
-                    vScale,
-                    _blockWidth
-                }
-                );
+            var platform = ComputePlatform.Platforms[0];
+            _devices = new List<ComputeDevice>();
+            _devices.Add(platform.Devices[0]);
 
-            _terrGeometry = new CLCalc.Program.Variable(new float[_chunkVertWidth*_chunkVertWidth*4]);
-            _terrNormals = new CLCalc.Program.Image2D(new float[_chunkVertWidth*_chunkVertWidth*4], _chunkVertWidth, _chunkVertWidth);
-            _terrBiNormals = new CLCalc.Program.Image2D(new float[_chunkVertWidth*_chunkVertWidth*4], _chunkVertWidth, _chunkVertWidth);
-            _terrTangents = new CLCalc.Program.Image2D(new float[_chunkVertWidth*_chunkVertWidth*4], _chunkVertWidth, _chunkVertWidth);
+            _properties = new ComputeContextPropertyList(platform);
+            _context = new ComputeContext(_devices, _properties, null, IntPtr.Zero);
 
-            _terrainGenArgs = new CLCalc.Program.MemoryObject[]{
-                constants,
-                null,
-                null,
-                _terrGeometry,
-                _terrNormals,
-                _terrBiNormals,
-                _terrTangents
+            _cmdQueue = new ComputeCommandQueue(_context, _devices[0], ComputeCommandQueueFlags.None);
+
+            _constants = new ComputeBuffer<float>(_context, ComputeMemoryFlags.ReadOnly, 7);
+            var constArr = new[]{
+                lacunarity,
+                gain,
+                offset,
+                octaves,
+                hScale,
+                vScale,
+                _blockWidth
             };
 
-            string genScript = Gbl.LoadScript("TGen_GenScript");
-            CLCalc.Program.Compile(new[]{genScript});
-            _terrainGenKernel = new CLCalc.Program.Kernel("GenTerrain");
+            _cmdQueue.WriteToBuffer(constArr, _constants, false, null);
 
-            _terrainGenWorkers = new[]{_chunkVertWidth, _chunkVertWidth};
+            _program = new ComputeProgram(_context, Gbl.LoadScript("TGen_GenScript"));
+            _program.Build(null, null, null, IntPtr.Zero);
+            _kernel = _program.CreateKernel("GenTerrain");
+
+            _geometry = new ComputeBuffer<float>(_context, ComputeMemoryFlags.ReadWrite, _chunkWidthInVerts*_chunkWidthInVerts*3);
+            _normals = new ComputeBuffer<byte>(_context, ComputeMemoryFlags.WriteOnly, _chunkWidthInVerts * _chunkWidthInVerts * 3);
+            _binormals = new ComputeBuffer<byte>(_context, ComputeMemoryFlags.WriteOnly, _chunkWidthInVerts * _chunkWidthInVerts * 3);
+            _tangents = new ComputeBuffer<byte>(_context, ComputeMemoryFlags.WriteOnly, _chunkWidthInVerts * _chunkWidthInVerts * 3);
+
+            _kernel.SetMemoryArgument(0, _constants);
+            _kernel.SetMemoryArgument(3, _geometry);
+            _kernel.SetMemoryArgument(4, _normals);
+            _kernel.SetMemoryArgument(5, _binormals);
+            _kernel.SetMemoryArgument(6, _tangents);
+
+            _cmdQueue.Finish();
         }
 
-        //10 millisecond overhead
         public void Generate(int offsetX, int offsetZ){
-            CLCalc.Program.Variable offsetXVar = new CLCalc.Program.Variable(new[]{offsetX});
-            CLCalc.Program.Variable offsetZVar = new CLCalc.Program.Variable(new[]{offsetZ});
+            var result1 = new float[_chunkWidthInVerts*_chunkWidthInVerts * 3];
+            var result2 = new byte[_chunkWidthInVerts * _chunkWidthInVerts * 3];
+            var result3 = new byte[_chunkWidthInVerts * _chunkWidthInVerts * 3];
+            var result4 = new byte[_chunkWidthInVerts * _chunkWidthInVerts * 3];
 
-            _terrainGenArgs[1] = offsetXVar;
-            _terrainGenArgs[2] = offsetZVar;
-            var sw = new Stopwatch();
-            sw.Start();
-            _terrainGenKernel.Execute(_terrainGenArgs, _terrainGenWorkers);
-            var v = new float[_chunkVertWidth*_chunkVertWidth*4];
-            _terrGeometry.ReadFromDeviceTo(v);
-            _terrBiNormals.ReadFromDeviceTo(v);
-            _terrNormals.ReadFromDeviceTo(v);
-            _terrTangents.ReadFromDeviceTo(v);
-            sw.Stop();
+            _kernel.SetValueArgument(1, offsetX);
+            _kernel.SetValueArgument(2, offsetZ);
 
-            double d = sw.Elapsed.Milliseconds;
+            _cmdQueue.Execute(_kernel, null, new long[]{_chunkWidthInVerts, _chunkWidthInVerts}, null, null);
+            _cmdQueue.ReadFromBuffer(_geometry, ref result1, true, null);
+            _cmdQueue.ReadFromBuffer(_normals, ref result2, true, null);
+            _cmdQueue.ReadFromBuffer(_binormals, ref result3, true, null);
+            _cmdQueue.ReadFromBuffer(_tangents, ref result4, true, null);
+
+
+            _cmdQueue.Finish();
+
             int f = 3;
         }
     }
