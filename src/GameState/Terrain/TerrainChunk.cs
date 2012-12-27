498e31125b9c81d92d148e571fedb93256fcc46e
Ben Samuels <bsamuels453@gmail.com>
2012-12-26 22:47:40 -0800

    renaming chunkdata to terrainchunk and implementing class

---
+++ src/GameState/Terrain/TerrainChunk.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Gondola.Common;
+using Gondola.Draw;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Gondola.GameState.Terrain {
+    class TerrainChunk : IDisposable{
+        public XZPair Identifier;
+
+        readonly TerrainBuffer _buffer;
+        readonly VertexPositionTexture[] _verticies;
+        readonly int[] _indicies;
+        readonly Texture2D _normals;
+        readonly Texture2D _binormals;
+        readonly Texture2D _tangents;
+
+        bool _bufferDataSet;
+
+        public TerrainChunk(XZPair identifier, VertexPositionTexture[] verticies, int[] indicies, Texture2D normals, Texture2D binormals, Texture2D tangents){
+            Identifier = identifier;
+            _verticies = verticies;
+            _indicies = indicies;
+            _normals = normals;
+            _binormals = binormals;
+            _tangents = tangents;
+            _buffer = new TerrainBuffer(indicies.Count(), verticies.Count(), indicies.Count() / 3, PrimitiveType.TriangleList);
+        }
+
+        public void SetBufferData(){
+            Debug.Assert(_bufferDataSet == false);
+            _buffer.SetData(_verticies, _indicies, _normals, _binormals, _tangents);
+            _normals.Dispose();
+            _binormals.Dispose();
+            _tangents.Dispose();
+            _bufferDataSet = true;
+        }
+
+        public void Dispose(){
+            if (!_bufferDataSet){
+                _normals.Dispose();
+                _binormals.Dispose();
+                _tangents.Dispose();
+            }
+            _buffer.Dispose();
+        }
+    }
+}
