81bad273802322eff8d07b9824e0f0a96edc973d
Ben Samuels <bsamuels453@gmail.com>
2013-02-12 13:10:40 -0800

    adding angle3 structure

---
+++ Src/Common/Angle3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gondola.Common {
+    struct Angle3 {
+        public float Pitch;
+        public float Roll;
+        public float Yaw;
+
+        public Angle3(float pitch, float roll, float yaw){
+            Pitch = pitch;
+            Roll = roll;
+            Yaw = Yaw;
+        }
+    }
+}
