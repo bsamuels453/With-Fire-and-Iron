6c8e3553e88e492df610333b1285040fc4d64426
Ben Samuels <bsamuels453@gmail.com>
2013-02-12 14:47:17 -0800

    adding a renderhelper class for common 3d functions

---
+++ Src/Common/RenderHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Gondola.Common {
+    static class RenderHelper {
+        public static Matrix CalculateViewMatrix(Vector3 position, Angle3 lookDir){
+            Matrix rotation = Matrix.CreateFromYawPitchRoll(lookDir.Yaw, -lookDir.Pitch, 0);
+            Vector3 transformedReference = Vector3.Transform(new Vector3(0,0,1), rotation);
+            Vector3 cameraDirection = position + transformedReference;
+            return Matrix.CreateLookAt(position, cameraDirection, Vector3.Up);
+        }
+    }
+}
