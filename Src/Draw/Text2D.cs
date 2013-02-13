2e79f1589a75625a9c8aebf8a84daeeb9f17e05d
Ben Samuels <bsamuels453@gmail.com>
2013-02-12 21:14:36 -0800

    adding color selection to Tex2D

--- Src/Draw/Text2D.cs
+++ Src/Draw/Text2D.cs
@@ -13,10 +13,12 @@ namespace Gondola.Draw{
         public Vector2 Position;
         public string Str;
 
+        public Color Color;
+
         public Text2D(RenderTarget target, int x, int y, string str, string font = "UI_SpriteFont"){
             Position = new Vector2(x, y);
             Str = str;
-
+            Color = Color.Black;
             _spriteBatch = target.SpriteBatch;
             _font = Gbl.ContentManager.Load<SpriteFont>(Gbl.RawLookup[font]);
         }
@@ -28,7 +30,7 @@ namespace Gondola.Draw{
                 _font,
                 Str,
                 Position,
-                Color.Black
+                Color
                 );
         }
 
