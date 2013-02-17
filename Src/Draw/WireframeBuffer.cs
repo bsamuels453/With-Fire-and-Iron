36c1cc7d88e2b0bdf711d4decb5521ee9267abb3
Ben Samuels <bsamuels453@gmail.com>
2013-02-17 12:13:58 -0800

    implementing wireframe buffer

--- Src/Draw/WireframeBuffer.cs
+++ Src/Draw/WireframeBuffer.cs
@@ -8,15 +8,23 @@ using Microsoft.Xna.Framework.Graphics;
 #endregion
 
 namespace Gondola.Draw{
-    internal class WireframeBuffer : BaseBufferObject<VertexPositionColor>{
+    internal class WireframeBuffer : BaseBufferObject<VertexPositionTexture>{
         public WireframeBuffer(int numIndicies, int numVerticies, int numPrimitives)
             : base(numIndicies, numVerticies, numPrimitives, PrimitiveType.LineList){
             BufferRasterizer = new RasterizerState();
             BufferRasterizer.CullMode = CullMode.None;
-            BufferEffect = Gbl.ContentManager.Load<Effect>(Gbl.RawLookup["WireframeEffect"]).Clone();
+            BufferEffect = Gbl.ContentManager.Load<Effect>(Gbl.RawLookup["Std_WireframeShader"]);
 
             BufferEffect.Parameters["Projection"].SetValue(Gbl.ProjectionMatrix);
             BufferEffect.Parameters["World"].SetValue(Matrix.Identity);
+        }
+
+        public void SetData(
+            VertexPositionTexture[] verticies,
+            int[] indicies
+            ) {
+            Indexbuffer.SetData(indicies);
+            Vertexbuffer.SetData(verticies);
         }
     }
 }
