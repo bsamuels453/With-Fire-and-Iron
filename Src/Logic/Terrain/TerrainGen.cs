885df8a847e338a0f7977515b00d1e9cc97267fa
Ben Samuels <bsamuels453@gmail.com>
2013-02-17 18:29:49 -0800

    cleaning up generation function by splitting into subfuncs

--- Src/Logic/Terrain/TerrainGen.cs
+++ Src/Logic/Terrain/TerrainGen.cs
@@ -42,7 +42,7 @@ namespace Gondola.Logic.Terrain{
 
         readonly ComputeProgram _winderPrgm;
         readonly ComputeKernel _winderKernel;
-        readonly ComputeBuffer<int> _indicies; 
+        readonly ComputeBuffer<int> _indicies;
 
         readonly ComputeContext _context;
         readonly List<ComputeDevice> _devices;
@@ -84,10 +84,10 @@ namespace Gondola.Logic.Terrain{
             };
 
             _cmdQueue.WriteToBuffer(genArr, _genConstants, false, null);
-            if (loadFromSource) {
+            if (loadFromSource){
                 _generationPrgm = new ComputeProgram(_context, Gbl.LoadScript("TGen_Generator"));
                 //_generationPrgm.Build(null, "", null, IntPtr.Zero);//use option -I + scriptDir for header search
-                _generationPrgm.Build(null, @"-g -s D:\Projects\Gondola\Scripts\GenTerrain.cl", null, IntPtr.Zero);//use option -I + scriptDir for header search
+                _generationPrgm.Build(null, @"-g -s D:\Projects\Gondola\Scripts\GenTerrain.cl", null, IntPtr.Zero); //use option -I + scriptDir for header search
                 Gbl.SaveBinary(_generationPrgm.Binaries, "TGen_Generator");
             }
             else{
@@ -96,18 +96,18 @@ namespace Gondola.Logic.Terrain{
                 _generationPrgm.Build(null, "", null, IntPtr.Zero);
             }
             //loadFromSource = false;
-            
+
             _terrainGenKernel = _generationPrgm.CreateKernel("GenTerrain");
             _normalGenKernel = _generationPrgm.CreateKernel("GenNormals");
 
 
             //despite the script using float3 for these fields, we need to consider it to be float4 because the 
             //implementation is basically a float4 wrapper that uses zero for the last variable
-            _geometry = new ComputeBuffer<float>(_context, ComputeMemoryFlags.None, _chunkWidthInVerts * _chunkWidthInVerts * 4);
-            _normals = new ComputeBuffer<byte>(_context, ComputeMemoryFlags.None, _chunkWidthInVerts * _chunkWidthInVerts * 4);
-            _binormals = new ComputeBuffer<byte>(_context, ComputeMemoryFlags.None, _chunkWidthInVerts * _chunkWidthInVerts * 4);
-            _tangents = new ComputeBuffer<byte>(_context, ComputeMemoryFlags.None, _chunkWidthInVerts * _chunkWidthInVerts * 4);
-            _uvCoords = new ComputeBuffer<float>(_context, ComputeMemoryFlags.None, _chunkWidthInVerts * _chunkWidthInVerts * 2);
+            _geometry = new ComputeBuffer<float>(_context, ComputeMemoryFlags.None, _chunkWidthInVerts*_chunkWidthInVerts*4);
+            _normals = new ComputeBuffer<byte>(_context, ComputeMemoryFlags.None, _chunkWidthInVerts*_chunkWidthInVerts*4);
+            _binormals = new ComputeBuffer<byte>(_context, ComputeMemoryFlags.None, _chunkWidthInVerts*_chunkWidthInVerts*4);
+            _tangents = new ComputeBuffer<byte>(_context, ComputeMemoryFlags.None, _chunkWidthInVerts*_chunkWidthInVerts*4);
+            _uvCoords = new ComputeBuffer<float>(_context, ComputeMemoryFlags.None, _chunkWidthInVerts*_chunkWidthInVerts*2);
 
             _terrainGenKernel.SetMemoryArgument(0, _genConstants);
             _terrainGenKernel.SetMemoryArgument(3, _geometry);
@@ -124,13 +124,13 @@ namespace Gondola.Logic.Terrain{
 
             #region setup quadtree kernel
 
-            if (loadFromSource) {
+            if (loadFromSource){
                 _qTreePrgm = new ComputeProgram(_context, Gbl.LoadScript("TGen_QTree"));
                 _qTreePrgm.Build(null, @"-g -s D:\Projects\Gondola\Scripts\Quadtree.cl", null, IntPtr.Zero);
                 //_qTreePrgm.Build(null, "", null, IntPtr.Zero);
                 Gbl.SaveBinary(_qTreePrgm.Binaries, "TGen_QTree");
             }
-            else {
+            else{
                 var binary = Gbl.LoadBinary("TGen_QTree");
                 _qTreePrgm = new ComputeProgram(_context, binary, _devices);
                 _qTreePrgm.Build(null, "", null, IntPtr.Zero);
@@ -139,11 +139,11 @@ namespace Gondola.Logic.Terrain{
             _qTreeKernel = _qTreePrgm.CreateKernel("QuadTree");
             _crossCullKernel = _qTreePrgm.CreateKernel("CrossCull");
 
-            _activeVerts = new ComputeBuffer<byte>(_context, ComputeMemoryFlags.None, _chunkWidthInVerts * _chunkWidthInVerts);
+            _activeVerts = new ComputeBuffer<byte>(_context, ComputeMemoryFlags.None, _chunkWidthInVerts*_chunkWidthInVerts);
 
             _dummy = new ComputeBuffer<int>(_context, ComputeMemoryFlags.None, 50);
             var rawNormals = new byte[_chunkWidthInVerts*_chunkWidthInVerts*4];
-            var dsada = new byte[_chunkWidthInVerts * _chunkWidthInVerts];
+            var dsada = new byte[_chunkWidthInVerts*_chunkWidthInVerts];
             for (int i = 0; i < dsada.Length; i++){
                 dsada[i] = 1;
             }
@@ -164,21 +164,21 @@ namespace Gondola.Logic.Terrain{
 
             #region setup winding kernel
 
-            if (loadFromSource) {
+            if (loadFromSource){
                 _winderPrgm = new ComputeProgram(_context, Gbl.LoadScript("TGen_VertexWinder"));
                 //_winderPrgm.Build(null, "", null, IntPtr.Zero);
                 _winderPrgm.Build(null, @"-g -s D:\Projects\Gondola\Scripts\VertexWinder.cl", null, IntPtr.Zero);
                 Gbl.SaveBinary(_winderPrgm.Binaries, "TGen_VertexWinder");
             }
-            else {
+            else{
                 var binary = Gbl.LoadBinary("TGen_VertexWinder");
                 _winderPrgm = new ComputeProgram(_context, binary, _devices);
                 _winderPrgm.Build(null, "", null, IntPtr.Zero);
             }
-            
+
             _winderKernel = _winderPrgm.CreateKernel("VertexWinder");
 
-            _indicies = new ComputeBuffer<int>(_context, ComputeMemoryFlags.None, (_chunkWidthInBlocks) * (_chunkWidthInBlocks) * 8);
+            _indicies = new ComputeBuffer<int>(_context, ComputeMemoryFlags.None, (_chunkWidthInBlocks)*(_chunkWidthInBlocks)*8);
 
             _winderKernel.SetMemoryArgument(0, _activeVerts);
             _winderKernel.SetMemoryArgument(1, _indicies);
@@ -192,19 +192,19 @@ namespace Gondola.Logic.Terrain{
             _cmdQueue.Finish();
         }
 
-        public TerrainChunk GenerateChunk(XZPair id) {
+        public TerrainChunk GenerateChunk(XZPair id){
             var sw = new Stopwatch();
             sw.Start();
 
-            int offsetX = id.X * _blockWidth * (_chunkWidthInBlocks);
-            int offsetZ = id.Z * _blockWidth * (_chunkWidthInBlocks);
+            int offsetX = id.X*_blockWidth*(_chunkWidthInBlocks);
+            int offsetZ = id.Z*_blockWidth*(_chunkWidthInBlocks);
 
             var rawGeometry = new float[_chunkWidthInVerts*_chunkWidthInVerts*4];
             var rawNormals = new byte[_chunkWidthInVerts*_chunkWidthInVerts*4];
             var rawBinormals = new byte[_chunkWidthInVerts*_chunkWidthInVerts*4];
             var rawTangents = new byte[_chunkWidthInVerts*_chunkWidthInVerts*4];
-            var rawUVCoords = new float[_chunkWidthInVerts * _chunkWidthInVerts * 2];
-            var indicies = new int[(_chunkWidthInBlocks) * (_chunkWidthInBlocks) * 8];
+            var rawUVCoords = new float[_chunkWidthInVerts*_chunkWidthInVerts*2];
+            var indicies = new int[(_chunkWidthInBlocks)*(_chunkWidthInBlocks)*8];
             var activeVerts = new byte[_chunkWidthInVerts*_chunkWidthInVerts];
 
             _terrainGenKernel.SetValueArgument(1, offsetX);
@@ -212,19 +212,19 @@ namespace Gondola.Logic.Terrain{
             _normalGenKernel.SetValueArgument(1, offsetX);
             _normalGenKernel.SetValueArgument(2, offsetZ);
 
-            _cmdQueue.Execute(_terrainGenKernel, null, new long[] { _chunkWidthInVerts, _chunkWidthInVerts }, null, null);
-            _cmdQueue.Execute(_normalGenKernel, null, new long[] { _chunkWidthInVerts, _chunkWidthInVerts }, null, null);
+            _cmdQueue.Execute(_terrainGenKernel, null, new long[]{_chunkWidthInVerts, _chunkWidthInVerts}, null, null);
+            _cmdQueue.Execute(_normalGenKernel, null, new long[]{_chunkWidthInVerts, _chunkWidthInVerts}, null, null);
 
-            for (int depth = 0; depth <5; depth++){
+            for (int depth = 0; depth < 6; depth++){
                 _qTreeKernel.SetValueArgument(0, depth);
                 _crossCullKernel.SetValueArgument(0, depth);
-                int cellWidth = (int)Math.Pow(2, depth) * 2;
+                int cellWidth = (int) Math.Pow(2, depth)*2;
                 //int cellWidth = depth * 2 + 2;
-                int qTreeWidth = _chunkWidthInBlocks / (cellWidth);
-                _cmdQueue.Execute(_qTreeKernel, null, new long[] { (qTreeWidth)-1, (qTreeWidth*2) }, null, null);
-                _cmdQueue.Execute(_crossCullKernel, null, new long[] { _chunkWidthInBlocks / cellWidth, _chunkWidthInBlocks / cellWidth }, null, null);
+                int qTreeWidth = _chunkWidthInBlocks/(cellWidth);
+                _cmdQueue.Execute(_qTreeKernel, null, new long[]{(qTreeWidth) - 1, (qTreeWidth*2)}, null, null);
+                _cmdQueue.Execute(_crossCullKernel, null, new long[]{_chunkWidthInBlocks/cellWidth, _chunkWidthInBlocks/cellWidth}, null, null);
             }
-            _cmdQueue.Execute(_winderKernel, null, new long[] { (_chunkWidthInBlocks), (_chunkWidthInBlocks * 2) }, null, null);
+            _cmdQueue.Execute(_winderKernel, null, new long[]{(_chunkWidthInBlocks), (_chunkWidthInBlocks*2)}, null, null);
 
             _cmdQueue.ReadFromBuffer(_geometry, ref rawGeometry, true, null);
             _cmdQueue.ReadFromBuffer(_normals, ref rawNormals, true, null);
@@ -241,13 +241,13 @@ namespace Gondola.Logic.Terrain{
             var texBinormal = new Texture2D(Gbl.Device, _chunkWidthInVerts, _chunkWidthInVerts, false, SurfaceFormat.Color);
             var texTangent = new Texture2D(Gbl.Device, _chunkWidthInVerts, _chunkWidthInVerts, false, SurfaceFormat.Color);
 
-            for (int v = 3; v < rawNormals.Length; v+=4){
+            for (int v = 3; v < rawNormals.Length; v += 4){
                 rawNormals[v] = 1;
             }
-            for (int v = 3; v < rawBinormals.Length; v += 4) {
+            for (int v = 3; v < rawBinormals.Length; v += 4){
                 rawBinormals[v] = 1;
             }
-            for (int v = 3; v < rawTangents.Length; v += 4) {
+            for (int v = 3; v < rawTangents.Length; v += 4){
                 rawTangents[v] = 1;
             }
 
@@ -255,60 +255,140 @@ namespace Gondola.Logic.Terrain{
             texBinormal.SetData(rawBinormals);
             texTangent.SetData(rawTangents);
 
-            var verts = new Vector3[_chunkWidthInVerts*_chunkWidthInVerts];
-            var uv = new Vector2[_chunkWidthInBlocks*_chunkWidthInBlocks*2];
-
-            int si = 0;
-            for (int i = 0; i < _chunkWidthInVerts * _chunkWidthInVerts; i++){
-                verts[i] = new Vector3(rawGeometry[si], rawGeometry[si + 1], rawGeometry[si + 2]);
-                si += 4;
-            }
-
             //var maxHeight = verts.Aggregate((agg, next) => next.Y > agg.Y ? next : agg).Y;
             //var minHeight = verts.Aggregate((agg, next) => next.Y < agg.Y ? next : agg).Y;
 
-            si = 0;
-            for (int v = 0; v < _chunkWidthInVerts * _chunkWidthInVerts; v++) {
-                uv[v]= new Vector2(rawUVCoords[si],rawUVCoords[si+1]);
-                si += 2;
-            }
-            var fixedInds = new List<int>();
-            for(int idx=0; idx<indicies.Length; idx+=4){
-                if (indicies[idx] == 0 && indicies[idx + 1] == 0 && indicies[idx + 2] == 0 && indicies[idx + 3] == 0) {
-                    continue;
-                }
-                //if (indicies[idx] < 0)
-                    //continue;
-                fixedInds.Add(indicies[idx]);
-                fixedInds.Add(indicies[idx+1]);
-                fixedInds.Add(indicies[idx+2]);
-            }
+            //these functions take the raw buffer data from opencl and remove stride and empty fields
+            var parsedUV = ParseUV(rawUVCoords);
+            var parsedGeometry = ParseGeometry(rawGeometry);
+            var parsedIndicies = ParseIndicies(indicies);
 
-           var vertexList = new VertexPositionTexture[_chunkWidthInVerts * _chunkWidthInVerts];
+            int[] culledIndexes;
+            VertexPositionTexture[] culledVertexes;
 
-           var li = new List<int>();
-            for(int i=0; i<fixedInds.Count; i++){
-                if (fixedInds[i] == 2)
-                    li.Add(i);
-            }
+            CullVertexes(parsedIndicies.ToList(), parsedGeometry, parsedUV, out culledIndexes, out culledVertexes);
 
-
-            for (int i = 0; i < _chunkWidthInVerts * _chunkWidthInVerts; i++){
-                vertexList[i] = new VertexPositionTexture(verts[i],uv[i] );
-            }
-
-            var chunkData = new TerrainChunk(id, vertexList, fixedInds.ToArray(), texNormal, texBinormal, texTangent);
+            var chunkData = new TerrainChunk(id, culledVertexes, culledIndexes, texNormal, texBinormal, texTangent);
 
             sw.Stop();
             double elapsed = sw.ElapsedMilliseconds;
             return chunkData;
         }
 
+        Vector2[] ParseUV(float[] rawVertexes){
+            int destIdx = 0;
+            var outVertexes = new Vector2[rawVertexes.Length/2];
+            for (
+                int srcIdx = 0;
+                srcIdx < rawVertexes.Length;
+                srcIdx
+                += 2){
+                outVertexes[destIdx] = new Vector2(
+                    rawVertexes[srcIdx],
+                    rawVertexes[srcIdx + 1]);
+
+                destIdx++;
+            }
+            return outVertexes;
+        }
+
+        Vector3[] ParseGeometry(float[] rawVertexes){
+            int destIdx = 0;
+            var outVertexes = new Vector3[rawVertexes.Length/4];
+            for (
+                int srcIdx = 0;
+                srcIdx < rawVertexes.Length;
+                srcIdx
+                += 4){
+                outVertexes[destIdx] = new Vector3(
+                    rawVertexes[srcIdx],
+                    rawVertexes[srcIdx + 1],
+                    rawVertexes[srcIdx + 2]);
+
+                destIdx++;
+            }
+            return
+
+                outVertexes;
+        }
+
+        /*
+        //IM HAVING FUN AND THERES NOTHING YOU CAN DO TO STOP ME
+        T[] ParseStridedData<T, S>(S[] src, int stride, int numDummyElems){
+            Type outType = typeof (T);
+            int destIdx = 0;
+            var dataOut = new T[src.Length/stride];
+            var ctorTypes = new Type[stride - numDummyElems];
+            for (int i = 0; i < stride - numDummyElems; i++)
+                ctorTypes[i] = typeof(S);
+
+            var ctor = outType.GetConstructor(ctorTypes);
+            for (int srcIdx = 0; srcIdx < src.Length; srcIdx += stride){
+                object[] ctorArgs = new object[stride - numDummyElems];
+                for (int i = 0; i < stride - numDummyElems; i++){
+                    ctorArgs[i] = src[srcIdx + i];
+                }
+                dataOut[destIdx] = (T) ctor.Invoke(ctorArgs);
+                destIdx++;
+            }
+            return dataOut;
+        }
+        */
+
+        List<int> ParseIndicies(int[] indicies){
+            var outIndicies = new List<int>();
+
+            for (int i = 0; i < indicies.Length; i+=4){
+                if (indicies[i] == 0 && indicies[i + 1] == 0 && indicies[i + 2] == 0){
+                    continue;
+                }
+                outIndicies.Add(indicies[i]);
+                outIndicies.Add(indicies[i + 1]);
+                outIndicies.Add(indicies[i + 2]);
+            }
+
+            return outIndicies;
+        }
+
+        void CullVertexes(
+            List<int> indicies,
+            Vector3[] geometry,
+            Vector2[] uvCoords,
+            out int[] reparsedIndicies,
+            out VertexPositionTexture[] oVertexes){
+
+            //first get a list of numeric indicies that will be used by the index buffer
+            //there will be missing indexes because of the culling
+            var indiciesInUse = new List<int>();
+            foreach (var indice in indicies){
+                if (!indiciesInUse.Contains(indice)){
+                    indiciesInUse.Add(indice);
+                }
+            }
+
+            //now use those indexes to cull vertexes that aren't referenced by the vbo.
+            //in the process, the index associated with each vertex changes, and the new 
+            //indexes are stored in reparsedIndicies
+            var vertexList = new List<VertexPositionTexture>();
+            reparsedIndicies = new int[indicies.Count];
+            foreach (var uniqueIdx in indiciesInUse) {
+                vertexList.Add(new VertexPositionTexture(geometry[uniqueIdx], uvCoords[uniqueIdx]));
+                int newIDx = vertexList.Count - 1;
+                int idx;
+                while ((idx = indicies.IndexOf(uniqueIdx)) != -1) {
+                    reparsedIndicies[idx] = newIDx;
+                    indicies[idx] = -1;
+                }
+            }
+
+            oVertexes = vertexList.ToArray();
+        }
+
         void DumpArray(byte[] array, int width){
             var sw = new StreamWriter("dump.txt");
             for (int x = 0; x < width; x++){
                 for (int z = 0; z < width; z++){
-                    sw.Write(array[x + z * width]+" ");
+                    sw.Write(array[x + z*width] + " ");
                 }
                 sw.Write('\n');
             }
