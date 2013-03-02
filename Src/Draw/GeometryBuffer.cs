6db0d9264b5cc0dc6c67d4dcf482f1663927cb36
Ben Samuels <bsamuels453@gmail.com>
2013-03-01 18:46:30 -0800

    adding rotation/translation to GeometryBuffer

--- Src/Draw/GeometryBuffer.cs
+++ Src/Draw/GeometryBuffer.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Gondola.Util;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Gondola.Draw {
     internal class GeometryBuffer<T> : BaseGeometryBuffer<T> where T : struct {
+        Vector3 _rotation;
+        Vector3 _translation;
+
         public GeometryBuffer(
             int numIndicies,
             int numVerticies,
@@ -15,6 +20,8 @@ namespace Gondola.Draw {
             CullMode cullMode = CullMode.None
             )
             : base(numIndicies, numVerticies, numPrimitives, settingsFileName, primitiveType, cullMode) {
+                _rotation = new Vector3();
+                _translation = new Vector3();
 
         }
 
@@ -40,6 +47,20 @@ namespace Gondola.Draw {
             int[] data = new int[BaseIndexBuffer.IndexCount];
             base.BaseIndexBuffer.GetData(data);
             return data;
+        }
+
+        public void Translate(Vector3 diff){
+            _translation += diff;
+            BaseWorldMatrix = Matrix.Identity;
+            BaseWorldMatrix *= Matrix.CreateRotationX(_rotation.X) * Matrix.CreateRotationY(_rotation.Y) * Matrix.CreateRotationZ(_rotation.Z);
+            BaseWorldMatrix *= Matrix.CreateTranslation(_translation.X, _translation.Y, _translation.Z);
+        }
+
+        public void Rotate(Angle3 diff) {
+            _rotation += diff.ToVec();
+            BaseWorldMatrix = Matrix.Identity;
+            BaseWorldMatrix *= Matrix.CreateRotationX(_rotation.X) * Matrix.CreateRotationY(_rotation.Y) * Matrix.CreateRotationZ(_rotation.Z);
+            BaseWorldMatrix *= Matrix.CreateTranslation(_translation.X, _translation.Y, _translation.Z);
         }
     }
 }
--- Src/Util/Angle3.cs
+++ Src/Util/Angle3.cs
@@ -1,4 +1,6 @@
-﻿namespace Gondola.Util {
+﻿using Microsoft.Xna.Framework;
+
+namespace Gondola.Util {
     struct Angle3 {
         public float Pitch;
         public float Roll;
@@ -8,6 +10,16 @@
             Pitch = pitch;
             Roll = roll;
             Yaw = yaw;
+        }
+
+        public Vector3 NormalizeToVec(){
+            float sum = Pitch + Roll + Yaw;
+            return new Vector3(Pitch / sum, Roll / sum, Yaw / sum);
+        }
+
+        public Vector3 ToVec(){
+            //all object start facing down x axis
+            return new Vector3(Roll, Yaw, Pitch);
         }
     }
 }
