dc5705a2f75860ee6428a50f2667d9d4b7df32fa
Ben Samuels <bsamuels453@gmail.com>
2013-03-03 14:58:15 -0800

    implementing cutting proto

--- Src/GameState/ObjectEditor/HullMesh.cs
+++ Src/GameState/ObjectEditor/HullMesh.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using Gondola.Draw;
 using Gondola.Util;
@@ -14,14 +15,10 @@ namespace Gondola.GameState.ObjectEditor{
     /// <summary>
     ///   This class handles the meshes that make up the hull on each deck of the airship. This class is used for projecting shapes into the mesh to allow for objects such as portholes. Each deck's hull is broken in two parts split down the center.
     /// </summary>
-    internal class HullMesh : IEnumerable {
+    internal class HullMesh : IEnumerable{
         readonly ObjectBuffer<SectionIdentifier> _structureBuffer;
         ObjectBuffer<SectionIdentifier> _fillBuffer;
-        Vector3[][] _structureVerts;
-
-        public CullMode CullMode{
-            set { _structureBuffer.CullMode = value; }
-        }
+        Vector3[][] _structureVerts;//necessary?
 
         public HullMesh(int layersPerDeck, int[] indicies, VertexPositionNormalTexture[] verts){
             _structureVerts = new Vector3[layersPerDeck][];
@@ -47,26 +44,39 @@ namespace Gondola.GameState.ObjectEditor{
             }
         }
 
-        public void Cut(Vector3 v) {
+        public CullMode CullMode{
+            set { _structureBuffer.CullMode = value; }
+        }
+
+        #region IEnumerable Members
+
+        public IEnumerator GetEnumerator(){
+            return _structureBuffer.GetEnumerator();
+        }
+
+        #endregion
+
+        public void Cut(Vector3 v){
             foreach (SectionIdentifier section in _structureBuffer){
                 if (section.ContainsPoint(new Vector2(v.X, v.Y)))
                     _structureBuffer.DisableObject(section);
             }
         }
 
+        #region Nested type: SectionIdentifier
 
         public class SectionIdentifier : IEquatable<SectionIdentifier>{
-            readonly Vector3[] _upperPts;
+            readonly OverlapState _farOverlap;
             readonly Vector3[] _lowerPts;
 
             readonly OverlapState _nearOverlap;
-            readonly OverlapState _farOverlap;
+            readonly Vector3[] _upperPts;
 
-            public SectionIdentifier(VertexPositionNormalTexture[] verts) {
+            public SectionIdentifier(VertexPositionNormalTexture[] verts){
                 _upperPts = new Vector3[2];
                 _lowerPts = new Vector3[2];
 
-                if (verts[0].Position.X < verts[1].Position.X) {
+                if (verts[0].Position.X < verts[1].Position.X){
                     _upperPts[0] = verts[0].Position;
                     _upperPts[1] = verts[1].Position;
                 }
@@ -74,7 +84,7 @@ namespace Gondola.GameState.ObjectEditor{
                     _upperPts[0] = verts[1].Position;
                     _upperPts[1] = verts[0].Position;
                 }
-                if (verts[2].Position.X < verts[3].Position.X) {
+                if (verts[2].Position.X < verts[3].Position.X){
                     _lowerPts[0] = verts[2].Position;
                     _lowerPts[1] = verts[3].Position;
                 }
@@ -100,12 +110,24 @@ namespace Gondola.GameState.ObjectEditor{
                 //int f = 4;
             }
 
+            #region IEquatable<SectionIdentifier> Members
+
+            public bool Equals(SectionIdentifier other){
+                if (_lowerPts.SequenceEqual(other._lowerPts) && _upperPts.SequenceEqual(other._upperPts)){
+                    return true;
+                }
+                return false;
+            }
+
+            #endregion
+
             public bool ContainsPoint(Vector2 p1){
-                //assume this is correct
+                /*
                 if (p1.Y > _upperPts[0].Y)
                     return false;
                 if (p1.Y < _lowerPts[0].Y)
                     return false;
+                */
 
                 if (p1.X < _lowerPts[0].X && p1.X < _upperPts[0].X)
                     return false;
@@ -116,69 +138,156 @@ namespace Gondola.GameState.ObjectEditor{
                 //at this point we know that the segment is within the orthogonal projection of this section
 
                 if (p1.X > _lowerPts[0].X && p1.X > _upperPts[0].X && p1.X < _lowerPts[1].X && p1.X < _upperPts[1].X)
-                    return true;//it's within the inner bounds
+                    return true; //it's within the inner bounds
 
                 var nearSample = DeterminePointPos(_lowerPts[0], _upperPts[0], p1, _nearOverlap);
                 var farSample = DeterminePointPos(_lowerPts[1], _upperPts[1], p1, _farOverlap);
-                /*
-                bool ok = false;
-                if (_nearOverlap == OverlapState.Hanging) {
-                    if (nearSample == Side.LeftSide) {
-                        ok = false;
-                    }
-                }
-                else{
-                    if (nearSample == Side.RightSide) {
-                        ok = false;
-                    }
-                }
-                */
 
                 if (nearSample == Side.RightSide && farSample == Side.LeftSide)
                     return true;
                 return false;
             }
 
-            enum OverlapState{
-                Hanging,
-                Building
-            }
-
-            enum Side{
-                LeftSide,
-                RightSide
-            }
-
             Side DeterminePointPos(Vector3 v1, Vector3 v2, Vector2 p1, OverlapState state){
                 var linePt = Lerp.Trace3X(v1, v2, p1.X);
                 switch (state){
                     case OverlapState.Building:
-                        if(linePt.Y < p1.Y)
+                        if (linePt.Y < p1.Y)
                             return Side.RightSide;
                         return Side.LeftSide;
                     case OverlapState.Hanging:
-                        if(linePt.Y < p1.Y)
+                        if (linePt.Y < p1.Y)
                             return Side.RightSide;
                         return Side.LeftSide;
                 }
                 throw new Exception();
             }
 
-            public void CutRectangle(BoundingBox box){
+            public void CutRectangle(BoundingBox box, out VertexPositionNormalTexture[] verts, out int[] inds){
+                var indsLi = new List<int>();
+                var vertsLi = new List<VertexPositionNormalTexture>();
+                Vector2 max = new Vector2(box.Max.Y, box.Max.Y);
+                Vector2 min = new Vector2(box.Min.X, box.Min.Y);
+                float sliceMin, sliceMax;
+                sliceMin = min.X;
+                sliceMax  = max.X;
 
-            }
-
-            public bool Equals(SectionIdentifier other){
-                if (_lowerPts.SequenceEqual(other._lowerPts) && _upperPts.SequenceEqual(other._upperPts)){
-                    return true;
+                //starting @ min
+                float progressive = 0;
+                //left to right
+                if (sliceMin < _lowerPts[0].X && sliceMin < _upperPts[0].X){
+                    //we know sliceMin is out of the picture
+                    if (sliceMax > _lowerPts[0].X && sliceMax < _upperPts[0].X ||
+                        sliceMax < _lowerPts[0].X && sliceMax > _upperPts[0].X){
+                        //near end clipped off
+                        //generate intermediate
+                        progressive = _lowerPts[0].X > _upperPts[0].X ? _lowerPts[0].X : _upperPts[0].X;
+                        //return
+                    }
+                    if (sliceMax > _lowerPts[0].X && sliceMax > _upperPts[0].X &&
+                        sliceMax < _lowerPts[1].X && sliceMax < _upperPts[1].X) {
+                        //near half clipped off
+                        //generate intermediate
+                        progressive = sliceMax;
+                        //return
+                    }
+                    if (sliceMax > _lowerPts[1].X && sliceMax < _upperPts[1].X ||
+                        sliceMax < _lowerPts[1].X && sliceMax > _upperPts[1].X){
+                        //near half engulfed
+                        progressive = sliceMax;
+                        //return
+                    }
+                    //entire engulfed?
+                    //return
                 }
-                return false;
+
+                //opposite render direction
+                //right to left
+                if (sliceMax < _lowerPts[1].X && sliceMax < _upperPts[1].X){
+                    //we know sliceMax is out of the picture
+                    if (sliceMin > _lowerPts[1].X && sliceMin < _upperPts[1].X ||
+                        sliceMin < _lowerPts[1].X && sliceMin > _upperPts[1].X) {
+                        //far end clipped off
+                        //generate intermediate
+                        progressive = _lowerPts[1].X < _upperPts[1].X ? _lowerPts[1].X : _upperPts[1].X;
+                        //return
+                    }
+                    if (sliceMin > _lowerPts[0].X && sliceMin > _upperPts[0].X &&
+                        sliceMin < _lowerPts[1].X && sliceMin < _upperPts[1].X) {
+                        //far half clipped off
+                        //generate intermediate
+                        progressive = sliceMin;
+                        //return
+                    }
+                    if (sliceMin > _lowerPts[0].X && sliceMin < _upperPts[0].X ||
+                        sliceMin < _lowerPts[0].X && sliceMin > _upperPts[0].X) {
+                        //far half engulfed
+                        progressive = sliceMin;
+                        //return
+                    }
+                    //entire engulfed?
+                    //return
+                }
+
+                //now the center outlier
+                if (sliceMin > _lowerPts[0].X && sliceMin < _upperPts[0].X ||
+                    sliceMin < _lowerPts[0].X && sliceMin > _upperPts[0].X){
+                    //slicemin is between near
+
+                    if(sliceMax < _lowerPts[1].X && sliceMax < _upperPts[1].X){
+                        //cut from between near to middle
+                        //return
+                    }
+
+                    if (sliceMax > _lowerPts[1].X && sliceMax < _upperPts[1].X ||
+                        sliceMax < _lowerPts[1].X && sliceMax > _upperPts[1].X){
+                        //cut from between near to between far
+                        //return
+                    }
+                    //return
+                }
+
+                if (sliceMax > _lowerPts[1].X && sliceMax < _upperPts[1].X ||
+                    sliceMax < _lowerPts[1].X && sliceMax > _upperPts[1].X){
+                    //slicemax is between far
+
+                    if (sliceMin > _lowerPts[0].X && sliceMin > _upperPts[0].X){
+                        //cut from between far to middle
+                        //return
+                    }
+                    //dont need cut between near and between far because covered above
+                    //return
+                }
+
+                if (sliceMin > _lowerPts[0].X && sliceMin > _upperPts[0].X &&
+                    sliceMax < _lowerPts[1].X && sliceMax < _upperPts[1].X){
+                    //center cut
+                }
+
+
+                verts = null;
+                inds = null;
             }
 
+            #region Nested type: OverlapState
+
+            enum OverlapState{
+                Hanging,
+                Building
+            }
+
+            #endregion
+
+            #region Nested type: Side
+
+            enum Side{
+                LeftSide,
+                RightSide
+            }
+
+            #endregion
         }
 
-        public IEnumerator GetEnumerator(){
-            return _structureBuffer.GetEnumerator();
-        }
+        #endregion
     }
 }
