a5ccbbeda0f3b685e849ded52600ed0f52e627e3
Ben Samuels <bsamuels453@gmail.com>
2013-03-14 11:38:13 -0700

    removing Text2D and replacing with the DebugText helper class

---
+++ Forge.Framework/Draw/DebugText.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Forge.Framework.UI;
+using Forge.Framework.UI.Widgets;
+using Microsoft.Xna.Framework;
+
+namespace Forge.Framework.Draw {
+    public static class DebugText {
+        static readonly Dictionary<string, TextBox> _text;
+
+        static DebugText(){
+            _text = new Dictionary<string, TextBox>();
+        }
+
+        static public void CreateText(string id, int x, int y){
+            _text.Add(id, new TextBox(x,y,DepthLevel.High, Color.Black));
+        }
+
+        static public void SetText(string id, string text){
+            _text[id].SetText(text);
+        }
+    }
+}
