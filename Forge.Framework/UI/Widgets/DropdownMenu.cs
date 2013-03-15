97b64d4006758d8a95be79d24c08337d7ed2305a
Ben Samuels <bsamuels453@gmail.com>
2013-03-14 20:47:39 -0700

    partial implementation of dropdown menu

---
+++ Forge.Framework/UI/Widgets/DropdownMenu.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Forge.Framework.UI.Widgets {
+    public class DropdownMenu : IUIElementBase{
+        public event Action<string> OnOptionSelected;
+
+        //readonly Button _dropdownBG;
+        readonly Button _dropdownButton;
+        readonly Button _dropdownTopElem;
+        readonly TextBox _selectedText;
+
+        readonly List<Button> _dropdownHighlights;
+        readonly List<TextBox> _dropdownText;
+
+        readonly string[] _options;
+
+        public DropdownMenu(int x, int y, string startingOption, string[] options){
+            const int width = 100;
+            const int baseHeight = 16;
+            const int menuWidth = width - baseHeight;
+            const string dropdownButtonTex = "Icons/DownArrow";
+            const string dropdownParentButTex = "Materials/DarkTextBoxMid";
+            const string dropdownBGTex = "Materials/TextBoxMid";
+            _options = options;
+
+            #region generate top
+
+            var buttonGen = new ButtonGenerator();
+            buttonGen.Depth = DepthLevel.Medium;
+
+            buttonGen.TextureName = dropdownBGTex;
+            buttonGen.X = x;
+            buttonGen.Y = y + baseHeight;
+            buttonGen.Width = menuWidth;
+            buttonGen.Height = baseHeight*options.Length;
+            //_dropdownBG = buttonGen.GenerateButton();
+            //_dropdownBG.Alpha = 0.5f;
+
+            buttonGen.X = x;
+            buttonGen.Y = y;
+            buttonGen.Height = baseHeight;
+            buttonGen.TextureName = dropdownParentButTex;
+            _dropdownTopElem = buttonGen.GenerateButton();
+
+            buttonGen.X = x + menuWidth;
+            buttonGen.TextureName = dropdownButtonTex;
+            buttonGen.Width = baseHeight;
+            _dropdownButton = buttonGen.GenerateButton();
+
+            _dropdownTopElem.OnLeftPressDispatcher += identifier => ShowSubmenu();
+            _dropdownButton.OnLeftPressDispatcher += identifier => ShowSubmenu();
+
+            buttonGen = new ButtonGenerator("ToolbarButton45.json");
+
+            buttonGen.Height = baseHeight;
+            buttonGen.Depth = DepthLevel.High;
+            buttonGen.Width = menuWidth;
+            buttonGen.TextureName = dropdownBGTex;
+            buttonGen.X = x;
+
+            _selectedText = new TextBox(
+                x + 1,
+                y + 1,
+                DepthLevel.Highlight,
+                Color.Black
+                );
+            _selectedText.SetText(startingOption);
+
+            #endregion
+
+            #region generate dropdown
+
+            int yPos = y + baseHeight;
+            _dropdownText = new List<TextBox>(options.Length);
+            _dropdownHighlights = new List<Button>(options.Length);
+            int id = 0;
+            foreach (var option in options){
+                _dropdownText.Add(new TextBox(
+                    x + 1,
+                    yPos + 1,
+                    DepthLevel.Highlight,
+                    Color.Black
+                    ));
+                _dropdownText.Last().SetText(option);
+
+                buttonGen.Identifier = id;
+                buttonGen.Y = yPos;
+                buttonGen.Identifier = id;
+                _dropdownHighlights.Add(
+                    buttonGen.GenerateButton());
+
+                _dropdownHighlights.Last().OnLeftClickDispatcher += OnSelectionMade;
+                _dropdownHighlights.Last().Enabled = false;
+                _dropdownText.Last().Enabled = false;
+                yPos += baseHeight;
+                id++;
+            }
+
+            #endregion
+
+            UIElementCollection.BoundCollection.AddElement(this);
+        }
+
+        void OnSelectionMade(int identifier){
+            HideSubmenu();
+            _selectedText.SetText(_options[identifier]);
+
+            if (OnOptionSelected != null){
+                OnOptionSelected.Invoke(_options[identifier]);
+            }
+        }
+
+        void ShowSubmenu(){
+
+        }
+
+        void HideSubmenu(){
+
+        }
+
+        public void UpdateLogic(double timeDelta){
+            throw new NotImplementedException();
+        }
+
+        public void UpdateInput(ref InputState state){
+            throw new NotImplementedException();
+        }
+
+        public float X{
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public float Y{
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public float Width{
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public float Height{
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public float Alpha{
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public float Depth{
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public bool HitTest(int x, int y){
+            throw new NotImplementedException();
+        }
+
+        public List<IUIElementBase> GetElementStack(int x, int y){
+            throw new NotImplementedException();
+        }
+    }
+}
