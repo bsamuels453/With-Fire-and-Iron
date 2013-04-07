06ac675a8ff89f874128e2952bb38c066ad26ce7
Ben Samuels <bsamuels453@gmail.com>
2013-04-06 17:21:07 -0700

    adding two new fields to hullsection

--- Forge.Core/Airship/HullSection.cs
+++ Forge.Core/Airship/HullSection.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace Forge.Core.Airship {
     public class HullSection : IEquatable<HullSection> {
         readonly float _xStart;
         readonly float _xEnd;
         readonly int _yPanel;
+        readonly Vector3 _centroid;//notimpl
+        readonly float _width;//notimpl
 
         public HullSection(float xStart, float xEnd, int yPanel) {
+            //throw new NotImplementedException();
             _xStart = xStart;
             _xEnd = xEnd;
             _yPanel = yPanel;
