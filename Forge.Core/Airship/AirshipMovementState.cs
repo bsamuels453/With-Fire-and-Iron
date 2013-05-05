e90a736fe88ab124a6dcd6cf2d70e4809da49fbd
Ben Samuels <bsamuels453@gmail.com>
2013-05-05 14:29:40 -0700

    splitting two data structures out of airship controller

---
+++ Forge.Core/Airship/AirshipMovementState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Forge.Core.Airship {
+    public struct AirshipMovementState {
+        public Vector3 CurPosition;
+        public Vector3 Angle;
+
+        public float CurVelocity;
+        public float CurAltitudeVelocity;
+
+        public float AngleTarget;
+        public float VelocityTarget;
+        public float AltitudeTarget;
+    }
+}
---
+++ Forge.Core/Airship/ModelAttributes.cs
@@ -0,0 +1,14 @@
+﻿namespace Forge.Core.Airship{
+    public struct ModelAttributes {
+        public float Length;
+        public float MaxAscentSpeed;
+        public float MaxForwardSpeed;
+        public float MaxReverseSpeed;
+        public float MaxTurnSpeed;
+
+        //public float AscentAcceleration;
+        //public float TurnAcceleration;
+        //public float EngineAcceleration;
+
+    }
+}
