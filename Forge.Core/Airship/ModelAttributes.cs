20cc52fb457e1d1ef18c6d20cbc49640bde6497d
Ben Samuels <bsamuels453@gmail.com>
2013-05-05 20:07:41 -0700

    adding a few more ModelAttributes fields

--- Forge.Core/Airship/ModelAttributes.cs
+++ Forge.Core/Airship/ModelAttributes.cs
@@ -1,6 +1,11 @@
-﻿namespace Forge.Core.Airship{
+﻿using Microsoft.Xna.Framework;
+
+namespace Forge.Core.Airship{
     public struct ModelAttributes {
+        public int NumDecks;
+        public Vector3 Centroid;
         public float Length;
+        public float Berth;
         public float MaxAscentSpeed;
         public float MaxForwardSpeed;
         public float MaxReverseSpeed;
