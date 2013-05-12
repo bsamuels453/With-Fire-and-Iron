de412a723cbdc78218be3e48c0f44c3a97736059
Ben Samuels <bsamuels453@gmail.com>
2013-05-11 17:53:29 -0700

    implemented second half of new decal generator

--- Forge.Core/ObjectEditor/HullMesh.cs
+++ Forge.Core/ObjectEditor/HullMesh.cs
@@ -149,11 +149,16 @@ namespace Forge.Core.ObjectEditor{
 
             while (subBoxBeginX < maxX){
 
-                var sortedTris = SortTriangles(groupedTris, subBoxBeginX, subBoxEndX);
+                var sortedTris = CullTriangles(groupedTris, subBoxBeginX, subBoxEndX);
 
                 //handling zero enclosed verts cases
                 foreach (var triangle in sortedTris[0]){
                     SliceZeroEnclosureTriangle(buff, triangle, subBoxBeginX, subBoxEndX);
+                }
+
+                //handling case where one vert is enclosed within the box
+                foreach (var triangle in sortedTris[1]) {
+                    SliceSingleEnclosureTriangle(buff, triangle, subBoxBeginX, subBoxEndX);
                 }
 
                 subBoxBeginX += _boxWidth;
@@ -164,10 +169,175 @@ namespace Forge.Core.ObjectEditor{
             int f = 4;
         }
 
-        static List<VertexPositionNormalTexture[]>[] SortTriangles(
+        void SliceZeroEnclosureTriangle(ObjectBuffer<HullSection> buff, VertexPositionNormalTexture[] triangle, float subBoxBegin, float subBoxEnd){
+            //first have to figure out the "anchor vertex"
+            VertexPositionNormalTexture anchor;
+            VertexPositionNormalTexture[] satellites;
+            float anchorBoundary, satelliteBoundary;
+
+            var side1 = from vert in triangle
+                where vert.Position.X >= subBoxEnd
+                select vert;
+            var side2 = from vert in triangle
+                where vert.Position.X <= subBoxBegin
+                select vert;
+            // ReSharper disable PossibleMultipleEnumeration db query too lightweight to care
+            if (side1.Count() == 1){
+                anchor = side1.Single();
+                satellites = side2.ToArray();
+                anchorBoundary = subBoxEnd;
+                satelliteBoundary = subBoxBegin;
+            }
+            else{
+                anchor = side2.Single();
+                satellites = side1.ToArray();
+                anchorBoundary = subBoxBegin;
+                satelliteBoundary = subBoxEnd;
+            }
+            // ReSharper restore PossibleMultipleEnumeration
+
+            if (satellites[0].Position.Y < satellites[1].Position.Y){
+                var temp = satellites[0];
+                satellites[0] = satellites[1];
+                satellites[1] = temp;
+            }
+
+            var s1Pos = Lerp.Trace3X(satellites[0].Position, anchor.Position, satelliteBoundary);
+            var s2Pos = Lerp.Trace3X(satellites[1].Position, anchor.Position, satelliteBoundary);
+
+            var a1Pos = Lerp.Trace3X(satellites[0].Position, anchor.Position, anchorBoundary);
+            var a2Pos = Lerp.Trace3X(satellites[1].Position, anchor.Position, anchorBoundary);
+
+            var s1Norm = InterpolateNorm(satellites[0].Normal, anchor.Normal, satellites[0].Position, anchor.Position, s1Pos);
+            var s2Norm = InterpolateNorm(satellites[1].Normal, anchor.Normal, satellites[1].Position, anchor.Position, s2Pos);
+
+            var a1Norm = InterpolateNorm(satellites[0].Normal, anchor.Normal, satellites[0].Position, anchor.Position, a1Pos);
+            var a2Norm = InterpolateNorm(satellites[1].Normal, anchor.Normal, satellites[1].Position, anchor.Position, a2Pos);
+
+            var s1UV = InterpolateUV(satellites[0].TextureCoordinate, anchor.TextureCoordinate, satellites[0].Position, anchor.Position, s1Pos);
+            var s2UV = InterpolateUV(satellites[1].TextureCoordinate, anchor.TextureCoordinate, satellites[1].Position, anchor.Position, s2Pos);
+
+            var a1UV = InterpolateUV(satellites[0].TextureCoordinate, anchor.TextureCoordinate, satellites[0].Position, anchor.Position, a1Pos);
+            var a2UV = InterpolateUV(satellites[1].TextureCoordinate, anchor.TextureCoordinate, satellites[1].Position, anchor.Position, a2Pos);
+
+            HullSection id = new HullSection(subBoxBegin, subBoxEnd, 2);
+            var s1 = new VertexPositionNormalTexture(s1Pos, s1Norm, s1UV);
+            var s2 = new VertexPositionNormalTexture(s2Pos, s2Norm, s2UV);
+
+            var a1 = new VertexPositionNormalTexture(a1Pos, a1Norm, a1UV);
+            var a2 = new VertexPositionNormalTexture(a2Pos, a2Norm, a2UV);
+            
+            var t1 = new[] { s1, a1, s2 };
+            var t2 = new[] { a2, s2, a1 };
+
+
+            var t1I = GenerateIndiceList(t1);
+            var t2I = GenerateIndiceList(t2);
+
+            buff.AddObject(id, t1I, t1);
+            buff.AddObject(id, t2I, t2);
+        }
+
+        void SliceSingleEnclosureTriangle(ObjectBuffer<HullSection> buff, VertexPositionNormalTexture[] triangle, float subBoxBegin, float subBoxEnd){
+            var leftSide = (from vert in triangle
+                            where vert.Position.X >= subBoxEnd
+                            select vert).ToArray();
+            var rightSide = (from vert in triangle
+                             where vert.Position.X <= subBoxBegin
+                             select vert).ToArray();
+            var middle = (from vert in triangle
+                          where vert.Position.X > subBoxBegin && vert.Position.X < subBoxEnd
+                          select vert).Single();
+
+            var id = new HullSection(subBoxBegin, subBoxEnd, 2);
+            if (leftSide.Length == 2 || leftSide.Length == 0) {
+                VertexPositionNormalTexture[] anchorVerts;
+                float interpLimit;
+
+                if (leftSide.Length == 2) {
+                    //handle case with two verts on left, one in mid, zero on right
+                    anchorVerts = leftSide;
+                    interpLimit = subBoxEnd;
+                }
+                else{
+                    //handle case with two verts on right, one in mid, zero on left
+                    anchorVerts = rightSide;
+                    interpLimit = subBoxBegin;
+                }
+                var interpPos1 = Lerp.Trace3X(anchorVerts[0].Position, middle.Position, interpLimit);
+                var interpPos2 = Lerp.Trace3X(anchorVerts[1].Position, middle.Position, interpLimit);
+
+                var interpNorm1 = InterpolateNorm(anchorVerts[0].Normal, middle.Normal, anchorVerts[0].Position, middle.Position, interpPos1);
+                var interpNorm2 = InterpolateNorm(anchorVerts[1].Normal, middle.Normal, anchorVerts[1].Position, middle.Position, interpPos2);
+
+                var interpUV1 = InterpolateUV(anchorVerts[0].TextureCoordinate, middle.TextureCoordinate, anchorVerts[0].Position, middle.Position, interpPos1);
+                var interpUV2 = InterpolateUV(anchorVerts[1].TextureCoordinate, middle.TextureCoordinate, anchorVerts[1].Position, middle.Position, interpPos2);
+
+                var interpVert1 = new VertexPositionNormalTexture(interpPos1, interpNorm1, interpUV1);
+                var interpVert2 = new VertexPositionNormalTexture(interpPos2, interpNorm2, interpUV2);
+
+                var t1 = new[] { interpVert1, middle, interpVert2 };
+                var t1I = GenerateIndiceList(t1);
+                buff.AddObject(id, t1I, t1);
+                 
+            }
+            else{
+                var leftVert = leftSide.Single();
+                var rightVert = rightSide.Single();
+
+                var l1Pos = Lerp.Trace3X(leftVert.Position, middle.Position, subBoxEnd);
+                var l2Pos = Lerp.Trace3X(leftVert.Position, rightVert.Position, subBoxEnd);
+
+                var r1Pos = Lerp.Trace3X(rightVert.Position, middle.Position, subBoxBegin);
+                var r2Pos = Lerp.Trace3X(rightVert.Position, leftVert.Position, subBoxBegin);
+
+                var s1Norm = InterpolateNorm(leftVert.Normal, middle.Normal, leftVert.Position, middle.Position, l1Pos);
+                var s2Norm = InterpolateNorm(leftVert.Normal, rightVert.Normal, leftVert.Position, rightVert.Position, l2Pos);
+
+                var a1Norm = InterpolateNorm(rightVert.Normal, middle.Normal, rightVert.Position, middle.Position, r1Pos);
+                var a2Norm = InterpolateNorm(rightVert.Normal, leftVert.Normal, rightVert.Position, leftVert.Position, r2Pos);
+
+                var s1UV = InterpolateUV(leftVert.TextureCoordinate, middle.TextureCoordinate, leftVert.Position, middle.Position, l1Pos);
+                var s2UV = InterpolateUV(leftVert.TextureCoordinate, rightVert.TextureCoordinate, leftVert.Position, rightVert.Position, l2Pos);
+
+                var a1UV = InterpolateUV(rightVert.TextureCoordinate, middle.TextureCoordinate, rightVert.Position, middle.Position, r1Pos);
+                var a2UV = InterpolateUV(rightVert.TextureCoordinate, leftVert.TextureCoordinate, rightVert.Position, leftVert.Position, r2Pos);
+
+                var l1 = new VertexPositionNormalTexture(l1Pos, s1Norm, s1UV);
+                var l2 = new VertexPositionNormalTexture(l2Pos, s2Norm, s2UV);
+
+                var r1 = new VertexPositionNormalTexture(r1Pos, a1Norm, a1UV);
+                var r2 = new VertexPositionNormalTexture(r2Pos, a2Norm, a2UV);
+
+                var t1 = new[] { r1, r2, middle };
+                var t2 = new[] { l1, l2, middle };
+
+                //we want to use the two edge vertexes that are furthest from the middle
+                var t3V1 = Vector3.Distance(l1.Position, middle.Position) < Vector3.Distance(l2.Position, middle.Position) ? l2 : l1;
+                var t3V2 = Vector3.Distance(r1.Position, middle.Position) < Vector3.Distance(r2.Position, middle.Position) ? r2 : r1;
+                var t3 = new[] { t3V1, t3V2, middle };
+
+                var t1I = GenerateIndiceList(t1);
+                var t2I = GenerateIndiceList(t2);
+                var t3I = GenerateIndiceList(t3);
+
+                buff.AddObject(id, t1I, t1);
+                buff.AddObject(id, t2I, t2);
+                buff.AddObject(id, t3I, t3);
+            }
+        }
+
+        /// <summary>
+        /// Culls the triangles that don't cross the x-region defined by subboxbegin and subboxend
+        /// </summary>
+        /// <param name="groupedTriangles"></param>
+        /// <param name="subBoxBegin"></param>
+        /// <param name="subBoxEnd"></param>
+        /// <returns></returns>
+        static List<VertexPositionNormalTexture[]>[] CullTriangles(
             VertexPositionNormalTexture[][] groupedTriangles,
             float subBoxBegin,
-            float subBoxEnd){
+            float subBoxEnd) {
 
             var retList = new List<VertexPositionNormalTexture[]>[3];
 
@@ -176,7 +346,7 @@ namespace Forge.Core.ObjectEditor{
                 (triangle, start, end) => {
                     int tot = 0;
                     foreach (var vert in triangle) {
-                        if (vert.Position.X <= end && vert.Position.X >= start)
+                        if (vert.Position.X < end && vert.Position.X > start)
                             tot++;
                     }
                     return tot;
@@ -216,79 +386,8 @@ namespace Forge.Core.ObjectEditor{
             return retList;
         }
 
-        void SliceZeroEnclosureTriangle(ObjectBuffer<HullSection> buff, VertexPositionNormalTexture[] triangle, float subBoxBegin, float subBoxEnd){
-            //first have to figure out the "anchor vertex"
-            VertexPositionNormalTexture anchor;
-            VertexPositionNormalTexture[] satellites;
-            float anchorBoundary, satelliteBoundary;
-
-            ////if (triangle[0].Position.X > 3)
-            //    return;
-
-            var side1 = from vert in triangle
-                where vert.Position.X > subBoxEnd
-                select vert;
-            var side2 = from vert in triangle
-                where vert.Position.X < subBoxBegin
-                select vert;
-            // ReSharper disable PossibleMultipleEnumeration db query too lightweight to care
-            if (side1.Count() == 1){
-                anchor = side1.Single();
-                satellites = side2.ToArray();
-                anchorBoundary = subBoxEnd;
-                satelliteBoundary = subBoxBegin;
-            }
-            else{
-                anchor = side2.Single();
-                satellites = side1.ToArray();
-                anchorBoundary = subBoxBegin;
-                satelliteBoundary = subBoxEnd;
-            }
-            // ReSharper restore PossibleMultipleEnumeration
-
-            if (satellites[0].Position.Y < satellites[1].Position.Y){
-                var temp = satellites[0];
-                satellites[0] = satellites[1];
-                satellites[1] = temp;
-            }
-
-            var s1Pos = Lerp.Trace3X(satellites[0].Position, anchor.Position, satelliteBoundary);//idx=satt[0].val
-            var s2Pos = Lerp.Trace3X(satellites[1].Position, anchor.Position, satelliteBoundary);//idx=satt[1].val
-
-            var a1Pos = Lerp.Trace3X(satellites[0].Position, anchor.Position, anchorBoundary);//idx=anch.val
-            var a2Pos = Lerp.Trace3X(satellites[1].Position, anchor.Position, anchorBoundary);//idx=anch.val
-
-            var s1Norm = InterpolateNorm(satellites[0].Normal, anchor.Normal, satellites[0].Position, anchor.Position, s1Pos);
-            var s2Norm = InterpolateNorm(satellites[1].Normal, anchor.Normal, satellites[1].Position, anchor.Position, s2Pos);
-
-            var a1Norm = InterpolateNorm(satellites[0].Normal, anchor.Normal, satellites[0].Position, anchor.Position, a1Pos);
-            var a2Norm = InterpolateNorm(satellites[1].Normal, anchor.Normal, satellites[1].Position, anchor.Position, a2Pos);
-
-            var s1UV = InterpolateUV(satellites[0].TextureCoordinate, anchor.TextureCoordinate, satellites[0].Position, anchor.Position, s1Pos);
-            var s2UV = InterpolateUV(satellites[1].TextureCoordinate, anchor.TextureCoordinate, satellites[1].Position, anchor.Position, s2Pos);
-
-            var a1UV = InterpolateUV(satellites[0].TextureCoordinate, anchor.TextureCoordinate, satellites[0].Position, anchor.Position, a1Pos);
-            var a2UV = InterpolateUV(satellites[1].TextureCoordinate, anchor.TextureCoordinate, satellites[1].Position, anchor.Position, a2Pos);
-
-            HullSection id = new HullSection(subBoxBegin, subBoxEnd, 2);
-            var s1 = new VertexPositionNormalTexture(s1Pos, s1Norm, s1UV);
-            var s2 = new VertexPositionNormalTexture(s2Pos, s2Norm, s2UV);
-
-            var a1 = new VertexPositionNormalTexture(a1Pos, a1Norm, a1UV);
-            var a2 = new VertexPositionNormalTexture(a2Pos, a2Norm, a2UV);
-            
-            var t1 = new[] { s1, a1, s2 };
-            var t2 = new[] { a2, s2, a1 };
-
-
-            var t1I = GenerateIndiceList(t1);
-            var t2I = GenerateIndiceList(t2);
-
-            buff.AddObject(id, t1I, t1);
-            buff.AddObject(id, t2I, t2);
-        }
-
         int[] GenerateIndiceList(VertexPositionNormalTexture[] verts) {
+            Debug.Assert(verts != null);
             var cross = Vector3.Cross(verts[1].Position - verts[0].Position, verts[2].Position - verts[0].Position);
 
             switch (_side) {
@@ -307,15 +406,14 @@ namespace Forge.Core.ObjectEditor{
             return null;
         }
 
-
-        Vector3 InterpolateNorm(Vector3 n1, Vector3 n2, Vector3 p1, Vector3 p2, Vector3 mid){
+        static Vector3 InterpolateNorm(Vector3 n1, Vector3 n2, Vector3 p1, Vector3 p2, Vector3 mid){
             float d1 = Vector3.Distance(p1, p2);
             float d2 = Vector3.Distance(p1, mid);
             float t = d2 / d1;
             return n1 * (1-t) + n2 * (t);
         }
 
-        Vector2 InterpolateUV(Vector2 u1, Vector2 u2, Vector3 p1, Vector3 p2, Vector3 mid){
+        static Vector2 InterpolateUV(Vector2 u1, Vector2 u2, Vector3 p1, Vector3 p2, Vector3 mid){
             float d1 = Vector3.Distance(p1, p2);
             float d2 = Vector3.Distance(p1, mid);
             float t = d2 / d1;
