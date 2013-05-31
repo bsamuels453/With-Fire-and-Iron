0a06a1f120d916398d46d63172e7c6785d440613
Ben Samuels <bsamuels453@gmail.com>
2013-05-31 16:02:30 -0700

    adding proto buff attributes

--- Forge.Core/Airship/Data/ModelAttributes.cs
+++ Forge.Core/Airship/Data/ModelAttributes.cs
@@ -1,14 +1,24 @@
 ﻿using MonoGameUtility;
+using ProtoBuf;
 
 namespace Forge.Core.Airship.Data{
+    [ProtoContract]
     public struct ModelAttributes {
+        [ProtoMember(1)]
         public int NumDecks;
+        [ProtoMember(2)]
         public Vector3 Centroid;
+        [ProtoMember(3)]
         public float Length;
+        [ProtoMember(4)]
         public float Berth;
+        [ProtoMember(5)]
         public float MaxAscentSpeed;
+        [ProtoMember(6)]
         public float MaxForwardSpeed;
+        [ProtoMember(7)]
         public float MaxReverseSpeed;
+        [ProtoMember(8)]
         public float MaxTurnSpeed;
 
         //public float AscentAcceleration;
