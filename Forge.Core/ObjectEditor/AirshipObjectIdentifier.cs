fcac2d5162ee6919edba751dceb6cffe8b961a99
Ben Samuels <bsamuels453@gmail.com>
2013-05-31 14:58:22 -0700

    adding protobuff attributes

--- Forge.Core/ObjectEditor/AirshipObjectIdentifier.cs
+++ Forge.Core/ObjectEditor/AirshipObjectIdentifier.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MonoGameUtility;
+using ProtoBuf;
 
 namespace Forge.Core.Logic {
+    [ProtoContract]
     internal class AirshipObjectIdentifier : IEquatable<AirshipObjectIdentifier> {
+        [ProtoMember(1)]
         public readonly ObjectType ObjectType;
+        [ProtoMember(2)]
         public readonly Vector3 Position;
 
         public AirshipObjectIdentifier(ObjectType objectType, Vector3 position) {
