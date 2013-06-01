23b67b0e5cbd61dc3f757e9e672100cec085e74f
Ben Samuels <bsamuels453@gmail.com>
2013-05-31 19:19:30 -0700

    adding ai controller class

---
+++ Forge.Core/Airship/AIAirshipController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Forge.Core.Airship.Data;
+using Forge.Framework;
+using MonoGameUtility;
+
+namespace Forge.Core.Airship {
+    class AIAirshipController : AirshipController{
+        public AIAirshipController(Action<Matrix> setWorldMatrix, ModelAttributes modelData, AirshipMovementData movementData, List<Hardpoint> hardPoints) : 
+            base(setWorldMatrix, modelData, movementData, hardPoints){
+
+
+
+        }
+
+        protected override void UpdateController(ref InputState state, double timeDelta){
+
+
+        }
+    }
+}
