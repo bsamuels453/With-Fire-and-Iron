acf6088de9d765f4b162472637ee178b5aab1470
Ben Samuels <bsamuels453@gmail.com>
2013-06-03 12:11:53 -0700

    implementing buff support

---
+++ Forge.Core/Airship/Data/AirshipEnums.cs
@@ -0,0 +1,19 @@
+﻿namespace Forge.Core.Airship.Data{
+    /// <summary>
+    ///   Defines the different kinds of maneuvers the autopilot can perform.
+    /// </summary>
+    public enum ManeuverTypeEnum{
+        None,
+        Orbit,
+        Approach,
+        Follow
+    }
+
+    /// <summary>
+    ///   Defines what kind of input device will control the airship.
+    /// </summary>
+    public enum AirshipControllerType{
+        Player,
+        AI
+    }
+}
