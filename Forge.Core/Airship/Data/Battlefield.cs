d65648eab2f6cc4b206ad7833330d0b41ba8f0f4
Ben Samuels <bsamuels453@gmail.com>
2013-06-07 10:52:04 -0700

    adding battlefield class that will handle all battlefield related things

---
+++ Forge.Core/Airship/Data/Battlefield.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Forge.Core.Physics;
+using Forge.Framework;
+
+namespace Forge.Core.Airship.Data {
+    /// <summary>
+    /// This class contains all of the information relevant to the current battlefield, for use by the airship class' AI, physics engine, etc.
+    /// </summary>
+    class Battlefield : IDisposable {
+        public readonly AirshipIndexer ShipsOnField;
+        public readonly ProjectilePhysics ProjectileEngine;
+
+        public Battlefield(){
+            ShipsOnField = new AirshipIndexer();
+            ProjectileEngine = new ProjectilePhysics();
+        }
+
+        bool _disposed;
+
+        public void Dispose(){
+            Debug.Assert(!_disposed);
+            ProjectileEngine.Dispose();
+            foreach (Airship airship in ShipsOnField){
+                airship.Dispose();
+            }
+            _disposed = true;
+        }
+
+        public void Update(ref InputState state, double timeDelta){
+            foreach (Airship airship in ShipsOnField){
+                airship.Update(ref state, timeDelta);
+            }
+            ProjectileEngine.Update(timeDelta);
+
+        }
+
+        ~Battlefield(){
+            Debug.Assert(_disposed);
+        }
+    }
+}
