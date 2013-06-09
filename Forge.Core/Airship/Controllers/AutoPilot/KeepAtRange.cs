f440ebe16c4daaae146244c42029fde685b1825c
Ben Samuels <bsamuels453@gmail.com>
2013-06-09 11:06:10 -0700

    implementing CalculateNextPosition

--- Forge.Core/Airship/Controllers/AutoPilot/KeepAtRange.cs
+++ Forge.Core/Airship/Controllers/AutoPilot/KeepAtRange.cs
@@ -1,31 +1,44 @@
-﻿#region
-
-using System;
-using System.Diagnostics;
-using Forge.Framework;
-using MonoGameUtility;
-
-#endregion
-
-namespace Forge.Core.Airship.Controllers.AutoPilot{
+﻿namespace Forge.Core.Airship.Controllers.AutoPilot{
     internal class KeepAtRange : AirshipAutoPilot{
+        readonly AirshipIndexer _airships;
+        readonly AirshipController _controller;
         readonly float _preferredRange;
         readonly Airship _targetAirship;
 
-        public KeepAtRange(int targetUid, float range, AirshipController controller, AirshipIndexer airships) : base(controller, airships){
+        public KeepAtRange(int targetUid, float range, AirshipController controller, AirshipIndexer airships){
             _preferredRange = range;
-            _targetAirship = airships[targetUid];
+            _targetAirship = _airships[targetUid];
+            _controller = controller;
+            _airships = airships;
         }
 
-        protected override void UpdateChild(double timeDelta){
+        public override Pathfinder.RetAttributes CalculateNextPosition(double timeDelta){
             //get target position
+            var diff = _controller.Position - _targetAirship.Position;
+            diff.Normalize();
+            var targetPos = diff*_preferredRange;
 
             //figure out if we should go forwards or backwards
+            bool useReverse = Pathfinder.ShouldReverseBeUsed
+                (
+                    _controller.Position,
+                    targetPos,
+                    _controller.Angle.Y,
+                    _controller.MaxTurnRate,
+                    _controller.MaxTurnAcceleration,
+                    _controller.MaxVelocity,
+                    _controller.MaxReverseVelocity,
+                    _controller.MaxAcceleration
+                );
 
-            //apply the change
-
-            throw new NotImplementedException();
+            var ret = Pathfinder.CalculateAirshipPath
+                (
+                    targetPos,
+                    _controller,
+                    (float) timeDelta,
+                    useReverse
+                );
+            return ret;
         }
-
     }
 }
