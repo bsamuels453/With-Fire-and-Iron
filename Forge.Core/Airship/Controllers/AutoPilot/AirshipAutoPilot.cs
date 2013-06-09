69595f27e7899fcf2d37e1aa3cc7db5f3d98b0d0
Ben Samuels <bsamuels453@gmail.com>
2013-06-09 11:05:40 -0700

    modifying autopilot interface to only have the one abstract updater method

--- Forge.Core/Airship/Controllers/AutoPilot/AirshipAutoPilot.cs
+++ Forge.Core/Airship/Controllers/AutoPilot/AirshipAutoPilot.cs
@@ -1,17 +1,5 @@
 ﻿namespace Forge.Core.Airship.Controllers.AutoPilot{
     internal abstract class AirshipAutoPilot{
-        protected readonly AirshipController Controller;
-        protected readonly AirshipIndexer Indexer;
-
-        protected AirshipAutoPilot(AirshipController controller, AirshipIndexer airships){
-            Controller = controller;
-            Indexer = airships;
-        }
-
-        public void Update(double timeDelta){
-            UpdateChild(timeDelta);
-        }
-
-        protected abstract void UpdateChild(double timeDelta);
+        public abstract Pathfinder.RetAttributes CalculateNextPosition(double timeDelta);
     }
 }
