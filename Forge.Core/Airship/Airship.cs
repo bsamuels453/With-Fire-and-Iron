29f77019fc520af428c67b114069593d9dc41397
Ben Samuels <bsamuels453@gmail.com>
2013-06-09 17:29:01 -0700

    propagating a change to airship controller parameters

--- Forge.Core/Airship/Airship.cs
+++ Forge.Core/Airship/Airship.cs
@@ -4,9 +4,11 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using Forge.Core.Airship.Controllers;
+using Forge.Core.Airship.Controllers.AutoPilot;
 using Forge.Core.Airship.Data;
 using Forge.Core.Physics;
 using Forge.Framework;
+using Forge.Framework.Draw;
 using MonoGameUtility;
 
 #endregion
@@ -48,7 +50,8 @@ namespace Forge.Core.Airship{
                         (
                         ModelAttributes,
                         stateData,
-                        _hardPoints
+                        _hardPoints,
+                        _battlefield.ShipsOnField
                         );
                     break;
 
@@ -57,34 +60,36 @@ namespace Forge.Core.Airship{
                         (
                         ModelAttributes,
                         stateData,
-                        _hardPoints
+                        _hardPoints,
+                        _battlefield.ShipsOnField
                         );
                     break;
             }
 
             _hullIntegrityMesh = new HullIntegrityMesh(HullSectionContainer, _battlefield.ProjectileEngine, _controller.Position, ModelAttributes.Length);
 
+            DebugText.CreateText("x:", 0, 0);
+            DebugText.CreateText("y:", 0, 15);
+            DebugText.CreateText("z:", 0, 30);
+
             sw.Stop();
 
             DebugConsole.WriteLine("Airship class assembled in " + sw.ElapsedMilliseconds + " ms");
         }
 
+        public AirshipStateData StateData{
+            get { return _controller.StateData; }
+        }
+
         public ModelAttributes ModelAttributes { get; private set; }
+
+        public ModelAttributes BuffedModelAttributes{
+            get { return _controller.GetBuffedAttributes(); }
+        }
 
         //public Vector3 Centroid { get; private set; }
         public HullSectionContainer HullSectionContainer { get; private set; }
         public DeckSectionContainer DeckSectionContainer { get; private set; }
-
-        /// <summary>
-        ///   Reflects the current position of the airship, as measured from its center.
-        /// </summary>
-        public Vector3 Position{
-            get { return _controller.Position; }
-        }
-
-        public float Velocity{
-            get { return _controller.Velocity; }
-        }
 
         #region IDisposable Members
 
@@ -103,7 +108,16 @@ namespace Forge.Core.Airship{
 
         #endregion
 
+        public void SetAutoPilot(AirshipAutoPilot autoPilot){
+            _controller.SetAutoPilot(autoPilot);
+        }
+
         public void Update(ref InputState state, double timeDelta){
+            DebugText.SetText("x:", "x:" + _controller.StateData.Position.X);
+            DebugText.SetText("y:", "y:" + _controller.StateData.Position.Y);
+            DebugText.SetText("z:", "z:" + _controller.StateData.Position.Z);
+
+
             _controller.Update(ref state, timeDelta);
             SetAirshipWMatrix(_controller.WorldMatrix);
 
