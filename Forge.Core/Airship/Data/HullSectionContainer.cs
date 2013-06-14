bd3fd5ce9b46c50b08592453a3d0d8b8bdf3c08a
Ben Samuels <bsamuels453@gmail.com>
2013-06-14 14:01:58 -0700

    changing _hullTextureUVMult field to _hullLength, and completing implementation of the decal applier

--- Forge.Core/Airship/Data/HullSectionContainer.cs
+++ Forge.Core/Airship/Data/HullSectionContainer.cs
@@ -15,10 +15,10 @@ using ProtoBuf;
 
 namespace Forge.Core.Airship.Data{
     public class HullSectionContainer : IEnumerable, IDisposable{
-        const int _decalSizeMultiplier = 16;
+        const int _decalSizeMultiplier = 32;
         bool _disposed;
+        float _hullLength;
         HullSection[] _hullSections;
-        float _hullTextureUVMult;
         TextureBlitter _textureBlitter;
         float _textureToDecalTexel;
 
@@ -50,7 +50,7 @@ namespace Forge.Core.Airship.Data{
                     width,
                     height,
                     texToDecal,
-                    hullTextureUVMult
+                    modelAttributes.Length
                 );
         }
 
@@ -71,7 +71,7 @@ namespace Forge.Core.Airship.Data{
                     serializedStruct.DecalWidth,
                     serializedStruct.DecalHeight,
                     serializedStruct.TextureToDecalTexel,
-                    serializedStruct.HullTextureUVMult
+                    serializedStruct.HullLength
                 );
         }
 
@@ -82,10 +82,10 @@ namespace Forge.Core.Airship.Data{
             int decalWidth,
             int decalHeight,
             float textureToDecal,
-            float hullTextureUVMult
+            float hullLength
             ){
             _hullSections = hullSections;
-            _hullTextureUVMult = hullTextureUVMult;
+            _hullLength = hullLength;
             _textureToDecalTexel = textureToDecal;
             TopExpIdx = 0;
             HullBuffersByDeck = hullBuffersByDeck;
@@ -150,7 +150,8 @@ namespace Forge.Core.Airship.Data{
         /// The Y axis goes from the top deck down to the keel of the ship.
         /// </param>
         public void AddDamageDecal(Vector2 position){
-            //need to convert position from model space to texel space
+            //need to convert position from model space to decal texture space
+            position = (position/_hullLength)*_textureBlitter.TargWidth;
 
             _textureBlitter.BlitLocations.Add(position);
             UpdateDecalTexture(_textureBlitter.GetBlittedTexture());
@@ -196,7 +197,7 @@ namespace Forge.Core.Airship.Data{
                 _textureBlitter.TargWidth,
                 _textureBlitter.TargHeight,
                 _textureToDecalTexel,
-                _hullTextureUVMult
+                _hullLength
                 );
             return ret;
         }
@@ -207,8 +208,8 @@ namespace Forge.Core.Airship.Data{
             [ProtoMember(2)] public readonly int DecalHeight;
             [ProtoMember(3)] public readonly int DecalWidth;
             [ProtoMember(4)] public readonly ObjectBuffer<int>.Serialized[] HullBuffersByDeck;
+            [ProtoMember(8)] public readonly float HullLength;
             [ProtoMember(5)] public readonly HullSection[] HullSections;
-            [ProtoMember(8)] public readonly float HullTextureUVMult;
             [ProtoMember(6)] public readonly int NumDecks;
             [ProtoMember(7)] public readonly float TextureToDecalTexel;
 
@@ -219,7 +220,7 @@ namespace Forge.Core.Airship.Data{
                 List<Vector2> damageDecalPositions,
                 int decalWidth,
                 int decalHeight,
-                float textureToDecalTexel, float hullTextureUVMult){
+                float textureToDecalTexel, float hullLength){
                 NumDecks = numDecks;
                 HullSections = hullSections;
                 HullBuffersByDeck = hullBuffersByDeck;
@@ -227,7 +228,7 @@ namespace Forge.Core.Airship.Data{
                 DecalWidth = decalWidth;
                 DecalHeight = decalHeight;
                 TextureToDecalTexel = textureToDecalTexel;
-                HullTextureUVMult = hullTextureUVMult;
+                HullLength = hullLength;
             }
         }
 
