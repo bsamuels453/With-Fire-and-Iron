7d9b7835cb4a2fea659b80bcf3cfea537e811e39
Ben Samuels <bsamuels453@gmail.com>
2013-06-13 20:13:24 -0700

    updating hullsectioncontainer for new decal system

--- Forge.Core/Airship/Data/HullSectionContainer.cs
+++ Forge.Core/Airship/Data/HullSectionContainer.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using Forge.Core.Util;
 using Forge.Framework.Draw;
 using Microsoft.Xna.Framework.Graphics;
 using MonoGameUtility;
@@ -16,18 +17,37 @@ namespace Forge.Core.Airship.Data{
         public readonly ObjectBuffer<int>[] HullBuffersByDeck;
         public readonly int NumDecks;
         readonly HullSection[] _hullSections;
-        readonly List<Vector2> _damagePoints; 
+        readonly TextureBlitter _textureBlitter;
         bool _disposed;
 
-        public HullSectionContainer(List<HullSection> hullSections, ObjectBuffer<int>[] hullBuffersByDeck, List<Vector2> damagePoints){
-            HullBuffersByDeck = hullBuffersByDeck;
+        /// <summary>
+        /// Provides an interface to hull geometry and hull geometry modification methods.
+        /// </summary>
+        /// <param name="hullSections"></param>
+        /// <param name="hullBuffersByDeck"></param>
+        /// <param name="modelAttributes"> </param>
+        /// <param name="damagePoints">Points at which the airship has been damaged. Measured in texel space of the decal.</param>
+        public HullSectionContainer(
+            List<HullSection> hullSections,
+            ObjectBuffer<int>[] hullBuffersByDeck,
+            ModelAttributes modelAttributes,
+            List<Vector2> damagePoints
+            ){
             _hullSections = hullSections.ToArray();
-
-            NumDecks = HullBuffersByDeck.Length;
+            HullBuffersByDeck = hullBuffersByDeck;
             TopExpIdx = 0;
             TopExposedHullLayer = HullBuffersByDeck[TopExpIdx];
+            NumDecks = HullBuffersByDeck.Length;
 
-            _damagePoints = damagePoints;
+            int width = (int) (modelAttributes.Length*32*2);
+            int height = (int) (modelAttributes.Depth*32);
+            _textureBlitter = new TextureBlitter(width, height, "Materials/ImpactCross");
+
+            foreach (var point in damagePoints){
+                _textureBlitter.BlitLocations.Add(point);
+            }
+
+            UpdateDecalTexture(_textureBlitter.GetBlittedTexture());
         }
 
         public int TopExpIdx { get; private set; }
@@ -54,6 +74,12 @@ namespace Forge.Core.Airship.Data{
 
         #endregion
 
+        void UpdateDecalTexture(Texture2D texture){
+            foreach (var buffer in HullBuffersByDeck){
+                buffer.ShaderParams["tex_DecalWrap"].SetValue(texture);
+            }
+        }
+
         /*
         static HullSection[][] GroupSectionsByDeck(HullSection[] hullSections){
             var groupedByDeck = (from section in hullSections
@@ -71,6 +97,11 @@ namespace Forge.Core.Airship.Data{
             return hullSectionByDeck;
         }
          */
+
+        public void AddDamageDecal(Vector2 position){
+            _textureBlitter.BlitLocations.Add(position);
+            UpdateDecalTexture(_textureBlitter.GetBlittedTexture());
+        }
 
         public int SetTopVisibleDeck(int deck){
             if (deck < 0 || deck >= NumDecks)
@@ -120,7 +151,10 @@ namespace Forge.Core.Airship.Data{
                 (
                 NumDecks,
                 _hullSections,
-                hullBuffData
+                hullBuffData,
+                _textureBlitter.BlitLocations,
+                _textureBlitter.TargWidth,
+                _textureBlitter.TargHeight
                 );
             return ret;
         }
@@ -130,11 +164,17 @@ namespace Forge.Core.Airship.Data{
             [ProtoMember(1)] public readonly ObjectBuffer<int>.Serialized[] HullBuffersByDeck;
             [ProtoMember(2)] public readonly HullSection[] HullSections;
             [ProtoMember(3)] public readonly int NumDecks;
+            [ProtoMember(4)] public readonly List<Vector2> DamageDecalPositions;
+            [ProtoMember(5)] public readonly int DamageDecalWidth;
+            [ProtoMember(6)] public readonly int DamageDecalHeight;
 
-            public Serialized(int numDecks, HullSection[] hullSections, ObjectBuffer<int>.Serialized[] hullBuffersByDeck){
+            public Serialized(int numDecks, HullSection[] hullSections, ObjectBuffer<int>.Serialized[] hullBuffersByDeck, List<Vector2> damageDecalPositions, int damageDecalWidth, int damageDecalHeight){
                 NumDecks = numDecks;
                 HullSections = hullSections;
                 HullBuffersByDeck = hullBuffersByDeck;
+                DamageDecalPositions = damageDecalPositions;
+                DamageDecalWidth = damageDecalWidth;
+                DamageDecalHeight = damageDecalHeight;
             }
         }
 
