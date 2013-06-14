e6d6dfd11b266c69a8367ddf7b6adc2a12df1b6d
Ben Samuels <bsamuels453@gmail.com>
2013-06-13 20:01:14 -0700

    adding hullattributegenerator class

---
+++ Forge.Core/Airship/Generation/HullAttributeGenerator.cs
@@ -0,0 +1,31 @@
+﻿#region
+
+using Forge.Core.Airship.Data;
+using MonoGameUtility;
+
+#endregion
+
+namespace Forge.Core.Airship.Generation{
+    /// <summary>
+    /// Calculates the attributes of an airship model based on its geometry.
+    /// </summary>
+    internal static class HullAttributeGenerator{
+        public static ModelAttributes Generate(int numDecks){
+            var modelAttribs = new ModelAttributes();
+            //in the future these attributes will be defined based off analyzing the hull
+            modelAttribs.Length = 50;
+            modelAttribs.MaxAscentRate = 25;
+            modelAttribs.MaxForwardVelocity = 40;
+            modelAttribs.MaxReverseVelocity = 20;
+            modelAttribs.MaxTurnSpeed = 0.87265f;
+            modelAttribs.Berth = 13.95f;
+            modelAttribs.Depth = 7.250177f;
+            modelAttribs.NumDecks = numDecks;
+            modelAttribs.Centroid = new Vector3(modelAttribs.Length/3, 0, 0);
+            modelAttribs.MaxAcceleration = 10;
+            modelAttribs.MaxAscentAcceleration = 7f;
+            modelAttribs.MaxTurnAcceleration = 0.22685f;
+            return modelAttribs;
+        }
+    }
+}
