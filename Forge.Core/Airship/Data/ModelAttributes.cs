686bcfad0379dc7eff557cc69c1e58fc607aa651
Ben Samuels <bsamuels453@gmail.com>
2013-06-13 20:03:30 -0700

    adding a Depth attribute to ModelAttributes

--- Forge.Core/Airship/Data/ModelAttributes.cs
+++ Forge.Core/Airship/Data/ModelAttributes.cs
@@ -10,14 +10,15 @@ namespace Forge.Core.Airship.Data{
     public struct ModelAttributes{
         [ProtoMember(1)] public float Berth;
         [ProtoMember(2)] public Vector3 Centroid;
-        [ProtoMember(3)] public float Length;
-        [ProtoMember(4)] public float MaxAcceleration;
-        [ProtoMember(5)] public float MaxAscentAcceleration;
-        [ProtoMember(6)] public float MaxAscentRate;
-        [ProtoMember(7)] public float MaxForwardVelocity;
-        [ProtoMember(8)] public float MaxReverseVelocity;
-        [ProtoMember(9)] public float MaxTurnAcceleration;
-        [ProtoMember(10)] public float MaxTurnSpeed;
-        [ProtoMember(11)] public int NumDecks;
+        [ProtoMember(3)] public float Depth;
+        [ProtoMember(4)] public float Length;
+        [ProtoMember(5)] public float MaxAcceleration;
+        [ProtoMember(6)] public float MaxAscentAcceleration;
+        [ProtoMember(7)] public float MaxAscentRate;
+        [ProtoMember(8)] public float MaxForwardVelocity;
+        [ProtoMember(9)] public float MaxReverseVelocity;
+        [ProtoMember(10)] public float MaxTurnAcceleration;
+        [ProtoMember(11)] public float MaxTurnSpeed;
+        [ProtoMember(12)] public int NumDecks;
     }
 }
