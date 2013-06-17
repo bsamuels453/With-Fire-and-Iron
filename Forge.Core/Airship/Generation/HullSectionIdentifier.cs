18b042602c45fe01eed82ff5017f8ecd389d72ea
Ben Samuels <bsamuels453@gmail.com>
2013-06-16 21:30:23 -0700

    moving hullsectionidentifier to a diff namespace

--- Forge.Core/Airship/Generation/HullSectionIdentifier.cs
+++ Forge.Core/Airship/Generation/HullSectionIdentifier.cs
@@ -1,10 +1,15 @@
 ﻿#region
 
 using System;
+using Forge.Core.Airship.Data;
 
 #endregion
 
-namespace Forge.Core.Airship.Data{
+namespace Forge.Core.Airship.Generation{
+    /// <summary>
+    /// Used to identify chunks of the airship's hull. Ever since the switch to texture painting
+    /// based decals, this class has only been in use in airship generation.
+    /// </summary>
     public struct HullSectionIdentifier : IEquatable<HullSectionIdentifier>{
         public readonly int Deck;
         public readonly float XStart;
