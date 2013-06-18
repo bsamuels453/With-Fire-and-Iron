267e4c8de3818100b6ff585c91d63005c539021f
Ben Samuels <bsamuels453@gmail.com>
2013-06-17 17:24:29 -0700

    tracking system kinda implemented, needs reworking

--- Forge.Core/Airship/Controllers/AutoPilot/Orbit.cs
+++ Forge.Core/Airship/Controllers/AutoPilot/Orbit.cs
@@ -13,6 +13,12 @@ namespace Forge.Core.Airship.Controllers.AutoPilot{
         readonly float _preferredRange;
         readonly Airship _selfShip;
         readonly Airship _targetAirship;
+
+        /// <summary>
+        /// This value defines by how many time constants we should be leading the target.
+        /// </summary>
+        readonly float _targetLeadMultiplier;
+
         Vector3 _prevTargetVec;
 
         public Orbit(Airship selfShip, AirshipIndexer airships, int targetUid, float range){
@@ -20,6 +26,7 @@ namespace Forge.Core.Airship.Controllers.AutoPilot{
             _airships = airships;
             _targetAirship = _airships[targetUid];
             _selfShip = selfShip;
+            _targetLeadMultiplier = 1;
             DebugText.CreateText("TargPos", 0, 40);
         }
 
@@ -28,21 +35,17 @@ namespace Forge.Core.Airship.Controllers.AutoPilot{
             var attributes = _selfShip.BuffedModelAttributes;
             var stateData = _selfShip.StateData;
 
-            //get target position
-
             var curPosition = stateData.Position;
 
-            var ang = _targetAirship.StateData.Angle;
-            ang.Y += _targetAirship.StateData.TurnRate*timeDeltaSeconds;
-            var unitVec = Common.GetComponentFromAngle(ang.Y - (float) Math.PI/2, 1);
+            var unitVec = Common.GetComponentFromAngle(_targetAirship.StateData.Angle.Y - (float) Math.PI/2, 1);
             var futureTargPosition = _targetAirship.StateData.Position;
-            futureTargPosition.X += unitVec.X*_targetAirship.StateData.Velocity*timeDeltaSeconds*20;
-            futureTargPosition.Z += -unitVec.Y*_targetAirship.StateData.Velocity*timeDeltaSeconds*20;
-            futureTargPosition.Y += _targetAirship.StateData.AscentRate*timeDeltaSeconds*20;
+            float posMult = _targetLeadMultiplier*(0.25f/timeDeltaSeconds);
+            futureTargPosition.X += unitVec.X*_targetAirship.StateData.Velocity*posMult;
+            futureTargPosition.Z += -unitVec.Y*_targetAirship.StateData.Velocity*posMult;
+            futureTargPosition.Y += _targetAirship.StateData.AscentRate*posMult;
 
             var target = futureTargPosition;
-            //if (Vector3.Distance(curPosition, target) >= _preferredRange * 0.9f){
-            //apply hack to fix the weird axis
+
             curPosition = new Vector3(curPosition.Z, curPosition.Y, curPosition.X);
             target = new Vector3(target.Z, target.Y, target.X);
 
@@ -51,13 +54,12 @@ namespace Forge.Core.Airship.Controllers.AutoPilot{
 
             var targetVec = Vector3.Cross(diff, Vector3.Down);
             _prevTargetVec = targetVec;
-            //}
+
 
             var targetPos = _prevTargetVec*_preferredRange + target;
             targetPos.Y = target.Y;
 
-
-            DebugText.SetText("TargPos", "(" + _prevTargetVec.X*_preferredRange + "," + _prevTargetVec.Z*_preferredRange + ")");
+            DebugText.SetText("TargPos", "(" + (curPosition - _targetAirship.StateData.Position).Length() + ")");
 
             var ret = Pathfinder.CalculateAirshipPath
                 (
