e1d9b7d426190516047bddb02fc9a25c8f8d840f
Ben Samuels <bsamuels453@gmail.com>
2013-06-19 13:29:22 -0700

    removing nested mouse exclusivity

--- Forge.Framework/Control/MouseManager.cs
+++ Forge.Framework/Control/MouseManager.cs
@@ -6,11 +6,13 @@ using System.Diagnostics;
 
 namespace Forge.Framework.Control{
     /// <summary>
-    ///   Used to obtain and release control of the mouse. This class dispatches mouse updates via events passed to it through ObtainExclusiveFocus.
+    /// Used to obtain and release control of the mouse. This class dispatches mouse updates to every
+    /// controller subscribed to it. However, if a controller requests exclusive focus of the mouse,
+    /// then only that controller will receive updates until that focus is released.
     /// </summary>
     public class MouseManager{
         /// <summary>
-        ///   The currently active controller that the MouseManager class is dispatching events to.
+        ///   The current exclusive controller that the MouseManager class is dispatching events to.
         /// </summary>
         MouseController _curController;
 
@@ -18,11 +20,6 @@ namespace Forge.Framework.Control{
         ///   The current state of the mouse. This is recorded here so that it can be used as the "prevstate" at next tick.
         /// </summary>
         ForgeMouseState _curState;
-
-        /// <summary>
-        ///   The previous active controller. This is recorded when cachePreviousController=true is passed to ObtainExclusiveFocus. When the current controller releases focus, this controller will become the current controller again.
-        /// </summary>
-        MouseController _prevController;
 
         public MouseManager(){
             _curState = new ForgeMouseState();
@@ -32,38 +29,16 @@ namespace Forge.Framework.Control{
         ///   Main method used for obtaining the mouse's focus so that events can be dispatched.
         /// </summary>
         /// <param name="controller"> The structure containing all of the events that the mouse will invoke. </param>
-        /// <param name="cachePreviousController"> 
-        /// Boolean representing whether or not the the current controller should
-        /// be cached when it is replaced by the calling of this method. If the controller is cached, it will be
-        /// reactivated when focus is released by the class that called the ObtainExclusiveFocus method that originally
-        /// displaced it. 
-        /// </param>
-        public void ObtainExclusiveFocus(
-            MouseController controller,
-            bool cachePreviousController = false){
-            if (_curController != null){
-                _curController.SafeInvokeOnFocusLost(isRestorationPossible: cachePreviousController);
-            }
-
-            //save controller so we can switch back to it after focus lost
-            if (cachePreviousController){
-                Debug.Assert(_prevController == null);
-                _prevController = _curController;
-            }
+        public void ObtainExclusiveFocus(MouseController controller){
+            Debug.Assert(_curController == null);
             _curController = controller;
         }
 
         /// <summary>
-        ///   Releases focus of the mouse so that the current controller will no longer recieve event updates. 
-        ///   If true was passed to cachePreviousController when focus was obtained, then the previous controller
-        ///   will be made active.
+        ///   Releases focus of the mouse so that the current controller will no longer recieve exclusive event updates. 
         /// </summary>
         public void ReleaseExclusiveFocus(){
             _curController = null;
-            if (_prevController != null){
-                _prevController.SafeInvokeOnFocusRegained();
-                _curController = _prevController;
-            }
         }
 
         /// <summary>
