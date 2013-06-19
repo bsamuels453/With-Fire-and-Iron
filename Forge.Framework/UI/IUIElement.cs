2558a542984407ef2bef3ab54b61f47337c06b75
Ben Samuels <bsamuels453@gmail.com>
2013-06-18 20:26:35 -0700

    adding iuielement interface

---
+++ Forge.Framework/UI/IUIElement.cs
@@ -0,0 +1,15 @@
+﻿namespace Forge.Framework.UI{
+    /// <summary>
+    /// Used to define a UI element/collection.
+    /// </summary>
+    internal interface IUIElement{
+        FrameStrata FrameStrata { get; }
+        float X { get; set; }
+        float Y { get; set; }
+        float Width { get; set; }
+        float Height { get; set; }
+        float Alpha { get; set; }
+        float Depth { get; set; }
+        bool HitTest(int x, int y);
+    }
+}
