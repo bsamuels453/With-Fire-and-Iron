ea5e253f8f158c18ca10f4bb2efa2f7a9198a312
Ben Samuels <bsamuels453@gmail.com>
2013-06-20 15:31:57 -0700

    fully implementing mouseover mask

--- Forge.Framework/UI/Elements/MouseoverMask.cs
+++ Forge.Framework/UI/Elements/MouseoverMask.cs
@@ -1,53 +1,111 @@
-﻿using System;
+﻿#region
+
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Forge.Framework.Control;
+using Forge.Framework.Draw;
+using Microsoft.Xna.Framework.Input;
 using MonoGameUtility;
 
-namespace Forge.Framework.UI.Elements {
-    class MouseoverMask : IUIElement{
-        public MouseoverMask(Rectangle boundingBox){
-            throw new NotImplementedException();
+#endregion
+
+namespace Forge.Framework.UI.Elements{
+    internal class MouseoverMask : IUIElement{
+        const float _fadedInAlpha = 0.1f;
+        readonly Sprite2D _sprite;
+        float _alpha;
+        Rectangle _boundingBox;
+
+        public MouseoverMask(Rectangle boundingBox, FrameStrata parentStrata){
+            FrameStrata = new FrameStrata(FrameStrata.Level.High, parentStrata, "MouseoverMaskLayer");
+            _boundingBox = boundingBox;
+            MouseController = new MouseController(this);
+
+            _sprite = new Sprite2D
+                (
+                "Materials/SolidYellow",
+                boundingBox.X,
+                boundingBox.Y,
+                boundingBox.Width,
+                boundingBox.Height,
+                FrameStrata
+                );
+
+            Alpha = 0;
         }
 
-        public FrameStrata FrameStrata{
-            get { throw new NotImplementedException(); }
-        }
+        #region IUIElement Members
+
+        public FrameStrata FrameStrata { get; private set; }
 
         public int X{
-            get { throw new NotImplementedException(); }
-            set { throw new NotImplementedException(); }
+            get { return _boundingBox.X; }
+            set{
+                _boundingBox.X = value;
+                _sprite.X = value;
+            }
         }
 
         public int Y{
-            get { throw new NotImplementedException(); }
-            set { throw new NotImplementedException(); }
+            get { return _boundingBox.Y; }
+            set{
+                _boundingBox.Y = value;
+                _sprite.Y = value;
+            }
         }
 
         public int Width{
-            get { throw new NotImplementedException(); }
+            get { return _boundingBox.Width; }
         }
 
         public int Height{
-            get { throw new NotImplementedException(); }
+            get { return _boundingBox.Height; }
         }
 
         public float Alpha{
-            get { throw new NotImplementedException(); }
-            set { throw new NotImplementedException(); }
+            get { return _alpha; }
+            set{
+                _alpha = value;
+                _sprite.Alpha = value;
+            }
         }
 
-        public MouseController MouseController{
-            get { throw new NotImplementedException(); }
-        }
+        public MouseController MouseController { get; private set; }
 
         public bool HitTest(int x, int y){
-            throw new NotImplementedException();
+            return false;
         }
 
         public List<IUIElement> GetElementStackAtPoint(int x, int y){
-            throw new NotImplementedException();
+            return new List<IUIElement>();
+        }
+
+        public void InitializeEvents(UIElementCollection parent){
+            parent.OnMouseEntry += OnMouseEntry;
+            parent.OnMouseExit += OnMouseExit;
+            parent.OnLeftDown += OnMouseLeftDown;
+            parent.OnLeftRelease += OnMouseLeftUp;
+        }
+
+        #endregion
+
+        void OnMouseEntry(ForgeMouseState state, float timeDelta, UIElementCollection caller){
+            if (state.LeftButtonState == ButtonState.Released){
+                Alpha = _fadedInAlpha;
+            }
+        }
+
+        void OnMouseExit(ForgeMouseState state, float timeDelta, UIElementCollection caller){
+            Alpha = 0;
+        }
+
+        void OnMouseLeftDown(ForgeMouseState state, float timeDelta, UIElementCollection caller){
+            Alpha = 0;
+        }
+
+        void OnMouseLeftUp(ForgeMouseState state, float timeDelta, UIElementCollection caller){
+            if (caller.ContainsMouse){
+                Alpha = _fadedInAlpha;
+            }
         }
     }
 }
