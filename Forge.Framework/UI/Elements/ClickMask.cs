28e96e7ff1eb1a93ab3e1ee71933c8460211566a
Ben Samuels <bsamuels453@gmail.com>
2013-06-19 18:29:07 -0700

    adding clickmask class

---
+++ Forge.Framework/UI/Elements/ClickMask.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Forge.Framework.Control;
+
+namespace Forge.Framework.UI.Elements {
+    class ClickMask : IUIElement {
+        public FrameStrata FrameStrata{
+            get { throw new NotImplementedException(); }
+        }
+
+        public int X{
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public int Y{
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public int Width{
+            get { throw new NotImplementedException(); }
+        }
+
+        public int Height{
+            get { throw new NotImplementedException(); }
+        }
+
+        public float Alpha{
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public MouseController MouseController{
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool HitTest(int x, int y){
+            throw new NotImplementedException();
+        }
+
+        public List<IUIElement> GetElementStackAtPoint(int x, int y){
+            throw new NotImplementedException();
+        }
+    }
+}
