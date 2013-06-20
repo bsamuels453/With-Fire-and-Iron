4cc60602aec02da149a04e9941e571571e518eae
Ben Samuels <bsamuels453@gmail.com>
2013-06-19 18:29:20 -0700

    adding standard button class

---
+++ Forge.Framework/UI/Elements/StandardButton.cs
@@ -0,0 +1,20 @@
+﻿#region
+
+using Microsoft.Xna.Framework.Graphics;
+using MonoGameUtility;
+
+#endregion
+
+namespace Forge.Framework.UI.Elements{
+    internal class StandardButton : UIElementCollection{
+        public StandardButton(
+            UIElementCollection parent,
+            FrameStrata.FrameStratum depth,
+            Rectangle boundingBox,
+            Texture2D texture,
+            bool clickMask,
+            bool enableMouseoverMask
+            ) : base(parent, depth, boundingBox, "StandardButton"){
+        }
+    }
+}
