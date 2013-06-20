e4436fcd20d3ddfee730a0832124f26e3e526d05
Ben Samuels <bsamuels453@gmail.com>
2013-06-20 15:47:59 -0700

    implementing click mask

--- Forge.Framework/UI/Elements/ClickMask.cs
+++ Forge.Framework/UI/Elements/ClickMask.cs
@@ -1,54 +1,101 @@
-﻿using System;
+﻿#region
+
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Forge.Framework.Control;
+using Forge.Framework.Draw;
 using MonoGameUtility;
 
-namespace Forge.Framework.UI.Elements {
-    class ClickMask : IUIElement {
+#endregion
 
-        public ClickMask(Rectangle boundingBox){
-            throw new NotImplementedException();
+namespace Forge.Framework.UI.Elements{
+    internal class ClickMask : IUIElement{
+        const float _fadedInAlpha = 0.2f;
+        readonly Sprite2D _sprite;
+        float _alpha;
+        Rectangle _boundingBox;
+
+        public ClickMask(Rectangle boundingBox, FrameStrata parentStrata){
+            FrameStrata = new FrameStrata(FrameStrata.Level.High, parentStrata, "ClickMask");
+            _boundingBox = boundingBox;
+            MouseController = new MouseController(this);
+
+            _sprite = new Sprite2D
+                (
+                "Materials/SolidBlack",
+                boundingBox.X,
+                boundingBox.Y,
+                boundingBox.Width,
+                boundingBox.Height,
+                FrameStrata
+                );
+
+            Alpha = 0;
         }
 
-        public FrameStrata FrameStrata{
-            get { throw new NotImplementedException(); }
-        }
+        #region IUIElement Members
+
+        public FrameStrata FrameStrata { get; private set; }
 
         public int X{
-            get { throw new NotImplementedException(); }
-            set { throw new NotImplementedException(); }
+            get { return _boundingBox.X; }
+            set{
+                _boundingBox.X = value;
+                _sprite.X = value;
+            }
         }
 
         public int Y{
-            get { throw new NotImplementedException(); }
-            set { throw new NotImplementedException(); }
+            get { return _boundingBox.Y; }
+            set{
+                _boundingBox.Y = value;
+                _sprite.Y = value;
+            }
         }
 
         public int Width{
-            get { throw new NotImplementedException(); }
+            get { return _boundingBox.Width; }
         }
 
         public int Height{
-            get { throw new NotImplementedException(); }
+            get { return _boundingBox.Height; }
         }
 
         public float Alpha{
-            get { throw new NotImplementedException(); }
-            set { throw new NotImplementedException(); }
+            get { return _alpha; }
+            set{
+                _alpha = value;
+                _sprite.Alpha = value;
+            }
         }
 
-        public MouseController MouseController{
-            get { throw new NotImplementedException(); }
-        }
+        public MouseController MouseController { get; private set; }
 
         public bool HitTest(int x, int y){
-            throw new NotImplementedException();
+            return false;
         }
 
         public List<IUIElement> GetElementStackAtPoint(int x, int y){
-            throw new NotImplementedException();
+            return new List<IUIElement>();
+        }
+
+        public void InitializeEvents(UIElementCollection parent){
+            parent.OnMouseExit += OnMouseExit;
+            parent.OnLeftDown += OnMouseLeftDown;
+            parent.OnLeftRelease += OnMouseLeftUp;
+        }
+
+        #endregion
+
+        void OnMouseExit(ForgeMouseState state, float timeDelta, UIElementCollection caller){
+            Alpha = 0;
+        }
+
+        void OnMouseLeftDown(ForgeMouseState state, float timeDelta, UIElementCollection caller){
+            Alpha = _fadedInAlpha;
+        }
+
+        void OnMouseLeftUp(ForgeMouseState state, float timeDelta, UIElementCollection caller){
+            Alpha = 0;
         }
     }
 }
