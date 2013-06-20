b61be43de7b2c01362c0de949033660aa0448e9c
Ben Samuels <bsamuels453@gmail.com>
2013-06-20 16:24:24 -0700

    implementing main static button class

--- Forge.Framework/UI/Elements/StandardButton.cs
+++ Forge.Framework/UI/Elements/StandardButton.cs
@@ -1,20 +1,32 @@
 ﻿#region
 
-using Microsoft.Xna.Framework.Graphics;
+using Forge.Framework.Draw;
 using MonoGameUtility;
 
 #endregion
 
 namespace Forge.Framework.UI.Elements{
-    internal class StandardButton : UIElementCollection{
+    public class StandardButton : UIElementCollection{
         public StandardButton(
             UIElementCollection parent,
-            FrameStrata.FrameStratum depth,
+            FrameStrata.Level depth,
             Rectangle boundingBox,
-            Texture2D texture,
-            bool clickMask,
+            string buttonTex,
+            bool enableClickMask,
             bool enableMouseoverMask
             ) : base(parent, depth, boundingBox, "StandardButton"){
+            if (enableClickMask){
+                var mask = new ClickMask(boundingBox, this.FrameStrata);
+                AddElement(mask);
+                mask.InitializeEvents(this);
+            }
+            if (enableMouseoverMask){
+                var mask = new MouseoverMask(boundingBox, this.FrameStrata);
+                AddElement(mask);
+                mask.InitializeEvents(this);
+            }
+            var texture = new Sprite2D(buttonTex, boundingBox, FrameStrata);
+            this.AddElement(texture);
         }
     }
 }
