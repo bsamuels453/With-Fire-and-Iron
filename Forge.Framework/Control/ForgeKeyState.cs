e0d4e309e7122795b937ecbf63d8ab0bc69b47e7
Ben Samuels <bsamuels453@gmail.com>
2013-06-26 15:44:22 -0700

    adding forgekeystate class

---
+++ Forge.Framework/Control/ForgeKeyState.cs
@@ -0,0 +1,19 @@
+﻿#region
+
+using Microsoft.Xna.Framework.Input;
+
+#endregion
+
+namespace Forge.Framework.Control{
+    internal struct ForgeKeyState{
+        /// <summary>
+        /// The current state of the key.
+        /// </summary>
+        public KeyState State;
+
+        /// <summary>
+        /// Whether or not the keystate has changed since the last poll.
+        /// </summary>
+        public bool StateChanged;
+    }
+}
