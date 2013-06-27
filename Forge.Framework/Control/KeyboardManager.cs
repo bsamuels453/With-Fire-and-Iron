374e6e7881e15a1b3902fc61f85cfd56bc2273d5
Ben Samuels <bsamuels453@gmail.com>
2013-06-26 18:06:52 -0700

    implementing keyboardmanager

---
+++ Forge.Framework/Control/KeyboardManager.cs
@@ -0,0 +1,64 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Input;
+
+#endregion
+
+namespace Forge.Framework.Control{
+    /// <summary>
+    /// Handles the dispatching of keyboard state information to KeyboardControllers.
+    /// </summary>
+    internal class KeyboardManager{
+        readonly Stack<KeyboardController> _cachedBindings;
+        readonly ForgeKeyState[] _keyState;
+        readonly Keys[] _keys;
+        KeyboardController _activeBinding;
+
+        public KeyboardManager(){
+            _cachedBindings = new Stack<KeyboardController>();
+            _keys = (Keys[]) Enum.GetValues(typeof (Keys));
+            _keyState = new ForgeKeyState[_keys.Length];
+        }
+
+        /// <summary>
+        /// Updates the keyboard state and dispatches the state to the current keyboard controller,
+        /// which will invoke keyboard events, if applicable.
+        /// </summary>
+        public void UpdateKeyboard(){
+            var curState = Keyboard.GetState();
+
+            foreach (var key in _keys){
+                var curKeyState = curState[key];
+                _keyState[(int) key].ApplyNewState(curKeyState);
+            }
+
+            if (_activeBinding != null){
+                _activeBinding.InvokeKeyboardBinds(_keyState);
+            }
+        }
+
+        /// <summary>
+        /// Sets the provided controller as the active controller, and caches the current
+        /// active controller if it exists.
+        /// </summary>
+        /// <param name="controller"></param>
+        public void SetActiveController(KeyboardController controller){
+            if (_activeBinding != null){
+                _cachedBindings.Push(_activeBinding);
+            }
+            _activeBinding = controller;
+        }
+
+        /// <summary>
+        /// Releases the current controller and restores the previous controller, if it exists.
+        /// </summary>
+        public void ReleaseActiveController(){
+            _activeBinding = null;
+            if (_cachedBindings.Count > 0){
+                _activeBinding = _cachedBindings.Pop();
+            }
+        }
+    }
+}
