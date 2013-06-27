9a9859a0599d19406a91f5c9e6802f15bf1bc57d
Ben Samuels <bsamuels453@gmail.com>
2013-06-26 17:22:29 -0700

    adding ApplyNewState and SatisfiesBindCondition methods

--- Forge.Framework/Control/ForgeKeyState.cs
+++ Forge.Framework/Control/ForgeKeyState.cs
@@ -1,5 +1,6 @@
 ﻿#region
 
+using System;
 using Microsoft.Xna.Framework.Input;
 
 #endregion
@@ -15,5 +16,47 @@ namespace Forge.Framework.Control{
         /// Whether or not the keystate has changed since the last poll.
         /// </summary>
         public bool StateChanged;
+
+        public void ApplyNewState(KeyState state){
+            if (State != state){
+                StateChanged = true;
+                State = state;
+            }
+        }
+
+        /// <summary>
+        /// Determines whether or not the current key state satisfies the provided bind condition.
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        public bool SatisfiesBindCondition(BindCondition condition){
+            switch (condition){
+                case BindCondition.KeyChangeUp:
+                    if (State == KeyState.Up && StateChanged)
+                        return true;
+                    return false;
+
+                case BindCondition.KeyChangeDown:
+                    if (State == KeyState.Down && StateChanged)
+                        return true;
+                    return false;
+
+                case BindCondition.KeyUp:
+                    if (State == KeyState.Up)
+                        return true;
+                    return false;
+
+                case BindCondition.KeyDown:
+                    if (State == KeyState.Down)
+                        return true;
+                    return false;
+
+                case BindCondition.KeyChange:
+                    return StateChanged;
+
+                default:
+                    throw new Exception();
+            }
+        }
     }
 }
