7e29f84d40f3393138fc0ca46b5baeac1eceb373
Ben Samuels <bsamuels453@gmail.com>
2013-06-27 15:39:08 -0700

    removing texture field of interface

--- Forge.Framework/Draw/IDrawableSprite.cs
+++ Forge.Framework/Draw/IDrawableSprite.cs
@@ -1,13 +1,11 @@
 ﻿#region
 
-using Microsoft.Xna.Framework.Graphics;
 using MonoGameUtility;
 
 #endregion
 
 namespace Forge.Framework.Draw{
     public interface IDrawableSprite{
-        Texture2D Texture { get; set; }
         void Draw();
         void SetTextureFromString(string textureName);
         void Dispose();
