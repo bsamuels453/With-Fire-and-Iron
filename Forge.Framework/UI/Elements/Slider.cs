39d202c94da32c4d6717261f2b45ba5e4b260510
Ben Samuels <bsamuels453@gmail.com>
2013-07-02 17:00:09 -0700

    completely finished implementing slider sprite generation

--- Forge.Framework/UI/Elements/Slider.cs
+++ Forge.Framework/UI/Elements/Slider.cs
@@ -4,8 +4,10 @@ using System;
 using System.Diagnostics;
 using Forge.Framework.Draw;
 using Forge.Framework.Resources;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using MonoGameUtility;
+using Point = MonoGameUtility.Point;
+using Rectangle = MonoGameUtility.Rectangle;
 
 #endregion
 
@@ -13,7 +15,6 @@ namespace Forge.Framework.UI.Elements{
     public class Slider : UIElementCollection{
         #region template defined
 
-        readonly int _endpointWidth;
         readonly int _handleHeight;
         readonly string _handleMaterial;
         readonly int _handleVerticalOff;
@@ -41,6 +42,8 @@ namespace Forge.Framework.UI.Elements{
 
         readonly int _verticalTrackPadding;
 
+        float _handleValue;
+
         public Slider(
             UIElementCollection parent,
             FrameStrata.Level depth,
@@ -63,13 +66,16 @@ namespace Forge.Framework.UI.Elements{
             _sliderTrackPadding = jobj["SliderTrackPadding"].ToObject<int>();
             _handleVerticalOff = jobj["HandleVerticalOffset"].ToObject<int>();
             _trackHeight = jobj["TrackHeight"].ToObject<int>();
-            _endpointWidth = jobj["EndpointWidth"].ToObject<int>();
             _handleWidth = jobj["HandleWidth"].ToObject<int>();
             _handleHeight = jobj["HandleHeight"].ToObject<int>();
 
             #endregion
 
             Debug.Assert(Math.Abs((maxVal - minVal)%step - 0) < 0.00001f);
+
+            var handleTex = Resource.LoadContent<Texture2D>(_handleMaterial);
+            var trackTex = Resource.LoadContent<Texture2D>(_trackMaterial);
+            var endTex = Resource.LoadContent<Texture2D>(_trackEndpointMaterial);
 
             #region logic
 
@@ -78,7 +84,7 @@ namespace Forge.Framework.UI.Elements{
             _valueMultiplier = sliderRange/valueRange;
             _minVal = minVal;
             _maxVal = maxVal;
-            _trackStart = position.X + _horizontalTrackPadding + _sliderTrackPadding + _endpointWidth;
+            _trackStart = position.X + _horizontalTrackPadding + _sliderTrackPadding;
 
             _stepOffsetsPixel = new int[(int) ((maxVal - minVal)/step)];
             _stepOffsetsValue = new float[_stepOffsetsPixel.Length];
@@ -90,50 +96,65 @@ namespace Forge.Framework.UI.Elements{
 
             #endregion
 
+            float defaultHandleValue = (_minVal + _maxVal)/2f;
+
             #region sprites
 
-            var handleTex = Resource.LoadContent<Texture2D>(_handleMaterial);
-            var trackTex = Resource.LoadContent<Texture2D>(_trackMaterial);
-            var endTex = Resource.LoadContent<Texture2D>(_trackEndpointMaterial);
+            var sb = new SpriteBatch(Resource.Device);
+            var target = new RenderTarget2D(Resource.Device, _trackWidth, trackTex.Height);
+            Resource.Device.SetRenderTarget(target);
+            Resource.Device.Clear(Color.Transparent);
 
-            var trackSprite = new Sprite2D
+            sb.Begin(SpriteSortMode.Texture, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.Default, RasterizerState.CullNone);
+            sb.Draw
                 (
-                trackTex,
-                position.X + _horizontalTrackPadding + _endpointWidth,
-                position.Y + _verticalTrackPadding,
-                _trackWidth,
-                _trackHeight,
-                this.FrameStrata,
-                FrameStrata.Level.Low
-                );
+                    trackTex,
+                    new Rectangle(endTex.Width, 0, _trackWidth - endTex.Width*2, trackTex.Height),
+                    null,
+                    Color.White);
+            sb.Draw
+                (
+                    endTex,
+                    Vector2.Zero,
+                    null,
+                    Color.White);
+            sb.Draw
+                (
+                    endTex,
+                    new Rectangle(_trackWidth - endTex.Width, 0, endTex.Width, endTex.Height),
+                    null,
+                    Color.White,
+                    0,
+                    Vector2.Zero,
+                    SpriteEffects.FlipHorizontally, 0);
+            sb.End();
 
-            var endSprite1 = new Sprite2D
+            //apply trackheight scaling
+            var finalBG = new RenderTarget2D(Resource.Device, target.Width, _trackHeight);
+            Resource.Device.SetRenderTarget(finalBG);
+            Resource.Device.Clear(Color.Transparent);
+
+            sb.Begin(SpriteSortMode.Texture, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.Default, RasterizerState.CullNone);
+            sb.Draw(target, new Rectangle(0, 0, finalBG.Width, finalBG.Height), null, Color.White);
+            sb.End();
+
+            Resource.Device.SetRenderTarget(null);
+
+            var bgSprite = new Sprite2D
                 (
-                endTex,
+                finalBG,
                 position.X + _horizontalTrackPadding,
                 position.Y + _verticalTrackPadding,
-                _endpointWidth,
-                _trackHeight,
+                finalBG.Width,
+                finalBG.Height,
                 this.FrameStrata,
-                FrameStrata.Level.Low
+                FrameStrata.Level.Background
                 );
-
-            var endSprite2 = new Sprite2D
-                (
-                endTex,
-                position.X + _horizontalTrackPadding + _endpointWidth + _trackWidth,
-                position.Y + _verticalTrackPadding,
-                _endpointWidth,
-                _trackHeight,
-                this.FrameStrata,
-                FrameStrata.Level.Low
-                );
-            endSprite2.SpriteEffect = SpriteEffects.FlipHorizontally;
 
             _sliderSprite = new Sprite2D
                 (
                 handleTex,
-                _trackStart + ConvertToPixelOffset((_minVal + _maxVal)/2f),
+                _trackStart + ConvertToPixelOffset(defaultHandleValue) - _handleWidth/2,
                 position.Y - _handleVerticalOff + _verticalTrackPadding,
                 _handleWidth,
                 _handleHeight,
@@ -141,12 +162,22 @@ namespace Forge.Framework.UI.Elements{
                 FrameStrata.Level.Medium
                 );
 
-            this.AddElement(trackSprite);
-            this.AddElement(endSprite1);
-            this.AddElement(endSprite2);
+            this.AddElement(bgSprite);
             this.AddElement(_sliderSprite);
 
             #endregion
+
+            HandleValue = defaultHandleValue;
+        }
+
+        public float HandleValue{
+            get { return _handleValue; }
+            set{
+                Debug.Assert(value <= _maxVal && value >= _minVal);
+                int offset = ConvertToPixelOffset(value);
+                _sliderSprite.X = offset + _trackStart;
+                _handleValue = value;
+            }
         }
 
         float ConvertToValue(int stepIndex){
