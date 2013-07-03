bff22b4061840fe7702daf7c65b835da0c8264d1
Ben Samuels <bsamuels453@gmail.com>
2013-07-03 12:57:16 -0700

    adding panel class

---
+++ Forge.Framework/UI/Elements/Panel.cs
@@ -0,0 +1,13 @@
+﻿#region
+
+using MonoGameUtility;
+
+#endregion
+
+namespace Forge.Framework.UI.Elements{
+    internal class Panel : UIElementCollection{
+        public Panel(UIElementCollection parent, FrameStrata.Level depth, Rectangle boundingBox, string alias)
+            : base(parent, depth, boundingBox, alias){
+        }
+    }
+}
