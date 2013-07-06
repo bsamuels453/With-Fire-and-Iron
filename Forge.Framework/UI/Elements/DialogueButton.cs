ad40b02605853d7caf3e2180ea449449c9df10b9
Ben Samuels <bsamuels453@gmail.com>
2013-07-06 13:35:58 -0700

    renaming standardbutton to dialoguebutton

---
+++ Forge.Framework/UI/Elements/DialogueButton.cs
@@ -0,0 +1,30 @@
+﻿#region
+
+using Forge.Framework.Draw;
+using Forge.Framework.Resources;
+using MonoGameUtility;
+
+#endregion
+
+namespace Forge.Framework.UI.Elements{
+    public class DialogueButton : UIElementCollection{
+        public DialogueButton(
+            UIElementCollection parent,
+            FrameStrata.Level depth,
+            Rectangle boundingBox,
+            string buttonTex
+            ) : base(parent, depth, boundingBox, "DialogueButton"){
+
+                var jobj = Resource.LoadJObject("UiTemplates/DialogueButton.json");
+
+                var clickMask = new ClickMask(boundingBox, this);
+                AddElement(clickMask);
+
+                var mouseoverMask = new MouseoverMask(boundingBox, this);
+                AddElement(mouseoverMask);
+
+            var texture = new Sprite2D(buttonTex, boundingBox, FrameStrata);
+            this.AddElement(texture);
+        }
+    }
+}
