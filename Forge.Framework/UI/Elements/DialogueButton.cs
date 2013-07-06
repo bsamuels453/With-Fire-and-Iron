0effe1c228ede55215e8bd4940e9fbd87d2e7cf8
Ben Samuels <bsamuels453@gmail.com>
2013-07-06 15:40:11 -0700

    implementing barebones dialogue button

--- Forge.Framework/UI/Elements/DialogueButton.cs
+++ Forge.Framework/UI/Elements/DialogueButton.cs
@@ -1,8 +1,11 @@
 ﻿#region
 
+using System.Diagnostics;
 using Forge.Framework.Draw;
 using Forge.Framework.Resources;
-using MonoGameUtility;
+using Microsoft.Xna.Framework;
+using Point = MonoGameUtility.Point;
+using Rectangle = MonoGameUtility.Rectangle;
 
 #endregion
 
@@ -11,20 +14,51 @@ namespace Forge.Framework.UI.Elements{
         public DialogueButton(
             UIElementCollection parent,
             FrameStrata.Level depth,
-            Rectangle boundingBox,
-            string buttonTex
-            ) : base(parent, depth, boundingBox, "DialogueButton"){
+            Point position,
+            string text
+            ) : base(parent, depth, new Rectangle(), "DialogueButton"){
+            var jobj = Resource.LoadJObject("UiTemplates/DialogueButton.json");
+            string bgMaterial = jobj["Material"].ToObject<string>();
+            string textFont = jobj["TextFont"].ToObject<string>();
+            int horizontalTextPadding = jobj["HorizontalTextPadding"].ToObject<int>();
+            int height = jobj["Height"].ToObject<int>();
+            int textVOffset = jobj["TextVerticalOffset"].ToObject<int>();
 
-                var jobj = Resource.LoadJObject("UiTemplates/DialogueButton.json");
+            var dims = TextBox.MeasureString(textFont, text);
 
-                var clickMask = new ClickMask(boundingBox, this);
-                AddElement(clickMask);
+            Debug.Assert(dims.Y < height);
 
-                var mouseoverMask = new MouseoverMask(boundingBox, this);
-                AddElement(mouseoverMask);
+            this.BoundingBox = new Rectangle
+                (
+                position.X,
+                position.Y,
+                (int) (dims.X + horizontalTextPadding*2),
+                height
+                );
 
-            var texture = new Sprite2D(buttonTex, boundingBox, FrameStrata);
-            this.AddElement(texture);
+
+            var clickMask = new ClickMask(BoundingBox, this);
+            var mouseoverMask = new MouseoverMask(BoundingBox, this);
+            var texture = new Sprite2D(bgMaterial, BoundingBox, this.FrameStrata, FrameStrata.Level.Background);
+
+            var textObject = new TextBox
+                (
+                position + new Point(horizontalTextPadding, textVOffset),
+                this,
+                FrameStrata.Level.High,
+                Color.Black,
+                textFont,
+                (int) dims.X,
+                1,
+                TextBox.Justification.Center
+                );
+            textObject.SetText(text);
+
+
+            AddElement(clickMask);
+            AddElement(mouseoverMask);
+            AddElement(texture);
+            AddElement(textObject);
         }
     }
 }
