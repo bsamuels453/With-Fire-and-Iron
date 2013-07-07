96c334f910b13e8c495f0ab67af19e4688958551
Ben Samuels <bsamuels453@gmail.com>
2013-07-07 16:10:53 -0700

    moving shader loading functionality into HLSLShaderLoader

--- Forge.Framework/Resources/HLSLShaderLoader.cs
+++ Forge.Framework/Resources/HLSLShaderLoader.cs
@@ -1,4 +1,103 @@
-﻿namespace Forge.Framework.Resources{
-    internal class HLSLShaderLoader{
+﻿#region
+
+using System;
+using System.Linq;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGameUtility;
+
+#endregion
+
+namespace Forge.Framework.Resources{
+    internal class HLSLShaderLoader : ResourceLoader{
+        readonly ConfigLoader _configLoader;
+        readonly ContentManager _contentManager;
+
+        public HLSLShaderLoader(ContentManager contentManager, ConfigLoader configLoader){
+            _contentManager = contentManager;
+            _configLoader = configLoader;
+        }
+
+        public void LoadShader(string configLocation, out Effect effect){
+            effect = null;
+            var configs = _configLoader.LoadConfig(configLocation);
+
+            foreach (var config in configs){
+                var key = config.Key;
+                var val = config.Value.ToObject<string>();
+                if (key.Equals("Shader")){
+                    lock (Resource.Device){
+                        effect = _contentManager.Load<Effect>(val).Clone();
+                    }
+                    if (effect == null){
+                        throw new Exception("Shader not found");
+                    }
+                    break;
+                }
+            }
+
+            if (effect == null){
+                throw new Exception("Shader not specified for " + configLocation);
+            }
+
+            //figure out its datatype
+            foreach (var config in configs){
+                string name = config.Key;
+                string configVal = config.Value.ToObject<string>();
+
+                if (configVal.Contains(",")){
+                    //it's a vector
+                    var commas = from value in configVal
+                        where value == ','
+                        select value;
+                    int commaCount = commas.Count();
+                    switch (commaCount){
+                        case 1:
+                            var vec2 = VectorParser.Parse<Vector2>(configVal);
+                            effect.Parameters[name].SetValue(vec2);
+                            break;
+                        case 2:
+                            var vec3 = VectorParser.Parse<Vector3>(configVal);
+                            effect.Parameters[name].SetValue(vec3);
+                            break;
+                        case 3:
+                            var vec4 = VectorParser.Parse<Vector4>(configVal);
+                            effect.Parameters[name].SetValue(vec4);
+                            break;
+                        default:
+                            throw new Exception("vector4 is the largest dimension of vector supported");
+                    }
+                    continue;
+                }
+                //figure out if it's a string
+                var alphanumerics = from value in configVal
+                    where char.IsLetter(value)
+                    select value;
+                if (alphanumerics.Any()){
+                    if (name == "Shader")
+                        continue;
+                    //it's a string, and in the context of shader settings, strings always coorespond with texture names
+                    Texture2D texture;
+                    lock (Resource.Device){
+                        texture = _contentManager.Load<Texture2D>(configVal);
+                    }
+                    effect.Parameters[name].SetValue(texture);
+                    continue;
+                }
+
+                if (configVal.Contains(".")){
+                    //it's a float
+                    effect.Parameters[name].SetValue(float.Parse(configVal));
+                    continue;
+                }
+
+                //assume its an integer
+                effect.Parameters[name].SetValue(int.Parse(configVal));
+            }
+        }
+
+
+        public override void Dispose(){
+        }
     }
 }
--- Forge.Framework/Resources/Resource.cs
+++ Forge.Framework/Resources/Resource.cs
@@ -3,7 +3,6 @@
 using System;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using Cloo;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
@@ -19,6 +18,7 @@ namespace Forge.Framework.Resources{
         public static ScreenSize ScreenSize;
         static OpenCLScriptLoader _openCLScriptLoader;
         static ConfigLoader _configLoader;
+        static HLSLShaderLoader _shaderLoader;
         static bool _disposed;
         public static GraphicsDevice Device { get; private set; }
 
@@ -27,6 +27,7 @@ namespace Forge.Framework.Resources{
             _contentManager = content;
             _openCLScriptLoader = new OpenCLScriptLoader();
             _configLoader = new ConfigLoader();
+            _shaderLoader = new HLSLShaderLoader(_contentManager, _configLoader);
         }
 
         /// <summary>
@@ -120,82 +121,7 @@ namespace Forge.Framework.Resources{
         /// <param name="configLocation"> The location of the shader's configuration file. </param>
         /// <param name="effect"> The loaded effect. New effects are cloned to prevent dupe issues. </param>
         public static void LoadShader(string configLocation, out Effect effect){
-            effect = null;
-
-            var configs = _configLoader.LoadConfig(configLocation);
-
-            foreach (var config in configs){
-                var key = config.Key;
-                var val = config.Value.ToObject<string>();
-                if (key.Equals("Shader")){
-                    lock (Device){
-                        effect = _contentManager.Load<Effect>(val).Clone();
-                    }
-                    if (effect == null){
-                        throw new Exception("Shader not found");
-                    }
-                    break;
-                }
-            }
-
-            if (effect == null){
-                throw new Exception("Shader not specified for " + configLocation);
-            }
-
-            //figure out its datatype
-            foreach (var config in configs){
-                string name = config.Key;
-                string configVal = config.Value.ToObject<string>();
-
-                if (configVal.Contains(",")){
-                    //it's a vector
-                    var commas = from value in configVal
-                        where value == ','
-                        select value;
-                    int commaCount = commas.Count();
-                    switch (commaCount){
-                        case 1:
-                            var vec2 = VectorParser.Parse<Vector2>(configVal);
-                            effect.Parameters[name].SetValue(vec2);
-                            break;
-                        case 2:
-                            var vec3 = VectorParser.Parse<Vector3>(configVal);
-                            effect.Parameters[name].SetValue(vec3);
-                            break;
-                        case 3:
-                            var vec4 = VectorParser.Parse<Vector4>(configVal);
-                            effect.Parameters[name].SetValue(vec4);
-                            break;
-                        default:
-                            throw new Exception("vector4 is the largest dimension of vector supported");
-                    }
-                    continue;
-                }
-                //figure out if it's a string
-                var alphanumerics = from value in configVal
-                    where char.IsLetter(value)
-                    select value;
-                if (alphanumerics.Any()){
-                    if (name == "Shader")
-                        continue;
-                    //it's a string, and in the context of shader settings, strings always coorespond with texture names
-                    Texture2D texture;
-                    lock (Device){
-                        texture = _contentManager.Load<Texture2D>(configVal);
-                    }
-                    effect.Parameters[name].SetValue(texture);
-                    continue;
-                }
-
-                if (configVal.Contains(".")){
-                    //it's a float
-                    effect.Parameters[name].SetValue(float.Parse(configVal));
-                    continue;
-                }
-
-                //assume its an integer
-                effect.Parameters[name].SetValue(int.Parse(configVal));
-            }
+            _shaderLoader.LoadShader(configLocation, out effect);
         }
 
 
