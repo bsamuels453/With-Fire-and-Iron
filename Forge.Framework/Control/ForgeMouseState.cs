5a3a878e980b3325a72ff74050ee5cfa270fdbe8
Ben Samuels <bsamuels453@gmail.com>
2013-07-07 23:38:03 -0700

    implementing input recording for mouse

--- Forge.Framework/Control/ForgeMouseState.cs
+++ Forge.Framework/Control/ForgeMouseState.cs
@@ -1,6 +1,11 @@
-﻿#region
+﻿#define RECORD_MOUSE
+//#define READ_MOUSE_FROM_FILE
 
+#region
+
+using System;
 using System.Diagnostics;
+using System.IO;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 
@@ -15,6 +20,8 @@ namespace Forge.Framework.Control{
 
         static readonly Stopwatch _rightclickTimer;
         static readonly Stopwatch _leftclickTimer;
+        static readonly StreamWriter _mouseRecord;
+        static readonly StreamReader _mouseReader;
 
         readonly bool _leftButtonChange;
         readonly bool _leftButtonClick;
@@ -59,6 +66,14 @@ namespace Forge.Framework.Control{
         static ForgeMouseState(){
             _rightclickTimer = new Stopwatch();
             _leftclickTimer = new Stopwatch();
+#if RECORD_MOUSE
+            _mouseRecord = new StreamWriter("mouse.dat");
+            _mouseRecord.AutoFlush = true;
+#endif
+
+#if READ_MOUSE_FROM_FILE
+            _mouseReader = new StreamReader("mouse.dat");
+#endif
         }
 
         /// <summary>
@@ -97,6 +112,15 @@ namespace Forge.Framework.Control{
             PrevState.BlockRightMButton = false;
             PrevState.BlockScrollWheel = false;
             var curState = Mouse.GetState();
+
+
+#if RECORD_MOUSE
+            WriteMouseStateToRecord(curState);
+#endif
+
+#if READ_MOUSE_FROM_FILE
+            curState = ReadMouseStateFromRecord();
+#endif
 
             _mousePos = new Point(curState.X, curState.Y);
 
@@ -143,6 +167,29 @@ namespace Forge.Framework.Control{
                     _rightclickTimer.Start();
                 }
             }
+        }
+
+        static void WriteMouseStateToRecord(MouseState state){
+            _mouseRecord.WriteLine
+                (state.X + " " + state.Y + " " + state.LeftButton + " " + state.RightButton + " " + state.MiddleButton + " " + state.ScrollWheelValue);
+        }
+
+        static MouseState ReadMouseStateFromRecord(){
+            var line = _mouseReader.ReadLine();
+            if (line.Length == 0){
+                throw new Exception("End of input stream");
+            }
+            var elements = line.Split(' ');
+
+            int x = int.Parse(elements[0]);
+            int y = int.Parse(elements[1]);
+            ButtonState left = (ButtonState) Enum.Parse(typeof (ButtonState), elements[2]);
+            ButtonState right = (ButtonState) Enum.Parse(typeof (ButtonState), elements[3]);
+            ButtonState middle = (ButtonState) Enum.Parse(typeof (ButtonState), elements[4]);
+            int scrollwheel = int.Parse(elements[5]);
+
+            var state = new MouseState(x, y, scrollwheel, left, middle, right, ButtonState.Released, ButtonState.Released);
+            return state;
         }
 
         #endregion
