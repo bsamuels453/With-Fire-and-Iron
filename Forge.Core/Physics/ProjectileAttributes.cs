db27531ea3f2567c77d575edf63080dbf8ec6984
Ben Samuels <bsamuels453@gmail.com>
2013-07-07 19:53:08 -0700

    removing model scale & adding json ctor

--- Forge.Core/Physics/ProjectileAttributes.cs
+++ Forge.Core/Physics/ProjectileAttributes.cs
@@ -1,18 +1,42 @@
-﻿namespace Forge.Core.Physics{
+﻿#region
+
+using System;
+using Newtonsoft.Json.Linq;
+
+#endregion
+
+namespace Forge.Core.Physics{
     /// <summary>
     ///   This class is used to define the attributes of each projectile.
     /// </summary>
-    public struct ProjectileAttributes{
+    public struct ProjectileAttributes : IEquatable<ProjectileAttributes>{
         public readonly float Mass;
         public readonly string Model;
-        public readonly float ModelScale;
         public readonly float Radius;
 
-        public ProjectileAttributes(string model, float radius, float mass, float modelScale){
+        public ProjectileAttributes(string model, float radius, float mass){
             Model = model;
             Radius = radius;
             Mass = mass;
-            ModelScale = modelScale;
         }
+
+        public ProjectileAttributes(JObject jobj){
+            Model = jobj["Model"].ToObject<string>();
+            Radius = jobj["Radius"].ToObject<float>();
+            Mass = jobj["Mass"].ToObject<float>();
+        }
+
+        #region IEquatable<ProjectileAttributes> Members
+
+        public bool Equals(ProjectileAttributes other){
+            if (Mass == other.Mass &&
+                Model.Equals(other.Model) &&
+                    Radius == other.Radius){
+                return true;
+            }
+            return false;
+        }
+
+        #endregion
     }
 }
