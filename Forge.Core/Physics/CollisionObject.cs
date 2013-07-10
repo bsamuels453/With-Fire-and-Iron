79485d8c28e2ffb9912b28ae826e5f16eac2b111
Ben Samuels <bsamuels453@gmail.com>
2013-07-10 16:58:24 -0700

    adding normal field to collisionobject

--- Forge.Core/Physics/CollisionObject.cs
+++ Forge.Core/Physics/CollisionObject.cs
@@ -12,6 +12,7 @@ namespace Forge.Core.Physics{
     public struct CollisionObject{
         public Vector3 Centroid;
         public int Id;
+        public Vector3 Normal;
         public Vector3[] Vertexes;
 
         public CollisionObject(int id, Vector3[] vertexes){
@@ -23,6 +24,10 @@ namespace Forge.Core.Physics{
                 Centroid += vertex;
             }
             Centroid /= vertexes.Length;
+            Vector3 crossSum = new Vector3();
+            crossSum += Vector3.Cross(vertexes[1] - vertexes[0], vertexes[2] - vertexes[0]);
+            crossSum.Normalize();
+            Normal = crossSum;
         }
 
         public bool IsInRange(Vector3 target, float range){
