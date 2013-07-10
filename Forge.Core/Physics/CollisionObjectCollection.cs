027ce09236e42fc0dd5121c00acceefee89766dd
Ben Samuels <bsamuels453@gmail.com>
2013-07-10 14:00:48 -0700

    moving collisioncallback to collisionobjectcollection

--- Forge.Core/Physics/CollisionObjectCollection.cs
+++ Forge.Core/Physics/CollisionObjectCollection.cs
@@ -6,23 +6,26 @@ using MonoGameUtility;
 #endregion
 
 namespace Forge.Core.Physics{
+    public delegate void CollisionCallback(int id, Vector3 localCollisionPoint, Ray localCollisionRay, Ray globalCollisionRay);
+
     /// <summary>
     /// used to group together collision objects, such as the plates on the side of an airship, into one datum.
     /// </summary>
     public class CollisionObjectCollection{
         public readonly List<Projectile> BlacklistedProjectiles;
-        public readonly CollisionObject[] CollisionObjects;
-        public readonly int FactionId;
-        public readonly BoundingSphere ShipSOI;
 
         /// <summary>
         ///   Position of target sphere, velocity of projectile relative to sphere Implement projectile relative speed multiplier here
         /// </summary>
-        public readonly ProjectilePhysics.CollisionCallback CollisionEventDispatcher;
+        public readonly CollisionCallback CollisionEventDispatcher;
+
+        public readonly CollisionObject[] CollisionObjects;
+        public readonly int FactionId;
+        public readonly BoundingSphere ShipSOI;
 
         public Matrix WorldTransform;
 
-        public CollisionObjectCollection(CollisionObject[] collisionObjects, int factionId, BoundingSphere soi, ProjectilePhysics.CollisionCallback collisionEventDispatcher){
+        public CollisionObjectCollection(CollisionObject[] collisionObjects, int factionId, BoundingSphere soi, CollisionCallback collisionEventDispatcher){
             CollisionObjects = collisionObjects;
             FactionId = factionId;
             WorldTransform = Matrix.Identity;
