f7335cac6ff6ea534febf3ddb945392909336486
Ben Samuels <bsamuels453@gmail.com>
2013-07-14 23:41:42 -0700

    updating hulldatamanager for some of the new ui subsystem interfaces

--- Forge.Core/ObjectEditor/HullDataManager.cs
+++ Forge.Core/ObjectEditor/HullDataManager.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using Forge.Core.Airship.Data;
-using Forge.Core.Airship.Generation;
+using Forge.Core.Airship.Export;
 using Forge.Core.Logic;
 using Forge.Core.ObjectEditor.Tools;
 using Forge.Framework.Draw;
@@ -25,51 +25,47 @@ namespace Forge.Core.ObjectEditor{
         #endregion
 
         public readonly Vector3 CenterPoint;
-
         public readonly float DeckHeight;
         public readonly DeckSectionContainer DeckSectionContainer;
         public readonly HullSectionContainer HullSectionContainer;
         public readonly int NumDecks;
         public readonly ObjectModelBuffer<AirshipObjectIdentifier>[] ObjectBuffers;
-        //public readonly ObjectBuffer<WallSegmentIdentifier>[] WallBuffers;
-        //public readonly List<WallSegmentIdentifier>[] WallIdentifiers;
+        public readonly ObjectBuffer<WallSegmentIdentifier>[] WallBuffers;
+        public readonly List<WallSegmentIdentifier>[] WallIdentifiers;
         public readonly float WallResolution;
         int _curDeck;
         bool _disposed;
 
-        public HullDataManager(HullGeometryInfo geometryInfo){
-            NumDecks = geometryInfo.NumDecks;
+        public HullDataManager(AirshipPackager.AirshipSerializationStruct data){
+            NumDecks = data.ModelAttributes.NumDecks;
             VisibleDecks = NumDecks;
-            DeckSectionContainer = geometryInfo.DeckSectionContainer;
-            DeckHeight = geometryInfo.DeckHeight;
-            WallResolution = geometryInfo.WallResolution;
-            CenterPoint = geometryInfo.CenterPoint;
-            HullSectionContainer = geometryInfo.HullSections;
-
+            DeckSectionContainer = new DeckSectionContainer(data.DeckSections);
+            DeckHeight = data.ModelAttributes.DeckHeight;
+            WallResolution = 0.5f;
+            CenterPoint = data.ModelAttributes.Centroid;
+            HullSectionContainer = new HullSectionContainer(data.HullSections);
 
             ObjectBuffers = new ObjectModelBuffer<AirshipObjectIdentifier>[NumDecks];
-
             for (int i = 0; i < ObjectBuffers.Count(); i++){
-                ObjectBuffers[i] = new ObjectModelBuffer<AirshipObjectIdentifier>(100, "Shader_TintedModel");
+                ObjectBuffers[i] = new ObjectModelBuffer<AirshipObjectIdentifier>(100, "Config/Shaders/TintedModel.config");
             }
-            throw new Exception();
-            /*
+
             WallBuffers = new ObjectBuffer<WallSegmentIdentifier>[NumDecks];
             for (int i = 0; i < WallBuffers.Count(); i++){
                 int potentialWalls = DeckSectionContainer.DeckVertexesByDeck[i].Count()*2;
-                WallBuffers[i] = new ObjectBuffer<WallSegmentIdentifier>(potentialWalls, 10, 20, 30, "Shader_AirshipWalls");
+                WallBuffers[i] = new ObjectBuffer<WallSegmentIdentifier>(potentialWalls, 10, 20, 30, "Config/Shaders/Airship_InternalWalls.config");
             }
 
             WallIdentifiers = new List<WallSegmentIdentifier>[NumDecks];
             for (int i = 0; i < WallIdentifiers.Length; i++){
                 WallIdentifiers[i] = new List<WallSegmentIdentifier>();
             }
-             */
+
             CurDeck = 0;
         }
 
-        //public ObjectBuffer<WallSegmentIdentifier> CurWallBuffer { get; private set; }
-        //public List<WallSegmentIdentifier> CurWallIdentifiers { get; private set; }
+        public ObjectBuffer<WallSegmentIdentifier> CurWallBuffer { get; private set; }
+        public List<WallSegmentIdentifier> CurWallIdentifiers { get; private set; }
         public ObjectModelBuffer<AirshipObjectIdentifier> CurObjBuffer { get; private set; }
 
         public int VisibleDecks { get; private set; }
@@ -80,20 +76,23 @@ namespace Forge.Core.ObjectEditor{
                 //higher curdeck means a lower deck is displayed
                 //low curdeck means higher deck displayed
                 //highest deck is 0
-                int diff = -(value - _curDeck);
+
+                if (value < 0)
+                    value = 0;
+                if (value >= NumDecks)
+                    value = NumDecks - 1;
+
                 int oldDeck = _curDeck;
+                int diff = -(value - _curDeck);
                 VisibleDecks += diff;
                 _curDeck = value;
-                throw new Exception();
-                /*
+
+                HullSectionContainer.SetTopVisibleDeck(_curDeck);
+                DeckSectionContainer.SetTopVisibleDeck(_curDeck);
+
                 CurWallBuffer = WallBuffers[_curDeck];
                 CurWallIdentifiers = WallIdentifiers[_curDeck];
-                 */
                 CurObjBuffer = ObjectBuffers[_curDeck];
-
-                foreach (var buffer in ObjectBuffers){
-                    buffer.Enabled = false;
-                }
 
                 for (int i = _curDeck; i < NumDecks; i++){
                     ObjectBuffers[i].Enabled = true;
@@ -109,12 +108,10 @@ namespace Forge.Core.ObjectEditor{
 
         public void Dispose(){
             Debug.Assert(!_disposed);
-            throw new Exception();
-            /*
+
             foreach (var buffer in WallBuffers){
                 buffer.Dispose();
             }
-             */
             foreach (var buffer in ObjectBuffers){
                 buffer.Dispose();
             }
@@ -128,13 +125,11 @@ namespace Forge.Core.ObjectEditor{
         public event CurDeckChanged OnCurDeckChange;
 
         public void MoveUpOneDeck(){
-            CurDeck = HullSectionContainer.SetTopVisibleDeck(CurDeck - 1);
-            DeckSectionContainer.SetTopVisibleDeck(CurDeck);
+            CurDeck--;
         }
 
         public void MoveDownOneDeck(){
-            CurDeck = HullSectionContainer.SetTopVisibleDeck(CurDeck + 1);
-            DeckSectionContainer.SetTopVisibleDeck(CurDeck);
+            CurDeck++;
         }
 
         ~HullDataManager(){
