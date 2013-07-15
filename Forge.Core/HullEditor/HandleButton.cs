fe10f0768999f04ec4e48a384291ced7405b8f42
Ben Samuels <bsamuels453@gmail.com>
2013-07-14 17:53:40 -0700

    implementing placeholder button

---
+++ Forge.Core/HullEditor/HandleButton.cs
@@ -0,0 +1,31 @@
+﻿#region
+
+using Forge.Framework.Control;
+using Forge.Framework.UI;
+using MonoGameUtility;
+
+#endregion
+
+namespace Forge.Core.HullEditor{
+    internal class HandleButton : UIElementCollection{
+        public HandleButton(MouseManager mouseManager) : base(mouseManager){
+        }
+
+        public HandleButton(UIElementCollection parent, FrameStrata.Level depth, Rectangle boundingBox, string alias) : base(parent, depth, boundingBox, alias){
+        }
+
+        public Vector2 CentPosition{
+            get{
+                float x = base.BoundingBox.X + base.BoundingBox.Width/2f;
+                float y = base.BoundingBox.Y + base.BoundingBox.Height/2f;
+                return new Vector2(x, y);
+            }
+            set{
+                float newX = value.X -= base.BoundingBox.Width/2f;
+                float newY = value.Y -= base.BoundingBox.Height/2f;
+                base.X = (int) newX;
+                base.Y = (int) newY;
+            }
+        }
+    }
+}
