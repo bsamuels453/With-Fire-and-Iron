1ab40b1a63a5eaa8592c5e31a463f8e7975afd88
Ben Samuels <bsamuels453@gmail.com>
2013-07-18 13:13:07 -0700

    making AddGridElement protected

--- Forge.Framework/UI/Elements/ElementGrid.cs
+++ Forge.Framework/UI/Elements/ElementGrid.cs
@@ -8,7 +8,7 @@ using MonoGameUtility;
 
 namespace Forge.Framework.UI.Elements{
     public class ElementGrid : DraggableCollection{
-        readonly IUIElement[,] _elements;
+        protected readonly IUIElement[,] Elements;
         readonly int _gridInsetX;
         readonly int _gridInsetY;
         readonly int _horizPadding;
@@ -36,30 +36,38 @@ namespace Forge.Framework.UI.Elements{
             int width = horizItems*_itemWidth + (horizItems + 1)*_horizPadding + _gridInsetX*2;
             int height = vertItems*_itemHeight + (vertItems + 1)*_vertPadding + _gridInsetY*2;
 
-            _elements = new IUIElement[horizItems,vertItems];
+            Elements = new IUIElement[horizItems,vertItems];
 
             base.BoundingBox = new Rectangle(position.X, position.Y, width, height);
         }
 
-        public void AddGridElement(UIElementCollection element, int x, int y){
-            element.X = base.BoundingBox.X + _gridInsetX + x*_itemWidth + (x + 1)*_horizPadding;
-            element.Y = base.BoundingBox.Y + _gridInsetY + y*_itemHeight + (y + 1)*_vertPadding;
+        protected void AddGridElement(UIElementCollection element, int x, int y){
+            var pos = CalculateElementPosition(x, y);
+            element.X = pos.X;
+            element.Y = pos.Y;
 
             Debug.Assert(element.Width == _itemWidth);
             Debug.Assert(element.Height == _itemHeight);
 
-            _elements[x, y] = element;
+            Elements[x, y] = element;
         }
 
-        public void AddGridElement(IUIElement element, int x, int y){
-            element.X = base.BoundingBox.X + _gridInsetX + x*_itemWidth + (x + 1)*_horizPadding;
-            element.Y = base.BoundingBox.Y + _gridInsetY + y*_itemHeight + (y + 1)*_vertPadding;
+        protected void AddGridElement(IUIElement element, int x, int y){
+            var pos = CalculateElementPosition(x, y);
+            element.X = pos.X;
+            element.Y = pos.Y;
 
             Debug.Assert(element.Width == _itemWidth);
             Debug.Assert(element.Height == _itemHeight);
 
             this.AddElement(element);
-            _elements[x, y] = element;
+            Elements[x, y] = element;
+        }
+
+        protected Point CalculateElementPosition(int cellX, int cellY){
+            int x = base.BoundingBox.X + _gridInsetX + cellX*_itemWidth + (cellX + 1)*_horizPadding;
+            int y = base.BoundingBox.Y + _gridInsetY + cellY*_itemHeight + (cellY + 1)*_vertPadding;
+            return new Point(x, y);
         }
     }
 }
