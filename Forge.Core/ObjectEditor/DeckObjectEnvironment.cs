d3de462d2dffc207f9f536a20b675c1445d6c94d
Ben Samuels <bsamuels453@gmail.com>
2013-07-19 15:49:06 -0700

    implementing grid limit calculations

--- Forge.Core/ObjectEditor/DeckObjectEnvironment.cs
+++ Forge.Core/ObjectEditor/DeckObjectEnvironment.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using MonoGameUtility;
 
 #endregion
@@ -12,9 +13,18 @@ namespace Forge.Core.ObjectEditor{
     /// </summary>
     internal class DeckObjectEnvironment{
         /// <summary>
+        /// Represents the limits of the silhouette that make up the outline of the airship deck.
+        /// This is used to prevent objects from being placed in the "rectangle" that makes up the
+        /// occupationGrid while being outside the actual limits of  the hull.
+        /// </summary>
+        readonly int[][] _gridLimitMax;
+
+        readonly int[][] _gridLimitMin;
+
+        /// <summary>
         /// These offsets are used to convert from model space to grid space.
         /// </summary>
-        readonly Vector2[] _gridOffsets;
+        readonly Point[] _gridOffsets;
 
         /// <summary>
         /// Tables of booleans that represent whether the "area" the table maps to is occupied
@@ -29,13 +39,18 @@ namespace Forge.Core.ObjectEditor{
 
             //grid representing avail spots
             _occupationGrids = new bool[hullData.NumDecks][,];
-            _gridOffsets = new Vector2[hullData.NumDecks];
-            SetupObjectOccupationGrids(hullData.DeckSectionContainer.DeckVertexesByDeck);
+            _gridOffsets = new Point[hullData.NumDecks];
+
+            var vertexes = hullData.DeckSectionContainer.DeckVertexesByDeck;
+
+            SetupObjectOccupationGrids(vertexes);
+            CalculateGridLimits(vertexes, out _gridLimitMax, out _gridLimitMin);
+            int gfgf = 4;
         }
 
         Point ConvertToGridSpace(Vector3 modelSpacePos, int deck){
             modelSpacePos *= 2;
-            Vector2 offset = _gridOffsets[deck];
+            Point offset = _gridOffsets[deck];
             int gridX = (int) (offset.X + modelSpacePos.X);
             int gridZ = (int) (offset.Y + modelSpacePos.Z);
             return new Point(gridX, gridZ);
@@ -59,10 +74,42 @@ namespace Forge.Core.ObjectEditor{
 
                 var layerLength = (int) ((maxX - minX)*2);
                 var layerWidth = (int) ((maxZ)*4);
-                _gridOffsets[i] = new Vector2(minX, maxZ);
+                _gridOffsets[i] = new Point((int) (minX*2), (int) (maxZ*2));
 
                 var grid = new bool[layerLength,layerWidth];
                 _occupationGrids[i] = grid;
+            }
+        }
+
+        void CalculateGridLimits(List<Vector3>[] vertexes, out int[][] gridLimitMax, out int[][] gridLimitMin){
+            gridLimitMax = new int[vertexes.Length][];
+            gridLimitMin = new int[vertexes.Length][];
+            for (int deck = 0; deck < vertexes.Length; deck++){
+                var layerVerts = vertexes[deck];
+
+                var vertsByRow =
+                    from vert in layerVerts
+                    where vert.Z >= 0
+                    group vert by vert.X;
+
+                var sortedVertsByRow = (
+                    from pairing in vertsByRow
+                    orderby pairing.Key ascending
+                    select pairing
+                    ).ToArray();
+
+                var layerLimitMin = new int[sortedVertsByRow.Length];
+                var layerLimitMax = new int[sortedVertsByRow.Length];
+
+                var rowArrayMax = _occupationGrids[deck].GetLength(1);
+                for (int row = 0; row < sortedVertsByRow.Length; row++){
+                    float max = sortedVertsByRow[row].Max(v => v.Z);
+                    var converted = ConvertToGridSpace(new Vector3(0, 0, -max), deck).Y;
+                    layerLimitMin[row] = converted;
+                    layerLimitMax[row] = rowArrayMax - converted;
+                }
+                gridLimitMax[deck] = layerLimitMax;
+                gridLimitMin[deck] = layerLimitMin;
             }
         }
 
