02c035f8b8c73abdeaf3eedd62b15147bf56a091
Ben Samuels <bsamuels453@gmail.com>
2013-07-18 20:26:20 -0700

    redoing objectidentifier to not be uid based

--- Forge.Core/ObjectEditor/ObjectIdentifier.cs
+++ Forge.Core/ObjectEditor/ObjectIdentifier.cs
@@ -1,6 +1,7 @@
 ﻿#region
 
 using System;
+using MonoGameUtility;
 
 #endregion
 
@@ -10,24 +11,36 @@ namespace Forge.Core.ObjectEditor{
     ///the object editor uses this to identify objects to be added/removed from the environment
     /// </summary>
     public class ObjectIdentifier : IEquatable<ObjectIdentifier>{
-        static long _maxUid;
+        public readonly int Deck;
+        public readonly Point Origin;
 
-        readonly long _uid;
-
-        static ObjectIdentifier(){
-            _maxUid = 0;
+        /// <summary>
+        /// Initialize the identifier using the deck and scaled origin.
+        /// Scaled origin refers to an origin in model space, scaled so that
+        /// its coordinates are integers.
+        /// </summary>
+        /// <param name="origin"></param>
+        /// <param name="deck"></param>
+        public ObjectIdentifier(Point origin, int deck){
+            Origin = origin;
+            Deck = deck;
         }
 
-        public ObjectIdentifier(){
-            _uid = _maxUid;
-            _maxUid++;
+        public ObjectIdentifier(Vector3 origin, int deck){
+            //deck plate step is 0.5f
+            var scaled = new Point((int) (origin.X*2), (int) (origin.Y*2));
+            Origin = scaled;
+            Deck = deck;
         }
 
         #region IEquatable<ObjectIdentifier> Members
 
         public bool Equals(ObjectIdentifier other){
-            if (_uid == other._uid)
-                return true;
+            if (Origin == other.Origin){
+                if (Deck == other.Deck){
+                    return true;
+                }
+            }
             return false;
         }
 
