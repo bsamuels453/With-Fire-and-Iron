fc1bc5f06b5a118c342f47383266dab43eaf634f
Ben Samuels <bsamuels453@gmail.com>
2013-07-19 17:40:06 -0700

    updating placement tool for compatibility with new object environment subsystem

--- Forge.Core/ObjectEditor/Tools/DeckObjectPlacementTool.cs
+++ Forge.Core/ObjectEditor/Tools/DeckObjectPlacementTool.cs
@@ -1,13 +1,12 @@
 ﻿#region
 
 using System.Collections.Generic;
-using System.Diagnostics;
 using Forge.Framework.Draw;
 using Forge.Framework.Resources;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using BoundingBox = MonoGameUtility.BoundingBox;
 using Matrix = MonoGameUtility.Matrix;
+using Point = MonoGameUtility.Point;
 using Vector3 = MonoGameUtility.Vector3;
 
 #endregion
@@ -17,18 +16,18 @@ namespace Forge.Core.ObjectEditor.Tools{
     /// Tool for placing generic objects on the airship's deck.
     /// </summary>
     internal class DeckObjectPlacementTool : DeckPlacementBase{
+        readonly DeckObjectEnvironment _deckObjectEnvironment;
         readonly ObjectModelBuffer<int> _ghostedObjectModel;
-        readonly HullDataManager _hullData;
-        readonly float _objectGridLength;
-        readonly float _objectGridWidth;
+        readonly HullEnvironment _hullData;
+        readonly Point _objectGridDims;
         readonly string _objectModelName;
 
-        public DeckObjectPlacementTool(HullDataManager hullData, string objectModel, float objectGridWidth, float objectGridLength) :
+        public DeckObjectPlacementTool(HullEnvironment hullData, DeckObjectEnvironment deckObjectEnvironment, string objectModel, Point objectGridDims) :
             base(hullData){
-            _objectGridWidth = objectGridWidth;
-            _objectGridLength = objectGridLength;
+            _objectGridDims = objectGridDims;
             _objectModelName = objectModel;
             _hullData = hullData;
+            _deckObjectEnvironment = deckObjectEnvironment;
 
             _ghostedObjectModel = new ObjectModelBuffer<int>(1, "Config/Shaders/TintedModel.config");
             _ghostedObjectModel.AddObject(0, Resource.LoadContent<Model>(_objectModelName), Matrix.Identity);
@@ -59,39 +58,19 @@ namespace Forge.Core.ObjectEditor.Tools{
         }
 
         protected override void HandleCursorRelease(){
-            var identifier = new ObjectIdentifier();
-
-            //Matrix trans = Matrix.CreateRotationX((float)-Math.PI / 2) * Matrix.CreateRotationY((float)-Math.PI / 2) * Matrix.CreateTranslation(CursorPosition);
-            Matrix trans = Matrix.Identity*Matrix.CreateTranslation(CursorPosition);
-            _hullData.CurObjBuffer.AddObject(identifier, Resource.LoadContent<Model>(_objectModelName), trans);
-
-            var quadsToHide = new List<ObjectIdentifier>();
-            var upperBoxesToHide = new List<BoundingBox>();
-            var lowerBoxesToHide = new List<BoundingBox>();
-            for (float x = 0; x < _objectGridWidth; x += GridResolution){
-                for (float z = 0; z < _objectGridLength; z += GridResolution){
-                    var min = CursorPosition + new Vector3(x, _hullData.DeckHeight, z);
-                    quadsToHide.Add(new ObjectIdentifier());
-                    upperBoxesToHide.Add(new BoundingBox(min, min + new Vector3(GridResolution, 0, GridResolution)));
-                    lowerBoxesToHide.Add
-                        (new BoundingBox(CursorPosition + new Vector3(x, 0, z), CursorPosition + new Vector3(x + GridResolution, 0, z + GridResolution)));
-                }
-            }
-            if (HullData.CurDeck != 0){
-                foreach (var quad in quadsToHide){
-                    /*
-                    bool b = _hullData.DeckSectionContainer.DeckBufferByDeck[_hullData.CurDeck - 1].DisableObject(quad);
-                    Debug.Assert(b);
-                     */
-                }
-                foreach (var bbox in upperBoxesToHide){
-                    _hullData.DeckSectionContainer.BoundingBoxesByDeck[_hullData.CurDeck - 1].Remove(bbox);
-                }
-            }
-            foreach (var bbox in lowerBoxesToHide){
-                _hullData.DeckSectionContainer.BoundingBoxesByDeck[_hullData.CurDeck].Remove(bbox);
-            }
-            GenerateGuideGrid();
+            _deckObjectEnvironment.AddObject
+                (
+                    _objectModelName,
+                    CursorPosition,
+                    _objectGridDims,
+                    _hullData.CurDeck,
+                    DeckObjectEnvironment.ObjectSideEffects.CutsIntoCeiling
+                );
+            //This used to be used to regenerate grid to remove grid lines obfuscated/covered by objects.
+            //For now, the deckobjectenvironment doesn't mess with the deck's bounding boxes because it
+            //would create ridic coupling issues. Leaving this here though as a footnote to keep in mind
+            //a clean way to do this in the future.
+            //GenerateGuideGrid(); 
         }
 
         protected override void HandleCursorDown(){
@@ -113,17 +92,7 @@ namespace Forge.Core.ObjectEditor.Tools{
         }
 
         protected override bool IsCursorValid(Vector3 newCursorPos, Vector3 prevCursorPosition, List<Vector3> deckFloorVertexes, float distToPt){
-            bool validCursor = true;
-            for (float x = 0; x <= _objectGridWidth; x += GridResolution){
-                for (float z = 0; z <= _objectGridLength; z += GridResolution){
-                    var vert = newCursorPos + new Vector3(x, 0, z);
-                    if (!deckFloorVertexes.Contains(vert)){
-                        validCursor = false;
-                        break;
-                    }
-                }
-            }
-            return validCursor;
+            return _deckObjectEnvironment.IsObjectPlacementValid(newCursorPos, _objectGridDims, _hullData.CurDeck, true);
         }
     }
 }
