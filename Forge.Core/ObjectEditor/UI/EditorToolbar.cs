6ba83da415c3078817ebabe45e0085e587686e7b
Ben Samuels <bsamuels453@gmail.com>
2013-07-22 14:54:03 -0700

    reimplementing simple cannon construction

--- Forge.Core/ObjectEditor/UI/EditorToolbar.cs
+++ Forge.Core/ObjectEditor/UI/EditorToolbar.cs
@@ -11,6 +11,7 @@ using MonoGameUtility;
 namespace Forge.Core.ObjectEditor.UI{
     internal class EditorToolbar : ElementGrid{
         const string _template = "UiTemplates/Specialized/ObjectEditorToolbar.json";
+        readonly ToolbarButton _buildCannonBut;
 
         readonly ToolbarButton _buildLadderBut;
         readonly ToolbarButton _buildWallBut;
@@ -29,20 +30,24 @@ namespace Forge.Core.ObjectEditor.UI{
             var wallBuildTex = jobj["BuildWallTex"].ToObject<string>();
             var wallDeleteTex = jobj["DeleteWallTex"].ToObject<string>();
             var ladderTex = jobj["BuildLadderTex"].ToObject<string>();
+            var cannonTex = jobj["BuildCannonTex"].ToObject<string>();
 
             _buildWallBut = new ToolbarButton(this, FrameStrata.Level.Medium, new Point(), wallBuildTex);
             _deleteWallBut = new ToolbarButton(this, FrameStrata.Level.Medium, new Point(), wallDeleteTex);
             _buildLadderBut = new ToolbarButton(this, FrameStrata.Level.Medium, new Point(), ladderTex);
+            _buildCannonBut = new ToolbarButton(this, FrameStrata.Level.Medium, new Point(), cannonTex);
 
             base.AddGridElement(_buildWallBut, 0, 0);
             base.AddGridElement(_deleteWallBut, 0, 1);
             base.AddGridElement(_buildLadderBut, 0, 2);
+            base.AddGridElement(_buildCannonBut, 0, 3);
 
-            _tools = new IToolbarTool[3];
+            _tools = new IToolbarTool[4];
 
             _tools[(int) Tools.BuildWall] = new WallBuildTool(hullEnv, wallEnv);
             _tools[(int) Tools.DeleteWall] = new WallDeleteTool(hullEnv, wallEnv);
             _tools[(int) Tools.BuildLadder] = new DeckObjectPlacementTool(hullEnv, deckObjEnv, "Models/Ladder", new XZPoint(2, 2));
+            _tools[(int) Tools.BuildCannon] = new DeckObjectPlacementTool(hullEnv, deckObjEnv, "Models/Cannon", new XZPoint(2, 5));
 
             InitializeToolEvents();
         }
@@ -75,6 +80,16 @@ namespace Forge.Core.ObjectEditor.UI{
                         _tools[(int) Tools.BuildLadder].Enabled = true;
                     }
                 };
+
+            _buildCannonBut.OnLeftClick +=
+                (state, f, arg3) =>{
+                    if (arg3.ContainsMouse){
+                        foreach (var tool in _tools){
+                            tool.Enabled = false;
+                        }
+                        _tools[(int) Tools.BuildCannon].Enabled = true;
+                    }
+                };
         }
 
         public override void Dispose(){
@@ -89,7 +104,8 @@ namespace Forge.Core.ObjectEditor.UI{
         enum Tools{
             BuildWall,
             DeleteWall,
-            BuildLadder
+            BuildLadder,
+            BuildCannon
         }
 
         #endregion
