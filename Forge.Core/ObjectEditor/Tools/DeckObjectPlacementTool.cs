dff7624f26db3827624bd7f282054273ba87cc79
Ben Samuels <bsamuels453@gmail.com>
2013-07-22 18:32:32 -0700

    implementing object cursor offset

--- Forge.Core/ObjectEditor/Tools/DeckObjectPlacementTool.cs
+++ Forge.Core/ObjectEditor/Tools/DeckObjectPlacementTool.cs
@@ -7,7 +7,6 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using MonoGameUtility;
 using Matrix = MonoGameUtility.Matrix;
-using Point = MonoGameUtility.Point;
 using Vector3 = MonoGameUtility.Vector3;
 
 #endregion
@@ -22,26 +21,30 @@ namespace Forge.Core.ObjectEditor.Tools{
         readonly HullEnvironment _hullData;
         readonly XZPoint _objectGridDims;
         readonly string _objectModelName;
+        protected Vector3 CursorOffset;
+        protected Matrix ObjectRotTransorm;
 
         public GameObjectEnvironment.SideEffect PlacementSideEffect;
-        Matrix _rotTransform;
 
         public DeckObjectPlacementTool(
             HullEnvironment hullData,
             GameObjectEnvironment gameObjectEnvironment,
             string objectModel,
             XZPoint objectGridDims,
-            GameObjectEnvironment.SideEffect placementSideEffects,
-            Matrix? objTransform = null) :
+            GameObjectEnvironment.SideEffect placementSideEffects) :
                 base(hullData){
             _objectGridDims = objectGridDims;
             _objectModelName = objectModel;
             _hullData = hullData;
             _gameObjectEnvironment = gameObjectEnvironment;
+            PlacementSideEffect = placementSideEffects;
+            CursorOffset = new Vector3(-objectGridDims.X/4f, 0, -objectGridDims.Z/4f);
 
             _ghostedObjectModel = new ObjectModelBuffer<int>(1, "Config/Shaders/TintedModel.config");
             _ghostedObjectModel.AddObject(0, Resource.LoadContent<Model>(_objectModelName), Matrix.Identity);
             _ghostedObjectModel.Enabled = false;
+
+            ObjectRotTransorm = Matrix.Identity;
         }
 
         protected override void EnableCursorGhost(){
@@ -61,7 +64,8 @@ namespace Forge.Core.ObjectEditor.Tools{
         }
 
         protected override void UpdateCursorGhost(){
-            _ghostedObjectModel.TransformAll(base.CursorPosition);
+            var translation = Matrix.CreateTranslation(base.CursorPosition + CursorOffset);
+            _ghostedObjectModel.SetObjectTransform(0, ObjectRotTransorm*translation);
         }
 
         protected override void HandleCursorChange(bool isDrawing){
@@ -71,10 +75,11 @@ namespace Forge.Core.ObjectEditor.Tools{
             _gameObjectEnvironment.AddObject
                 (
                     _objectModelName,
-                    CursorPosition,
+                    CursorPosition + CursorOffset,
                     _objectGridDims,
                     _hullData.CurDeck,
-                    DeckObjectEnvironment.SideEffect.CutsIntoCeiling
+                    ObjectRotTransorm,
+                    PlacementSideEffect
                 );
             //This used to be used to regenerate grid to remove grid lines obfuscated/covered by objects.
             //For now, the deckobjectenvironment doesn't mess with the deck's bounding boxes because it
@@ -102,7 +107,7 @@ namespace Forge.Core.ObjectEditor.Tools{
         }
 
         protected override bool IsCursorValid(Vector3 newCursorPos, Vector3 prevCursorPosition, List<Vector3> deckFloorVertexes, float distToPt){
-            return _gameObjectEnvironment.IsObjectPlacementValid(newCursorPos, _objectGridDims, _hullData.CurDeck, PlacementSideEffect);
+            return _gameObjectEnvironment.IsObjectPlacementValid(newCursorPos + CursorOffset, _objectGridDims, _hullData.CurDeck, PlacementSideEffect);
         }
     }
 }
