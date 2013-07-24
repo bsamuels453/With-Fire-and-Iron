291fba3fbd3b21a5794bb0088dfd4c97de5c3783
Ben Samuels <bsamuels453@gmail.com>
2013-07-24 13:36:22 -0700

    adding additional gameobject fields

--- Forge.Core/ObjectEditor/GameObject.cs
+++ Forge.Core/ObjectEditor/GameObject.cs
@@ -6,29 +6,38 @@ using MonoGameUtility;
 #endregion
 
 namespace Forge.Core.ObjectEditor{
-    internal struct GameObject : IEquatable<ObjectIdentifier>{
+    public struct GameObject : IEquatable<ObjectIdentifier>{
         public readonly int Deck;
         public readonly XZPoint GridDimensions;
         public readonly ObjectIdentifier Identifier;
+        public readonly string ModelName;
+        public readonly Vector3 ModelspacePosition;
         public readonly long ObjectUid;
         public readonly XZPoint Position;
+        public readonly float Rotation;
         public readonly GameObjectEnvironment.SideEffect SideEffect;
         public readonly GameObjectType Type;
 
         public GameObject(
-            ObjectIdentifier identifier,
+            Vector3 modelspacePosition,
+            int deck,
             XZPoint gridDimensions,
-            XZPoint gridPosition,
             GameObjectEnvironment.SideEffect sideEffect,
             long objectUid,
-            int deck, GameObjectType type){
-            Identifier = identifier;
+            GameObjectType type,
+            string modelName,
+            float rotation
+            ){
+            Identifier = new ObjectIdentifier(modelspacePosition, deck);
             GridDimensions = gridDimensions;
-            Position = gridPosition;
+            Position = Identifier.Origin;
             SideEffect = sideEffect;
             ObjectUid = objectUid;
             Deck = deck;
             Type = type;
+            ModelName = modelName;
+            Rotation = rotation;
+            ModelspacePosition = modelspacePosition;
         }
 
         #region IEquatable<ObjectIdentifier> Members
