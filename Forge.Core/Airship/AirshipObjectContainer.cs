44d9b009450448a80c4c1cc08805dce93ed33efc
Ben Samuels <bsamuels453@gmail.com>
2013-07-24 17:31:27 -0700

    adding airshipobjectcontainer interface

---
+++ Forge.Core/Airship/AirshipObjectContainer.cs
@@ -0,0 +1,33 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using Forge.Core.ObjectEditor;
+using MonoGameUtility;
+
+#endregion
+
+namespace Forge.Core.Airship{
+    internal class AirshipObjectContainer : IDisposable{
+        public AirshipObjectContainer(List<GameObject> gameObjects){
+        }
+
+        public Matrix WorldTransform { get; set; }
+
+        #region IDisposable Members
+
+        public void Dispose(){
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        public void SetTopVisibleDeck(int deck){
+            throw new NotImplementedException();
+        }
+
+        public void Update(){
+            throw new NotImplementedException();
+        }
+    }
+}
