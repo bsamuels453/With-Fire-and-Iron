877808edfdc2fb6bac864f2bf4800666601f73ac
Ben Samuels <bsamuels453@gmail.com>
2013-07-25 15:55:56 -0700

    adding scale matrix field to projectile

--- Forge.Core/Physics/Projectile.cs
+++ Forge.Core/Physics/Projectile.cs
@@ -13,17 +13,19 @@ namespace Forge.Core.Physics{
 
         public readonly RigidBody Body;
         public readonly Func<Vector3> GetPosition;
+        public readonly Matrix Scale;
         public readonly long TimeCreationIndex;
         public readonly long Uid;
         readonly Action<Projectile> _terminate; //not sure when this is actually needed. might be better to do a timeout
         //public event Action<float, Vector3, Vector3> OnCollision; //theres no real reason for the projectile to care about OnCollision (yet)
 
-        public Projectile(RigidBody body, Func<Vector3> getPosition, Action<Projectile> terminate, long creationTime){
+        public Projectile(RigidBody body, Func<Vector3> getPosition, Action<Projectile> terminate, long creationTime, Matrix scale){
             Body = body;
             GetPosition = getPosition;
             _terminate = terminate;
             Uid = _maxUid;
             TimeCreationIndex = creationTime;
+            Scale = scale;
             _maxUid++;
         }
 
