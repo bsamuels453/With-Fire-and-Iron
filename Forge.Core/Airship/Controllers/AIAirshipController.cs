0c3aafd41d7cb0a85742740fc53b0e40ed823c19
Ben Samuels <bsamuels453@gmail.com>
2013-07-25 15:57:08 -0700

    passing weaponsystems to airship controller rather than hardpoint list

--- Forge.Core/Airship/Controllers/AIAirshipController.cs
+++ Forge.Core/Airship/Controllers/AIAirshipController.cs
@@ -1,6 +1,5 @@
 ﻿#region
 
-using System.Collections.Generic;
 using Forge.Core.Airship.Data;
 
 #endregion
@@ -10,8 +9,8 @@ namespace Forge.Core.Airship.Controllers{
     /// The controller used for the AI to control the airship.
     /// </summary>
     public class AIAirshipController : AirshipController{
-        public AIAirshipController(ModelAttributes modelData, AirshipStateData stateData, List<Hardpoint> hardPoints, AirshipIndexer airships) :
-            base(modelData, stateData, airships, hardPoints){
+        public AIAirshipController(ModelAttributes modelData, AirshipStateData stateData, WeaponSystems weaponSystems, AirshipIndexer airships) :
+            base(modelData, stateData, airships, weaponSystems){
         }
 
         protected override void UpdateController(double timeDelta){
