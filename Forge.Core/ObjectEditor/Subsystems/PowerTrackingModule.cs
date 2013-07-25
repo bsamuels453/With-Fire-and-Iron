b82ee308cc38b163e61ddb7aed2b0f2493728262
Ben Samuels <bsamuels453@gmail.com>
2013-07-24 17:19:08 -0700

    adding class files for tracking subsystems

---
+++ Forge.Core/ObjectEditor/Subsystems/PowerTrackingModule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Forge.Core.ObjectEditor.Subsystems {
+    /// <summary>
+    /// Tracks power consumption and other power systems on the airship. Reports
+    /// power statistics and warnings for the airship's power grid.
+    /// </summary>
+    class PowerTrackingModule {
+    }
+}
---
+++ Forge.Core/ObjectEditor/Subsystems/WeaponTrackingModule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Forge.Core.ObjectEditor.Subsystems {
+    /// <summary>
+    /// Tracks weapon systems currently placed in the airship. Generates statistics
+    /// about potential weapon performance and handles the grouping of weapons.
+    /// </summary>
+    class WeaponTrackingModule {
+    }
+}
