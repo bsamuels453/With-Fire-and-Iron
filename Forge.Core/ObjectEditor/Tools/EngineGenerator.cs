12581ffa9b08b0d3000057d4579b3abf2f2a9cbc
Ben Samuels <bsamuels453@gmail.com>
2013-07-31 13:57:03 -0700

    adding IZoningFiller and EngineGenerator classes

---
+++ Forge.Core/ObjectEditor/Tools/EngineGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using MonoGameUtility;
+using Vector3 = Microsoft.Xna.Framework.Vector3;
+
+namespace Forge.Core.ObjectEditor.Tools {
+    class EngineGenerator : IZoneFiller {
+        public void Dispose(){
+            //throw new NotImplementedException();
+        }
+
+        public Color TintColor {
+            get;
+            set;
+        }
+
+        public bool Enabled {
+            get;
+            set;
+        }
+
+        public void Reset(){
+            //throw new NotImplementedException();
+        }
+
+        public List<GameObject> ExtractGeneratedObjects(){
+            //throw new NotImplementedException();
+            return null;
+        }
+
+        public void FillZone(int deck, Vector3 modelspacePos, XZPoint dimensions){
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ Forge.Core/ObjectEditor/Tools/IZoneFiller.cs
@@ -0,0 +1,19 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using MonoGameUtility;
+using Vector3 = Microsoft.Xna.Framework.Vector3;
+
+#endregion
+
+namespace Forge.Core.ObjectEditor.Tools{
+    internal interface IZoneFiller : IDisposable{
+        Color TintColor { set; }
+        bool Enabled { get; set; }
+        void Reset();
+        List<GameObject> ExtractGeneratedObjects();
+        void FillZone(int deck, Vector3 modelspacePos, XZPoint dimensions);
+    }
+}
