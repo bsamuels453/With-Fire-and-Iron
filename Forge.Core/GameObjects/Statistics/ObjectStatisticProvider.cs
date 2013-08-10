c075cb25a133711e02104074012460c167ba71bf
Ben Samuels <bsamuels453@gmail.com>
2013-08-10 16:12:06 -0700

    implementing all of the get methods

--- Forge.Core/GameObjects/Statistics/ObjectStatisticProvider.cs
+++ Forge.Core/GameObjects/Statistics/ObjectStatisticProvider.cs
@@ -1,10 +1,11 @@
 #region
 
-using System;
+using System.Linq;
 using Forge.Core.ObjectEditor;
 using Forge.Framework.Resources;
 using Microsoft.Xna.Framework.Graphics;
 using MonoGameUtility;
+using Newtonsoft.Json.Linq;
 
 #endregion
 
@@ -21,7 +22,7 @@ namespace Forge.Core.GameObjects.Statistics{
     /// without changing the interface.
     /// </summary>
     public static class ObjectStatisticProvider{
-        static GenericObjectDef[] _gameObjects;
+        static readonly GenericObjectDef[] _gameObjects;
 
         static ObjectStatisticProvider(){
             _gameObjects = Resource.GameObjectLoader.LoadAllGameObjects();
@@ -30,24 +31,62 @@ namespace Forge.Core.GameObjects.Statistics{
         public static void Initialize(){
         }
 
-        public static XZPoint GetObjectDims(GameObjectFamily type, long uid){
-            throw new NotImplementedException();
+        static JObject GetObject(GameObjectFamily family, long uid){
+            return _gameObjects.Single(o => o.Family == (int) family && o.Uid == uid).JObject;
+        }
+
+        public static XZPoint GetObjectDims(GameObjectFamily family, long uid){
+            var obj = GetObject(family, uid);
+            return obj["Dimensions"].ToObject<XZPoint>();
         }
 
         public static Model[] GetModels(GameObjectFamily family, long uid){
-            throw new NotImplementedException();
+            var obj = GetObject(family, uid);
+            var modelStrs = obj["Model"].ToObject<string[]>();
+            var models = modelStrs.Select(Resource.LoadContent<Model>).ToArray();
+            return models;
         }
 
         public static GameObjectEnvironment.SideEffect[] GetSideEffects(GameObjectFamily family, long uid){
-            throw new NotImplementedException();
+            var obj = GetObject(family, uid);
+            var sideEffects = obj["SideEffects"].ToObject<GameObjectEnvironment.SideEffect[]>();
+            return sideEffects;
         }
 
         public static Texture2D GetIcon(GameObjectFamily family, long uid){
-            throw new NotImplementedException();
+            var obj = GetObject(family, uid);
+            var iconTex = obj["Icon"].ToObject<string>();
+            return Resource.LoadContent<Texture2D>(iconTex);
         }
 
         public static string GetName(GameObjectFamily family, long uid){
-            throw new NotImplementedException();
+            var obj = GetObject(family, uid);
+            var name = obj["Name"].ToObject<string>();
+            return name;
+        }
+
+        public static Vector3 GetProjectileEmitterOffset(GameObjectFamily family, long uid){
+            var obj = GetObject(family, uid);
+            var offset = obj["ProjectileEmitterOffset"].ToObject<Vector3>();
+            return offset;
+        }
+
+        public static float GetFiringForce(GameObjectFamily family, long uid){
+            var obj = GetObject(family, uid);
+            var firingForce = obj["FiringForce"].ToObject<float>();
+            return firingForce;
+        }
+
+        public static float GetMass(GameObjectFamily family, long uid){
+            var obj = GetObject(family, uid);
+            var mass = obj["Mass"].ToObject<float>();
+            return mass;
+        }
+
+        public static float GetRadius(GameObjectFamily family, long uid){
+            var obj = GetObject(family, uid);
+            var radius = obj["Radius"].ToObject<float>();
+            return radius;
         }
     }
 }
