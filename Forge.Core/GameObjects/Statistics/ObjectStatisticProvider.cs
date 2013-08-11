6dafd6c4ff7a8a0b896e437b69168e9f36d93ec5
Ben Samuels <bsamuels453@gmail.com>
2013-08-10 17:02:07 -0700

    updating docs

--- Forge.Core/GameObjects/Statistics/ObjectStatisticProvider.cs
+++ Forge.Core/GameObjects/Statistics/ObjectStatisticProvider.cs
@@ -12,9 +12,9 @@ using Newtonsoft.Json.Linq;
 namespace Forge.Core.GameObjects.Statistics{
     /// <summary>
     /// Used to load statistics about gameobjects. Essentially, GameObject class only contains identifying
-    /// information for what the object is, in addition to any data specific to that object such as rotation and position.
+    /// information for what the object is and any data -specific- to that instantiation of the object such as position, rotation, etc.
     /// This provider is used to provide additional data about gameobjects so that we can extend gameobject functionality
-    /// without changing the data struct. This class will provide information such as dimensions, access points, and any
+    /// without changing the gameobject data struct or adding new ones. This class will provide information such as dimensions, access points, and any
     /// other information that will be identical across objects of the same uid/family.
     /// 
     /// Another bonus is that this class essentially defines every single possible kv field that can be assigned to an object
