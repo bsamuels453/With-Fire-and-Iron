112f725407e791c77dfafc5c1ae4390b751e94dc
Ben Samuels <bsamuels453@gmail.com>
2013-08-13 16:30:29 -0700

    adding gameobjectattr class

---
+++ Forge.Core/GameObjects/GameObjectAttr.cs
@@ -0,0 +1,23 @@
+﻿namespace Forge.Core.GameObjects{
+    //enums marked with req are required fields for editor game objects
+    public enum GameObjectAttr{
+        Dimensions,
+        ModelName,
+        Icon,
+        Name,
+
+        IsInteractable, //req
+        IsMultifloorInteractable, //req
+        InteractionArea,
+        InteractionOrientation,
+
+        SideEffect, //req
+        HasMultifloorAABB, //req
+        CeilingCutArea,
+
+        ProjectileEmitterOffset,
+        FiringForce,
+        Mass,
+        Radius,
+    }
+}
